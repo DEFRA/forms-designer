41126e358227db269618127a2475de0b97742125
Alex Luckett <alexgluckett@gmail.com>
2024-02-22 11:16:48 +0000

    add redis username and password to single instance cache

--- designer/server/common/helpers/redis-client.js
+++ designer/server/common/helpers/redis-client.js
@@ -19,12 +19,18 @@ function buildRedisClient() {
   let redisClient
 
   if (config.useSingleInstanceCache) {
+    logger.info("Connecting to Redis using single instance");
+    
     redisClient = new IoRedis({
+      username: config.redisUsername,
+      password: config.redisPassword,
       port,
       host: config.redisHost,
       db
     })
   } else {
+    logger.info("Connecting to Redis using cluster");
+
     redisClient = new IoRedis.Cluster(
       [
         {
