b5da4d3bc4b165f4b19f22fdd5a413de1d4e1674
Colin Rotherham <work@colinr.com>
2024-04-02 13:12:44 +0100

    Enable ESLint recommended plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,23 +20,67 @@ module.exports = {
         'standard',
         'eslint:recommended',
         'plugin:import/recommended',
+        'plugin:n/recommended',
+        'plugin:prettier/recommended',
         'plugin:promise/recommended',
-        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
         'prettier'
       ],
       files: ['**/*.{cjs,js,jsx,mjs,ts,tsx}'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        ecmaVersion: 'latest'
+        ecmaVersion: 'latest',
+        project: ['./tsconfig.json', './tsconfig.dev.json'],
+        tsconfigRootDir: __dirname,
+        EXPERIMENTAL_useProjectService: true
       },
-      plugins: ['@typescript-eslint', 'import', 'promise'],
+      plugins: ['@typescript-eslint', 'import', 'n', 'prettier', 'promise'],
       rules: {
+        'prettier/prettier': 'error',
+        'no-console': 'error',
+
+        // Check import or require statements are A-Z ordered
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          {
+            fixStyle: 'inline-type-imports'
+          }
+        ],
+
+        // Allow void return shorthand in arrow functions
+        '@typescript-eslint/no-confusing-void-expression': [
+          'error',
+          {
+            ignoreArrowShorthand: true
+          }
+        ],
+
+        // Only show warnings for missing types
+        '@typescript-eslint/no-unsafe-argument': 'warn',
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        '@typescript-eslint/no-unsafe-call': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn',
+        '@typescript-eslint/no-unsafe-return': 'warn',
+
+        // Check type support for template string implicit `.toString()`
+        '@typescript-eslint/restrict-template-expressions': [
+          'error',
+          {
+            allowBoolean: true,
+            allowNumber: true
+          }
+        ],
+
+        // Check namespace import members
         'import/namespace': [
           'error',
           {
             allowComputed: true
           }
         ],
+
+        // Check import or require statements are A-Z ordered
         'import/order': [
           'error',
           {
@@ -44,15 +88,16 @@ module.exports = {
             'newlines-between': 'always'
           }
         ],
+
+        // Skip rules handled by import plugin
+        'n/no-extraneous-require': 'off',
+        'n/no-extraneous-import': 'off',
+        'n/no-missing-require': 'off',
+        'n/no-missing-import': 'off',
+
+        // Prefer rules that are type aware
         'no-unused-vars': 'off',
         'no-use-before-define': 'off',
-        '@typescript-eslint/consistent-type-imports': [
-          'error',
-          {
-            fixStyle: 'inline-type-imports'
-          }
-        ],
-        '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unused-vars': ['error'],
         '@typescript-eslint/no-use-before-define': ['error', 'nofunc']
       },
@@ -88,11 +133,17 @@ module.exports = {
       }
     },
     {
-      files: ['**/*.{jsx,tsx}'],
+      files: ['**/*.{js,mjs}'],
+      parserOptions: {
+        sourceType: 'module'
+      }
+    },
+    {
       env: {
         browser: true
       },
       extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
+      files: ['**/*.{jsx,tsx}'],
       parserOptions: {
         ecmaFeatures: { jsx: true },
         sourceType: 'module'
@@ -109,24 +160,21 @@ module.exports = {
       }
     },
     {
+      env: {
+        jest: true
+      },
+      extends: ['plugin:jest/style'],
       files: [
         '**/*.test.{cjs,js,mjs,ts,tsx}',
         '**/__mocks__/**',
         'jest.environment.*',
         'jest.setup.*'
       ],
-      env: {
-        jest: true
-      },
-      extends: ['plugin:jest/style'],
       plugins: ['jest']
     }
   ],
   parserOptions: {
-    allowAutomaticSingleRunInference: true,
-    project: ['./tsconfig.json', './tsconfig.dev.json'],
-    tsconfigRootDir: __dirname,
-    EXPERIMENTAL_useProjectService: true
+    allowAutomaticSingleRunInference: true
   },
   root: true
 }
