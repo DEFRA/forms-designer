43edc0d6dd3772a83954127f72d5a502becc0945
Colin Rotherham <work@colinr.com>
2024-04-02 09:44:28 +0100

    Add ESLint `--cache` flag

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 export default {
-  '*.{cjs,js,jsx,mjs,ts,tsx}': 'eslint --fix',
+  '*.{cjs,js,jsx,mjs,ts,tsx}':
+    'eslint --cache --cache-location .cache/eslint --cache-strategy content --fix',
   '*.{cjs,js,jsx,json,md,mjs,scss,ts,tsx}': 'prettier --write'
 }
