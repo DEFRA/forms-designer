e8c4143e3054b7b1ff3b5bb0458c04ab3805c65f
Colin Rotherham <work@colinr.com>
2024-04-09 13:47:49 +0100

    Add JSDoc types to forms/fetch libs

--- designer/server/src/lib/fetch.js
+++ designer/server/src/lib/fetch.js
@@ -1,5 +1,11 @@
 import wreck from '@hapi/wreck'
 
+/**
+ * @template {object} [BodyType=Buffer]
+ * @param {string} method
+ * @param {string} url
+ * @param {RequestOptions} options
+ */
 export async function request(method, url, options) {
   const response = await wreck.request(method, url, options)
 
@@ -7,31 +13,72 @@ export async function request(method, url, options) {
     throw new Error(response.statusMessage)
   }
 
+  /** @type {BodyType} */
   const body = await wreck.read(response, options)
 
   return { response, body }
 }
 
+/**
+ * @template {object} [BodyType=Buffer]
+ * @param {string} url
+ * @param {RequestOptions} options
+ */
 export function get(url, options) {
-  return request('get', url, options)
+  const requestByType = /** @type {typeof request<BodyType>} */ (request)
+  return requestByType('get', url, options)
 }
 
+/**
+ * @template {object} [BodyType=Buffer]
+ * @param {string} url
+ * @param {RequestOptions} options
+ */
 export function post(url, options) {
-  return request('post', url, options)
+  const requestByType = /** @type {typeof request<BodyType>} */ (request)
+  return requestByType('post', url, options)
 }
 
+/**
+ * @template {object} [BodyType=Buffer]
+ * @param {string} url
+ * @param {RequestOptions} options
+ */
 export function put(url, options) {
-  return request('post', url, options)
+  const requestByType = /** @type {typeof request<BodyType>} */ (request)
+  return requestByType('put', url, options)
 }
 
+/**
+ * @template {object} [BodyType=Buffer]
+ * @param {string} url
+ * @param {RequestOptions} options
+ */
 export function del(url, options) {
-  return request('delete', url, options)
+  const requestByType = /** @type {typeof request<BodyType>} */ (request)
+  return requestByType('delete', url, options)
 }
 
+/**
+ * @template {object} [BodyType=Buffer]
+ * @param {string} url
+ * @param {RequestOptions} options
+ */
 export function getJson(url, options = {}) {
-  return get(url, { json: true, ...options })
+  const getByType = /** @type {typeof get<BodyType>} */ (get)
+  return getByType(url, { json: true, ...options })
 }
 
+/**
+ * @template {object} [BodyType=Buffer]
+ * @param {string} url
+ * @param {RequestOptions} options
+ */
 export function postJson(url, options = {}) {
-  return post(url, { json: true, ...options })
+  const postByType = /** @type {typeof post<BodyType>} */ (post)
+  return postByType(url, { json: true, ...options })
 }
+
+/**
+ * @typedef {Parameters<typeof wreck.defaults>[0]} RequestOptions
+ */
--- designer/server/src/lib/forms.js
+++ designer/server/src/lib/forms.js
@@ -1,26 +1,73 @@
 import config from '../config.js'
+
 import { getJson } from './fetch.js'
 
 const endpoint = `${config.managerUrl}/forms-manager/forms`
 
-export async function list () {
-  const { body } = await getJson(endpoint)
+/**
+ * List forms
+ */
+export async function list() {
+  const getJsonByType = /** @type {typeof getJson<FormConfiguration[]>} */ (
+    getJson
+  )
+
+  const { body } = await getJsonByType(endpoint)
 
   return body
 }
 
-export async function get (id) {
+/**
+ * Get form by ID
+ * @param {string} id
+ */
+export async function get(id) {
   // TODO
+  const form = /** @type {FormConfiguration} */ ({})
+  return Promise.resolve(form)
 }
 
-export async function create (data) {
+/**
+ * Create form
+ * @param {FormConfigurationInput} data
+ */
+export async function create(data) {
   // TODO
+  const form = /** @type {FormConfiguration} */ ({})
+  return Promise.resolve(form)
 }
 
-export async function update (id, data) {
+/**
+ * Update form by ID
+ * @param {string} id
+ * @param {Partial<FormConfigurationInput>} data
+ */
+export async function update(id, data) {
   // TODO
+  const form = /** @type {FormConfiguration} */ ({})
+  return Promise.resolve(form)
 }
 
-export async function remove (id) {
+/**
+ * Remove form by ID
+ * @param {string} id
+ */
+export async function remove(id) {
   // TODO
+  return Promise.resolve(true)
 }
+
+/**
+ * @todo Export from @defra/forms-manager
+ * @typedef {object} FormConfiguration
+ * @property {string} id - The id of the form
+ * @property {string} title - The human-readable title of the form
+ * @property {string} organisation - The organisation this form belongs to
+ * @property {string} teamName - The name of the team who own this form
+ * @property {string} teamEmail - The email of the team who own this form
+ */
+
+/**
+ * @todo Export from @defra/forms-manager
+ * @typedef {Omit<FormConfiguration, 'id'>} FormConfigurationInput
+ */
