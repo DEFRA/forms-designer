94dc2c5df80501742a11519e89a42c45e97640af
Colin Rotherham <work@colinr.com>
2024-04-12 15:45:04 +0100

    Fix server component macro tests

--- designer/server/src/common/components/heading/template.test.js
+++ designer/server/src/common/components/heading/template.test.js
@@ -1,30 +1,34 @@
-import { renderComponent } from '~/test-helpers/component-helpers.js'
+import { renderMacro } from '~/test/helpers/component-helpers.js'
 
 describe('Heading Component', () => {
-  let $heading
+  let $heading = /** @type {Element | null} */ (null)
 
   describe('With caption', () => {
     beforeEach(() => {
-      $heading = renderComponent('heading', {
-        text: 'Services',
-        caption: 'A page showing available services'
+      const { document } = renderMacro('appHeading', 'heading/macro.njk', {
+        params: {
+          text: 'Services',
+          caption: 'A page showing available services'
+        }
       })
+
+      $heading = document.querySelector('[data-testid="app-heading"]')
     })
 
     test('Should render app heading component', () => {
-      expect($heading('[data-testid="app-heading"]')).toHaveLength(1)
+      expect($heading).not.toBeNull()
     })
 
     test('Should contain expected heading', () => {
-      expect($heading('[data-testid="app-heading-title"]').text().trim()).toBe(
-        'Services'
-      )
+      expect(
+        $heading?.querySelector('[data-testid="app-heading-title"]')
+      ).toHaveTextContent('Services')
     })
 
     test('Should have expected heading caption', () => {
       expect(
-        $heading('[data-testid="app-heading-caption"]').text().trim()
-      ).toBe('A page showing available services')
+        $heading?.querySelector('[data-testid="app-heading-caption"]')
+      ).toHaveTextContent('A page showing available services')
     })
   })
 })
