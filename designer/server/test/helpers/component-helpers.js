1f3bff794f071bf7ced4858835eb392d9da184e8
Colin Rotherham <work@colinr.com>
2024-04-12 15:31:49 +0100

    Add test helpers for Nunjucks rendering

---
+++ designer/server/test/helpers/component-helpers.js
@@ -0,0 +1,27 @@
+import { JSDOM } from 'jsdom'
+
+import { render } from '~/src/common/nunjucks/index.js'
+
+/**
+ * Render Nunjucks macro into DOM
+ * @param {Parameters<typeof render.macro>} args
+ */
+export function renderMacro(...args) {
+  return renderDOM(render.macro(...args))
+}
+
+/**
+ * Render Nunjucks view into DOM
+ * @param {Parameters<typeof render.view>} args
+ */
+export function renderView(...args) {
+  return renderDOM(render.view(...args))
+}
+
+/**
+ * Render DOM
+ * @param {string | Buffer} [html]
+ */
+export function renderDOM(html) {
+  return new JSDOM(html).window
+}
