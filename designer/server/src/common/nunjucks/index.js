07d94e9204884a2f3f2e0bfce3f7052d86c32745
Colin Rotherham <work@colinr.com>
2024-04-12 15:28:54 +0100

    Export Nunjucks render methods for test helpers

--- designer/server/src/common/nunjucks/index.js
+++ designer/server/src/common/nunjucks/index.js
@@ -1,2 +1,3 @@
+export * as render from '~/src/common/nunjucks/render.js'
 export { environment } from '~/src/common/nunjucks/environment.js'
 export { plugin } from '~/src/common/nunjucks/plugin.js'
