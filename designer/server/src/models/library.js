4d0caf3862823a96f4ea0061a13d6063851707b0
Alex Luckett <alexgluckett@gmail.com>
2024-04-17 10:55:50 +0100

    Add edit links for form library entries

--- designer/server/src/models/library.js
+++ designer/server/src/models/library.js
@@ -3,10 +3,13 @@ import * as forms from '~/src/lib/forms.js'
 export async function listLibraryViewModel() {
   const list = await forms.list()
   const pageTitle = 'Form library'
-  const head = [{ text: 'Name' }, { text: 'Last updated' }]
+  const head = [{ text: 'Name' }, { text: 'Last updated' }, { text: 'Actions' }]
   const rows = list.map((form) => [
     { text: form.title },
-    { text: '9 Apr 2024' }
+    { text: '9 Apr 2024' },
+    {
+      html: `<a href="/forms-designer/editor/${form.id}" role="button" draggable="false" class="govuk-button" data-module="govuk-button">Edit</a>`
+    }
   ])
 
   return { pageTitle, head, rows }
