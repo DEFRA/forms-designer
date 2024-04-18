6ebd2960dff8c64800f673fe4a3dd7a2f81d7730
Colin Rotherham <work@colinr.com>
2024-04-18 22:14:35 +0100

    Add One Login service header component

--- designer/client/src/javascripts/application.js
+++ designer/client/src/javascripts/application.js
@@ -1,3 +1,7 @@
 import { initAll } from 'govuk-frontend'
 
+import { ServiceHeader } from '../../../server/src/common/components/service-header/service-header.js'
+
 initAll()
+
+new ServiceHeader(document.querySelector("[data-module='one-login-header']"))
