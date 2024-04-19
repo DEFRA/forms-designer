fa08f8927eff1ba5d280a7b40f723476341a1c82
Colin Rotherham <work@colinr.com>
2024-04-19 11:55:44 +0100

    Export One Login service navigation as ES module

--- designer/server/src/common/components/service-header/service-header.js
+++ designer/server/src/common/components/service-header/service-header.js
@@ -1,72 +1,109 @@
 /**
-* A modified adaptation of the Design System header script
-* To initialise the One Login header, run:
-* new window.CrossServiceHeader(document.querySelector("[data-module='one-login-header']")).init();
-*/
-function CrossServiceHeader ($module) {
-  this.$header = $module
-  this.$navigation = $module && $module.querySelectorAll("[data-one-login-header-nav]")
-  this.$numberOfNavs = this.$navigation && this.$navigation.length
-}
-/**
-* Initialise header
-*
-* Check for the presence of the header, menu and menu button – if any are
-* missing then there's nothing to do so return early.
-*/
-CrossServiceHeader.prototype.init = function () {
-  if (!this.$header && !this.$numberOfNavs) {
-    return
-  }
-  /**
-  * The header can render with one or two navigation elements which collapse
-  * into dropdowns on the mobile variation. This initialises the dropdown
-  * functionality for all navs that have a menu button which has:
-  * 1. a class of .js-x-header-toggle
-  * 2. an aria-controls attribute which can be mapped to the ID of the element
-  * that should be hidden on mobile
-  */
-  for (var i = 0; i < this.$numberOfNavs; i++) {
-    var $nav = this.$navigation[i];
-    $nav.$menuButton = $nav.querySelector('.js-x-header-toggle');
+ * Service header component
+ *
+ * A modified adaptation of the Design System header script
+ * To initialise the One Login header, run:
+ * new window.ServiceHeader(document.querySelector("[data-module='one-login-header']"))
+ */
+export class ServiceHeader {
+  $module
+  $navigation
 
-    $nav.$menu = $nav.$menuButton && $nav.querySelector(
-      '#' + $nav.$menuButton.getAttribute('aria-controls')
-    );
-    $nav.menuItems = $nav.$menu && $nav.$menu.querySelectorAll('li');
-    if (!$nav.$menuButton || !$nav.$menu || $nav.menuItems.length < 2) {
-      return;
+  /**
+   * @param {Element | null} $module - HTML element to use for header
+   */
+  constructor($module) {
+    this.$module = $module
+    this.$navigation =
+      $module?.querySelectorAll('[data-one-login-header-nav]') ?? []
+
+    /**
+     * The header can render with one or two navigation elements which collapse
+     * into dropdowns on the mobile variation. This initialises the dropdown
+     * functionality for all navs that have a menu button which has:
+     * 1. a class of .js-x-header-toggle
+     * 2. an aria-controls attribute which can be mapped to the ID of the element
+     * that should be hidden on mobile
+     */
+    for (const $nav of this.$navigation) {
+      const $menuButton = $nav.querySelector('.js-x-header-toggle')
+      const $menuId = $menuButton?.getAttribute('aria-controls')
+
+      if (!($menuButton instanceof HTMLButtonElement) || !$menuId) {
+        continue
+      }
+
+      const $menu = document.getElementById($menuId)
+      const $menuItems = $menu?.querySelectorAll('li')
+
+      if (!$menu || !$menuItems || $menuItems.length < 2) {
+        continue
+      }
+
+      $nav.classList.add('toggle-enabled')
+      this.checkMode($menu, $menuButton)
+
+      /**
+       * Handle menu button click
+       *
+       * When the menu button is clicked, change the visibility of the menu and then
+       * sync the accessibility state and menu button state
+       */
+      $menuButton.addEventListener('click', () =>
+        this.handleMenuButtonClick($menu, $menuButton)
+      )
+    }
+  }
+
+  /**
+   * Sync state
+   * @param {HTMLElement} $menu
+   * @param {HTMLElement} $menuButton
+   */
+  checkMode($menu, $menuButton) {
+    if (!$menu.dataset.openClass || !$menuButton.dataset.openClass) {
+      return
     }
 
-    $nav.classList.add("toggle-enabled");
-    $nav.$menuOpenClass = $nav.$menu && $nav.$menu.dataset.openClass;
-    $nav.$menuButtonOpenClass = $nav.$menuButton && $nav.$menuButton.dataset.openClass;
-    $nav.$menuButtonOpenLabel = $nav.$menuButton && $nav.$menuButton.dataset.labelForShow;
-    $nav.$menuButtonCloseLabel = $nav.$menuButton && $nav.$menuButton.dataset.labelForHide;
-    $nav.$menuButtonOpenText = $nav.$menuButton && $nav.$menuButton.dataset.textForShow;
-    $nav.$menuButtonCloseText = $nav.$menuButton && $nav.$menuButton.dataset.textForHide;
-    $nav.isOpen = false;
+    $menuButton.setAttribute(
+      'aria-expanded',
+      `${$menu.classList.contains($menu.dataset.openClass)}`
+    )
 
-    $nav.$menuButton.addEventListener('click', this.handleMenuButtonClick.bind($nav));
+    const menuIsOpen = $menuButton.getAttribute('aria-expanded') === 'true'
+
+    if ($menuButton.dataset.labelForHide && $menuButton.dataset.labelForShow) {
+      $menuButton.setAttribute(
+        'aria-label',
+        menuIsOpen
+          ? $menuButton.dataset.labelForHide
+          : $menuButton.dataset.labelForShow
+      )
+    }
+
+    if ($menuButton.dataset.textForHide && $menuButton.dataset.textForShow) {
+      $menuButton.innerHTML = menuIsOpen
+        ? $menuButton.dataset.textForHide
+        : $menuButton.dataset.textForShow
+    }
   }
-}
 
+  /**
+   * Handle menu button click
+   *
+   * When the menu button is clicked, change the visibility of the menu and then
+   * sync the accessibility state and menu button state
+   * @param {HTMLElement} $menu
+   * @param {HTMLElement} $menuButton
+   */
+  handleMenuButtonClick($menu, $menuButton) {
+    if (!$menu.dataset.openClass || !$menuButton.dataset.openClass) {
+      return
+    }
 
-/**
-* Handle menu button click
-*
-* When the menu button is clicked, change the visibility of the menu and then
-* sync the accessibility state and menu button state
-*/
-CrossServiceHeader.prototype.handleMenuButtonClick = function () {
-  this.isOpen = !this.isOpen;
-  this.$menuOpenClass && this.$menu.classList.toggle(this.$menuOpenClass, this.isOpen);
-  this.$menuButtonOpenClass && this.$menuButton.classList.toggle(this.$menuButtonOpenClass, this.isOpen);
-  this.$menuButton.setAttribute('aria-expanded', this.isOpen);
-  if (this.$menuButtonCloseLabel && this.$menuButtonOpenLabel) {
-    this.$menuButton.setAttribute('aria-label', (this.isOpen ? this.$menuButtonCloseLabel : this.$menuButtonOpenLabel));
-  }
-  if (this.$menuButtonCloseText && this.$menuButtonOpenText) {
-    this.$menuButton.innerHTML = this.isOpen ? this.$menuButtonCloseText : this.$menuButtonOpenText;
+    // Toggle menu open/closed
+    $menu.classList.toggle($menu.dataset.openClass)
+    $menuButton.classList.toggle($menuButton.dataset.openClass)
+    this.checkMode($menu, $menuButton)
   }
 }
