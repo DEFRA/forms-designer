ee52634f396856741a65eb75b35331dbe069520e
Colin Rotherham <work@colinr.com>
2024-04-30 17:26:33 +0100

    Show forms library table with link and tag

--- designer/server/src/models/library.js
+++ designer/server/src/models/library.js
@@ -1,16 +1,17 @@
 import * as forms from '~/src/lib/forms.js'
 
 export async function listLibraryViewModel() {
-  const list = await forms.list()
   const pageTitle = 'Forms library'
-  const head = [{ text: 'Name' }, { text: 'Last updated' }, { text: 'Actions' }]
-  const rows = list.map((form) => [
-    { text: form.title },
-    { text: '9 Apr 2024' },
-    {
-      html: `<a href="/editor/${form.id}" role="button" draggable="false" class="govuk-button" data-module="govuk-button">Edit</a>`
-    }
-  ])
+  const results = await forms.list()
 
-  return { pageTitle, head, rows }
+  const formItems = results.map((form) => ({
+    title: form.title,
+    href: `/editor/${form.id}`,
+    status: 'draft',
+    updated: {
+      date: '9 Apr 2024'
+    }
+  }))
+
+  return { pageTitle, formItems }
 }
