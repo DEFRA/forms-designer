f1acb82315f51f1b652c51b786fc99cc00c02a47
Colin Rotherham <work@colinr.com>
2024-05-01 17:05:11 +0100

    Save form values on POST

--- designer/server/src/routes/create.js
+++ designer/server/src/routes/create.js
@@ -34,6 +34,25 @@ export default [
   }),
 
   /**
+   * @satisfies {ServerRoute<{ Payload: Pick<FormMetadataInput, 'title'> }>}
+   */
+  ({
+    method: 'POST',
+    path: '/create/title',
+    handler(request, h) {
+      const { payload, yar } = request
+
+      // Update form metadata, redirect to next step
+      yar.set(sessionNames.create, {
+        ...yar.get(sessionNames.create),
+        title: payload.title
+      })
+
+      return h.redirect('/create/organisation').temporary()
+    }
+  }),
+
+  /**
    * @satisfies {ServerRoute}
    */
   ({
@@ -54,6 +73,25 @@ export default [
   }),
 
   /**
+   * @satisfies {ServerRoute<{ Payload: Pick<FormMetadataInput, 'organisation'> }>}
+   */
+  ({
+    method: 'POST',
+    path: '/create/organisation',
+    handler(request, h) {
+      const { payload, yar } = request
+
+      // Update form metadata, redirect to next step
+      yar.set(sessionNames.create, {
+        ...yar.get(sessionNames.create),
+        title: payload.organisation
+      })
+
+      return h.redirect('/create/team').temporary()
+    }
+  }),
+
+  /**
    * @satisfies {ServerRoute}
    */
   ({
@@ -70,6 +108,27 @@ export default [
         'question-inputs',
         create.teamViewModel(metadata, validation)
       )
+    }
+  }),
+
+  /**
+   * @satisfies {ServerRoute<{ Payload: Pick<FormMetadataInput, 'teamName' | 'teamEmail'> }>}
+   */
+  ({
+    method: 'POST',
+    path: '/create/team',
+    handler(request, h) {
+      const { payload, yar } = request
+
+      // Update form metadata, redirect to new form
+      yar.set(sessionNames.create, {
+        ...yar.get(sessionNames.create),
+        teamName: payload.teamName,
+        teamEmail: payload.teamEmail
+      })
+
+      /** @todo Submit new form metadata */
+      return h.redirect('/create/team').temporary()
     }
   })
 ]
