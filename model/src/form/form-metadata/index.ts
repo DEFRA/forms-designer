dcc0525b8eada23ce783990635a02c913063b7f7
Colin Rotherham <work@colinr.com>
2024-05-01 16:17:03 +0100

    Export Joi schema for form metadata fields

--- model/src/form/form-metadata/index.ts
+++ model/src/form/form-metadata/index.ts
@@ -2,18 +2,23 @@ import Joi from 'joi'
 
 import { type FormMetadata } from '~/src/form/form-metadata/types.js'
 
+export const titleSchema = Joi.string().max(250).trim().required()
+export const organisationSchema = Joi.string().max(100).trim().required()
+export const teamNameSchema = Joi.string().max(100).trim().required()
+export const teamEmailSchema = Joi.string()
+  .email({ tlds: { allow: ['uk'] } })
+  .trim()
+  .required()
+
 /**
  * Joi schema for `FormMetadata` interface
  * @see {@link FormMetadata}
  */
 export const formMetadataSchema = Joi.object<FormMetadata>()
   .keys({
-    title: Joi.string().max(250).trim().required(),
-    organisation: Joi.string().max(100).trim().required(),
-    teamName: Joi.string().max(100).trim().required(),
-    teamEmail: Joi.string()
-      .email({ tlds: { allow: ['uk'] } })
-      .trim()
-      .required()
+    title: titleSchema,
+    organisation: organisationSchema,
+    teamName: teamNameSchema,
+    teamEmail: teamEmailSchema
   })
   .required()
