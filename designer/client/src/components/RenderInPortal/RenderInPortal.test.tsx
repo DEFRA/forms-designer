b2127026f266ae82f7528b59d9d38d4f441c2de5
Colin Rotherham <work@colinr.com>
2024-05-02 13:41:48 +0100

    Fix tests for RenderInPortal component

--- designer/client/src/components/RenderInPortal/RenderInPortal.test.tsx
+++ designer/client/src/components/RenderInPortal/RenderInPortal.test.tsx
@@ -1,54 +1,54 @@
-test.todo('RenderInPortal renders a single component in portal')
-test.todo('RenderInPortal renders a multiple components in parallel')
-/**
- suite("Component RenderInPortal", () => {
-  test("renders paragraph inside portal", () => {
-    let portalRoot = document.querySelector("#portal-root");
- 
-    expect(portalRoot.innerHTML).to.equal("");
- 
+import { mount } from 'enzyme'
+import React from 'react'
+
+import { RenderInPortal } from '~/src/components/RenderInPortal/index.js'
+
+describe('RenderInPortal component', () => {
+  test('renders paragraph inside portal', () => {
+    let portalRoot = document.getElementById('portal-root')
+
+    expect(portalRoot?.innerHTML).toBe('')
+
     const wrapper = mount(
       <RenderInPortal>
         <p id="test-paragraph">Test</p>
       </RenderInPortal>
-    );
-    portalRoot = document.querySelector("#portal-root");
-    expect(portalRoot.innerHTML).to.equal(
+    )
+    portalRoot = document.getElementById('portal-root')
+    expect(portalRoot?.innerHTML).toBe(
       '<div><p id="test-paragraph">Test</p></div>'
-    );
- 
-    wrapper.unmount();
-    portalRoot = document.querySelector("#portal-root");
-    expect(portalRoot.innerHTML).to.equal("");
-  });
- 
-  test("renders multiple portals in parallel", () => {
+    )
+
+    wrapper.unmount()
+    portalRoot = document.getElementById('portal-root')
+    expect(portalRoot?.innerHTML).toBe('')
+  })
+
+  test('renders multiple portals in parallel', () => {
     const wrapper1 = mount(
       <RenderInPortal>
         <p id="test-paragraph1">Test 1</p>
       </RenderInPortal>
-    );
+    )
     const wrapper2 = mount(
       <RenderInPortal>
         <p id="test-paragraph2">Test 2</p>
       </RenderInPortal>
-    );
- 
-    let portalRoot = document.querySelector("#portal-root");
-    expect(portalRoot.innerHTML).to.equal(
+    )
+
+    let portalRoot = document.getElementById('portal-root')
+    expect(portalRoot?.innerHTML).toBe(
       '<div><p id="test-paragraph1">Test 1</p></div><div><p id="test-paragraph2">Test 2</p></div>'
-    );
- 
-    wrapper1.unmount();
-    portalRoot = document.querySelector("#portal-root");
-    expect(portalRoot.innerHTML).to.equal(
+    )
+
+    wrapper1.unmount()
+    portalRoot = document.getElementById('portal-root')
+    expect(portalRoot?.innerHTML).toBe(
       '<div><p id="test-paragraph2">Test 2</p></div>'
-    );
- 
-    wrapper2.unmount();
-    portalRoot = document.querySelector("#portal-root");
-    expect(portalRoot.innerHTML).to.equal("");
-  });
-});
- 
- */
+    )
+
+    wrapper2.unmount()
+    portalRoot = document.getElementById('portal-root')
+    expect(portalRoot?.innerHTML).toBe('')
+  })
+})
