f11964c2bafa2ac97ae1852ee7dd08be02832f5e
Colin Rotherham <work@colinr.com>
2024-05-08 16:23:41 +0100

    Update forms library to use real form data

--- designer/server/src/models/forms/library.js
+++ designer/server/src/models/forms/library.js
@@ -2,17 +2,7 @@ import * as forms from '~/src/lib/forms.js'
 
 export async function listViewModel() {
   const pageTitle = 'Forms library'
-  const results = await forms.list()
-
-  const formItems = results.map((form) => ({
-    title: form.title,
-    href: `/editor/${form.id}`,
-    status: 'draft',
-    updated: {
-      date: '9 Apr 2024',
-      name: 'Enrique Chase'
-    }
-  }))
+  const formItems = await forms.list()
 
   return { pageTitle, formItems }
 }
