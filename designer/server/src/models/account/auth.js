957d77b63bd4d58992bf5d5731cabdc419078118
Alex Luckett <alexgluckett@gmail.com>
2024-05-13 17:23:10 +0100

    use govukErrorSummary macro `errorList` param

--- designer/server/src/models/account/auth.js
+++ designer/server/src/models/account/auth.js
@@ -10,8 +10,17 @@ export function signedOutViewModel() {
  *  @param {boolean} userFailedAuthorisation - indicates the user has failed authorisation, but authentication was successful
  */
 export function signInViewModel(userFailedAuthorisation) {
+  const errorList = []
+
+  if (userFailedAuthorisation) {
+    errorList.push({
+      html: `We couldn’t sign you in. Please contact the system administrator for help,
+      <a href="mailto:steven.thomas@defra.gov.uk" class="govuk-link">steven.thomas@defra.gov.uk</a>`
+    })
+  }
+
   return {
     pageTitle: `Sign in to ${config.serviceName}`,
-    userFailedAuthorisation
+    errorList
   }
 }
