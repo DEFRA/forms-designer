42ee1b7bb7a664e1252bf052accb97ddca15b9f1
Colin Rotherham <work@colinr.com>
2024-05-17 09:39:24 +0100

    Add tests for grid layout attributes param

--- designer/server/src/common/components/grid-layout/template.test.js
+++ designer/server/src/common/components/grid-layout/template.test.js
@@ -75,6 +75,33 @@ describe('Grid layout component', () => {
     })
   })
 
+  describe('With attributes param', () => {
+    beforeEach(() => {
+      const { document } = renderMacro(
+        'appGridLayout',
+        'grid-layout/macro.njk',
+        {
+          params: {
+            attributes: {
+              'data-example1': 'value1',
+              'data-example2': 'value2',
+              'data-example3': 'value3'
+            }
+          }
+        }
+      )
+
+      $rows = document.querySelectorAll('.govuk-grid-row')
+      $columns = document.querySelectorAll("[class^='govuk-grid-column']")
+    })
+
+    test('Should render expected attributes', () => {
+      expect($columns[0]).toHaveAttribute('data-example1', 'value1')
+      expect($columns[0]).toHaveAttribute('data-example2', 'value2')
+      expect($columns[0]).toHaveAttribute('data-example3', 'value3')
+    })
+  })
+
   describe('With multiple rows', () => {
     beforeEach(() => {
       const { document } = renderMacro(
@@ -110,6 +137,39 @@ describe('Grid layout component', () => {
     test('Should render expected contents', () => {
       expect($columns[0]).toContainHTML('Row 1, Column 1')
       expect($columns[1]).toContainHTML('Row 2, Column 1')
+    })
+  })
+
+  describe('With multiple rows (with attributes)', () => {
+    beforeEach(() => {
+      const { document } = renderMacro(
+        'appGridLayout',
+        'grid-layout/macro.njk',
+        {
+          params: [
+            {
+              text: 'Row 1, Column 1',
+              attributes: {
+                'data-row1': 'column1'
+              }
+            },
+            {
+              text: 'Row 2, Column 1',
+              attributes: {
+                'data-row2': 'column1'
+              }
+            }
+          ]
+        }
+      )
+
+      $rows = document.querySelectorAll('.govuk-grid-row')
+      $columns = document.querySelectorAll("[class^='govuk-grid-column']")
+    })
+
+    test('Should render expected attributes', () => {
+      expect($columns[0]).toHaveAttribute('data-row1', 'column1')
+      expect($columns[1]).toHaveAttribute('data-row2', 'column1')
     })
   })
 
@@ -162,6 +222,57 @@ describe('Grid layout component', () => {
       expect($columns[1]).toContainHTML('Row 1, Column 2')
       expect($columns[2]).toContainHTML('Row 2, Column 1')
       expect($columns[3]).toContainHTML('Row 2, Column 2')
+    })
+  })
+
+  describe('With multiple rows and columns (with attributes)', () => {
+    beforeEach(() => {
+      const { document } = renderMacro(
+        'appGridLayout',
+        'grid-layout/macro.njk',
+        {
+          params: [
+            [
+              {
+                text: 'Row 1, Column 1',
+                attributes: {
+                  'data-row1': 'column1'
+                }
+              },
+              {
+                text: 'Row 1, Column 2',
+                attributes: {
+                  'data-row1': 'column2'
+                }
+              }
+            ],
+            [
+              {
+                text: 'Row 2, Column 1',
+                attributes: {
+                  'data-row2': 'column1'
+                }
+              },
+              {
+                text: 'Row 2, Column 2',
+                attributes: {
+                  'data-row2': 'column2'
+                }
+              }
+            ]
+          ]
+        }
+      )
+
+      $rows = document.querySelectorAll('.govuk-grid-row')
+      $columns = document.querySelectorAll("[class^='govuk-grid-column']")
+    })
+
+    test('Should render expected attributes', () => {
+      expect($columns[0]).toHaveAttribute('data-row1', 'column1')
+      expect($columns[1]).toHaveAttribute('data-row1', 'column2')
+      expect($columns[2]).toHaveAttribute('data-row2', 'column1')
+      expect($columns[3]).toHaveAttribute('data-row2', 'column2')
     })
   })
 })
