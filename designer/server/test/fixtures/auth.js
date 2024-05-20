bbb1095d957d719085770fb6c76b85c08215242e
Colin Rotherham <work@colinr.com>
2024-05-20 15:44:56 +0100

    Update auth fixtures to easily stub auth credentials

--- designer/server/test/fixtures/auth.js
+++ designer/server/test/fixtures/auth.js
@@ -1,65 +1,80 @@
 import { token } from '@hapi/jwt'
 import { DateTime, Duration } from 'luxon'
 
-import * as scopes from '~/src/common/constants/scopes.js'
+import { SCOPE_READ, SCOPE_WRITE } from '~/src/common/constants/scopes.js'
 
 const issuedAt = DateTime.now().minus({ minutes: 30 })
 const expiresAt = DateTime.now().plus({ minutes: 30 })
 
 /**
- * @satisfies {UserProfile}
+ * @param {Pick<UserProfile, 'groups'>} [options]
  */
-export const profile = {
-  aud: 'dummy-aud',
-  iss: 'https://login.microsoftonline.com/dummy-tid/v2.0',
-  iat: issuedAt.toUTC().toSeconds(),
-  nbf: issuedAt.toUTC().toSeconds(),
-  exp: expiresAt.toUTC().toSeconds(),
-  name: 'John Smith',
-  email: 'dummy@defra.gov.uk',
-  given_name: 'John',
-  family_name: 'Smith',
-  sub: 'dummy_session_id',
-  groups: ['valid-test-group']
+export function profile(options) {
+  return /** @satisfies {UserProfile} */ ({
+    aud: 'dummy-aud',
+    iss: 'https://login.microsoftonline.com/dummy-tid/v2.0',
+    iat: issuedAt.toUTC().toSeconds(),
+    nbf: issuedAt.toUTC().toSeconds(),
+    exp: expiresAt.toUTC().toSeconds(),
+    name: 'John Smith',
+    email: 'dummy@defra.gov.uk',
+    given_name: 'John',
+    family_name: 'Smith',
+    sub: 'dummy_session_id',
+    groups: options?.groups
+  })
 }
 
 /**
- * @satisfies {UserCredentials}
+ * @param {UserProfile} token
  */
-export const user = {
-  id: profile.sub,
-  email: profile.email,
-  displayName: profile.name,
-  issuedAt: issuedAt.toUTC().toISO(),
-  expiresAt: expiresAt.toUTC().toISO()
+export function user(token) {
+  return /** @satisfies {UserCredentials} */ ({
+    id: token.sub,
+    email: token.email,
+    displayName: token.name ?? '',
+    issuedAt: issuedAt.toUTC().toISO(),
+    expiresAt: expiresAt.toUTC().toISO()
+  })
 }
 
 /**
- * @satisfies {AuthArtifacts}
+ * @param {TokenClaims} claims
  */
-export const artifacts = {
-  token_type: 'Bearer',
-  scope: 'email openid profile User.Read',
-  expires_in: Duration.fromObject({ minutes: 30 }).as('seconds'),
-  ext_expires_in: Duration.fromObject({ minutes: 30 }).as('seconds'),
-  access_token: token.generate(profile, 'testSecret'),
-  id_token: token.generate(profile, 'testSecret'),
-  refresh_token: 'dummy-encrypted-token'
+export function artifacts(claims) {
+  return /** @satisfies {AuthArtifacts} */ ({
+    token_type: 'Bearer',
+    scope: 'email openid profile User.Read',
+    expires_in: Duration.fromObject({ minutes: 30 }).as('seconds'),
+    ext_expires_in: Duration.fromObject({ minutes: 30 }).as('seconds'),
+    access_token: token.generate(claims.token, 'testSecret'),
+    id_token: token.generate(claims.idToken, 'testSecret'),
+    refresh_token: 'dummy-encrypted-token'
+  })
 }
 
 /**
  * Credentials with tokens, user session and editor scopes
- * @satisfies {AuthCredentials}
+ * @param {Pick<AuthCredentials, 'user' | 'scope'> & { claims: TokenClaims }} options
  */
-export const credentials = {
-  provider: 'azure-oidc',
-  scope: [scopes.SCOPE_READ, scopes.SCOPE_WRITE],
-  user,
-  query: {},
-  token: artifacts.access_token,
-  idToken: artifacts.id_token,
-  refreshToken: artifacts.refresh_token,
-  expiresIn: artifacts.expires_in
+export function credentials(options) {
+  const tokens = artifacts(options.claims)
+
+  return /** @satisfies {AuthCredentials} */ ({
+    provider: 'azure-oidc',
+    scope: options.scope,
+    user: options.user,
+    query: {},
+    token: tokens.access_token,
+    idToken: tokens.id_token,
+    refreshToken: tokens.refresh_token,
+    expiresIn: tokens.expires_in
+  })
+}
+
+const claims = {
+  token: profile(),
+  idToken: profile({ groups: ['valid-test-group'] })
 }
 
 /**
@@ -68,18 +83,12 @@ export const credentials = {
  */
 export const auth = {
   strategy: 'azure-oidc',
-  artifacts,
-  credentials
-}
-
-/**
- * Request auth without scopes for Hapi `server.inject()`
- * @satisfies {ServerInjectOptions['auth']}
- */
-export const authNoScopes = {
-  strategy: 'azure-oidc',
-  artifacts,
-  credentials: { ...credentials, scope: [] }
+  artifacts: artifacts(claims),
+  credentials: credentials({
+    claims,
+    user: user(claims.idToken),
+    scope: [SCOPE_READ, SCOPE_WRITE]
+  })
 }
 
 /**
@@ -88,4 +97,6 @@ export const authNoScopes = {
  * @typedef {import('@hapi/hapi').ServerInjectOptions} ServerInjectOptions
  * @typedef {import('@hapi/hapi').UserCredentials} UserCredentials
  * @typedef {import('~/src/common/helpers/auth/azure-oidc.js').UserProfile} UserProfile
+ * @typedef {import('~/src/common/helpers/auth/get-user-session.js').Tokens} Tokens
+ * @typedef {Record<keyof Tokens, ReturnType<typeof profile>>} TokenClaims
  */
