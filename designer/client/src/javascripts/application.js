af266465849f5980899512eaedb7ad38d9fcebe3
Colin Rotherham <work@colinr.com>
2024-05-21 16:38:53 +0100

    Import necessary GOV.UK Frontend components only

--- designer/client/src/javascripts/application.js
+++ designer/client/src/javascripts/application.js
@@ -1,6 +1,15 @@
 import { ServiceHeader } from '@defra/forms-designer/server/src/common/components/service-header/service-header.js'
-import { initAll } from 'govuk-frontend'
+import {
+  createAll,
+  Button,
+  ErrorSummary,
+  CharacterCount,
+  Radios
+} from 'govuk-frontend'
 
-initAll()
+createAll(Button)
+createAll(CharacterCount)
+createAll(ErrorSummary)
+createAll(Radios)
 
 new ServiceHeader(document.querySelector("[data-module='one-login-header']"))
