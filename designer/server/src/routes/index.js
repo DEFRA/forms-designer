8826e1a4d8a789a35769ce117a17b21371d3fa22
Alex Luckett <alexgluckett@gmail.com>
2024-05-24 11:29:30 +0100

    healthCheck -> health

--- designer/server/src/routes/index.js
+++ designer/server/src/routes/index.js
@@ -1,8 +1,8 @@
 import account from './account/index.js'
 import assets from './assets.js'
 import forms from './forms/index.js'
-import healthCheck from './health.js'
+import health from './health.js'
 import help from './help.js'
 import home from './home.js'
 
-export default [account, assets, forms, healthCheck, home, help].flat()
+export default [account, assets, forms, health, home, help].flat()
