a6e7a0ad27830255436538dd137fd69db910dbc2
Colin Rotherham <work@colinr.com>
2024-05-30 10:37:56 +0100

    Flash editor errors to yar session

--- designer/server/src/routes/forms/api.js
+++ designer/server/src/routes/forms/api.js
@@ -1,6 +1,8 @@
 import { formDefinitionSchema } from '@defra/forms-model'
 import Joi from 'joi'
 
+import { sessionNames } from '~/src/common/constants/session-names.js'
+import { buildErrorDetails } from '~/src/common/helpers/build-error-details.js'
 import * as forms from '~/src/lib/forms.js'
 
 export default [
@@ -35,7 +37,7 @@ export default [
         parse: true
       },
       async handler(request, h) {
-        const { auth, params, payload } = request
+        const { auth, params, payload, yar } = request
         const { id } = params
         const token = auth.credentials.token
 
@@ -48,13 +50,18 @@ export default [
           if (result.error) {
             const { error } = result
 
+            yar.flash(sessionNames.validationFailure, {
+              formErrors: buildErrorDetails(error),
+              formValues: payload
+            })
+
             throw new Error(
               `Schema validation failed, reason: ${error.message}`,
               { cause: error }
             )
           }
 
-          const value = result.value
+          const { value } = result
 
           // Update the form definition
           await forms.updateDraftFormDefinition(id, value, token)
@@ -62,13 +69,6 @@ export default [
           return h.response({ ok: true }).code(204)
         } catch (err) {
           request.logger.error(err, 'Designer Server PUT /api/{id}/data error')
-          const errorSummary = {
-            id,
-            payload,
-            errorMessage: err.message,
-            error: err.stack
-          }
-          request.yar.set(`error-summary-${id}`, errorSummary)
           return h.response({ ok: false, err }).code(500)
         }
       }
