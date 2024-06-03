3d40d8c251f9d997a3e2928853660d6708f18444
Colin Rotherham <work@colinr.com>
2024-06-03 18:26:29 +0100

    Sort A-Z

--- designer/server/jest.setup.cjs
+++ designer/server/jest.setup.cjs
@@ -1,21 +1,21 @@
 const { Duration } = require('luxon')
 
-process.env.SESSION_COOKIE_PASSWORD = 'test-env-session-cookie-password'
-process.env.ROLE_EDITOR_GROUP_ID = 'valid-test-group'
+process.env.APP_BASE_URL = 'http://localhost:3000'
+process.env.AZURE_CLIENT_ID = 'dummy'
+process.env.AZURE_CLIENT_SECRET = 'dummy'
 process.env.MANAGER_URL = 'http://localhost:3001'
+process.env.OIDC_WELL_KNOWN_CONFIGURATION_URL = 'dummy'
 process.env.PREVIEW_URL = 'http://localhost:3009'
-process.env.SESSION_TTL = Duration.fromObject({ days: 1 })
-  .as('milliseconds')
-  .toString()
+process.env.REDIS_HOST = 'dummy'
+process.env.REDIS_HOST = 'dummy'
+process.env.REDIS_KEY_PREFIX = 'forms-designer'
+process.env.REDIS_PASSWORD = 'dummy'
+process.env.REDIS_USERNAME = 'dummy'
+process.env.ROLE_EDITOR_GROUP_ID = 'valid-test-group'
+process.env.SESSION_COOKIE_PASSWORD = 'test-env-session-cookie-password'
 process.env.SESSION_COOKIE_TTL = Duration.fromObject({ minutes: 30 })
   .as('milliseconds')
   .toString()
-process.env.AZURE_CLIENT_ID = 'dummy'
-process.env.AZURE_CLIENT_SECRET = 'dummy'
-process.env.OIDC_WELL_KNOWN_CONFIGURATION_URL = 'dummy'
-process.env.APP_BASE_URL = 'http://localhost:3000'
-process.env.REDIS_HOST = 'dummy'
-process.env.REDIS_USERNAME = 'dummy'
-process.env.REDIS_PASSWORD = 'dummy'
-process.env.REDIS_KEY_PREFIX = 'forms-designer'
-process.env.REDIS_HOST = 'dummy'
+process.env.SESSION_TTL = Duration.fromObject({ days: 1 })
+  .as('milliseconds')
+  .toString()
