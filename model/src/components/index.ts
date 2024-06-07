8445e60a8668d9d2bf7df4769b4408502b44c2e8
Colin Rotherham <work@colinr.com>
2024-06-07 12:05:43 +0100

    Remove unused `ConditionalComponentTypes`

--- model/src/components/index.ts
+++ model/src/components/index.ts
@@ -1,3 +1,2 @@
 export { ComponentTypes } from '~/src/components/component-types.js'
-export { ConditionalComponentTypes } from '~/src/components/conditional-component-types.js'
 export { ComponentType, ComponentSubType } from '~/src/components/enums.js'
