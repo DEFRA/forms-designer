c3e9fd86b289735bfbff14b87cc8ad578f91bcc1
Colin Rotherham <work@colinr.com>
2024-06-13 20:35:06 +0100

    Add Nunjucks `formatJSON` filter

--- designer/server/src/common/nunjucks/filters/index.js
+++ designer/server/src/common/nunjucks/filters/index.js
@@ -1,4 +1,3 @@
-import { formatCurrency } from '~/src/common/nunjucks/filters/format-currency.js'
-import { formatDate } from '~/src/common/nunjucks/filters/format-date.js'
-
-export { formatDate, formatCurrency }
+export { formatCurrency } from '~/src/common/nunjucks/filters/format-currency.js'
+export { formatDate } from '~/src/common/nunjucks/filters/format-date.js'
+export { formatJSON } from '~/src/common/nunjucks/filters/format-json.js'
