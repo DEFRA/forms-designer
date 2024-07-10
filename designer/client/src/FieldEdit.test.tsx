4e4753d58cf83bb894ce8a191ed385ffc413a1b6
Colin Rotherham <work@colinr.com>
2024-07-10 12:01:07 +0100

    Update `<FieldEdit>` tests to check sets of options
    Rather than duplicate the same checks for `<ComponentTypeEdit>`

--- designer/client/src/FieldEdit.test.tsx
+++ designer/client/src/FieldEdit.test.tsx
@@ -11,80 +11,99 @@ import { FieldEdit } from '~/src/FieldEdit.jsx'
 import { RenderWithContext } from '~/test/helpers/renderers.jsx'
 
 describe('Field Edit', () => {
-  const selectedComponent = {
-    name: 'IDDQl4',
-    type: ComponentType.UkAddressField,
-    title: 'UK address field',
-    hint: '',
-    options: {},
-    schema: {}
-  } satisfies ComponentDef
+  let selectedComponent: ComponentDef
+  let data: FormDefinition
 
-  const data = {
-    pages: [
-      {
-        title: 'First page',
-        path: '/first-page',
-        components: [selectedComponent]
-      }
-    ],
-    lists: [],
-    sections: [],
-    conditions: []
-  } satisfies FormDefinition
+  beforeEach(() => {
+    selectedComponent = {
+      name: 'IDDQl4',
+      title: 'Text field',
+      type: ComponentType.TextField,
+      hint: '',
+      options: {},
+      schema: {}
+    }
+
+    data = {
+      pages: [
+        {
+          title: 'First page',
+          path: '/first-page',
+          components: [selectedComponent]
+        }
+      ],
+      lists: [],
+      sections: [],
+      conditions: []
+    }
+  })
 
   afterEach(cleanup)
 
-  test('Help text changes', () => {
+  it('should render options for non-list components', () => {
+    render(
+      <RenderWithContext data={data} state={{ selectedComponent }}>
+        <FieldEdit />
+      </RenderWithContext>
+    )
+
+    const $inputs = screen.queryAllByRole('textbox')
+    const $checkboxes = screen.queryAllByRole('checkbox')
+    const $selects = screen.queryAllByRole('combobox')
+
+    expect($inputs).toHaveLength(3)
+    expect($checkboxes).toHaveLength(1)
+    expect($selects).toHaveLength(0)
+  })
+
+  it('should render options for list component', () => {
+    selectedComponent.title = 'List'
+    selectedComponent.type = ComponentType.List
+
+    render(
+      <RenderWithContext data={data} state={{ selectedComponent }}>
+        <FieldEdit />
+      </RenderWithContext>
+    )
+
+    const $inputs = screen.queryAllByRole('textbox')
+    const $checkboxes = screen.queryAllByRole('checkbox')
+    const $selects = screen.queryAllByRole('combobox')
+
+    expect($inputs).toHaveLength(3)
+    expect($checkboxes).toHaveLength(0)
+    expect($selects).toHaveLength(0)
+  })
+
+  it('should render options for components with lists', () => {
+    selectedComponent.title = 'Autocomplete'
+    selectedComponent.type = ComponentType.AutocompleteField
+
+    render(
+      <RenderWithContext data={data} state={{ selectedComponent }}>
+        <FieldEdit />
+      </RenderWithContext>
+    )
+
+    const $inputs = screen.queryAllByRole('textbox')
+    const $checkboxes = screen.queryAllByRole('checkbox')
+    const $selects = screen.queryAllByRole('combobox')
+
+    expect($inputs).toHaveLength(3)
+    expect($checkboxes).toHaveLength(1)
+    expect($selects).toHaveLength(0)
+  })
+
+  it('should not render options for content components', () => {
+    selectedComponent.title = 'Details'
+    selectedComponent.type = ComponentType.Details
+
     const { container } = render(
       <RenderWithContext data={data} state={{ selectedComponent }}>
         <FieldEdit />
       </RenderWithContext>
     )
 
-    expect(container).toHaveTextContent('Enter the name to show for this field')
-
-    expect(container).toHaveTextContent(
-      'Enter the description to show for this field'
-    )
-
-    expect(container).toHaveTextContent(
-      'Tick this box if you do not want the title to show on the page'
-    )
-
-    expect(container).toHaveTextContent(
-      'This is generated automatically and does not show on the page. Only change it if you are using an integration that requires you to, for example GOV.UK Notify. It must not contain spaces.'
-    )
-
-    expect(
-      screen.getByText(
-        'Tick this box if users do not need to complete this field to progress through the form'
-      )
-    ).toBeInTheDocument()
-  })
-
-  test('Content fields should not have optional checkbox', () => {
-    const { container } = render(
-      <RenderWithContext data={data} state={{ selectedComponent }}>
-        <FieldEdit isContentField={true} />
-      </RenderWithContext>
-    )
-    expect(container).toHaveTextContent('Enter the name to show for this field')
-
-    expect(container).toHaveTextContent(
-      'Enter the description to show for this field'
-    )
-
-    expect(container).toHaveTextContent(
-      'Tick this box if you do not want the title to show on the page'
-    )
-
-    expect(container).toHaveTextContent(
-      'This is generated automatically and does not show on the page. Only change it if you are using an integration that requires you to, for example GOV.UK Notify. It must not contain spaces.'
-    )
-
-    expect(container).not.toHaveTextContent(
-      'Tick this box if users do not need to complete this field to progress through the form'
-    )
+    expect(container.textContent).toBe('')
   })
 })
