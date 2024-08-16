5c62f98b52932119e5364c22a591331254ebdaa8
Colin Rotherham <work@colinr.com>
2024-08-16 09:23:43 +0100

    Align helpers `<RenderWithContext>` and `<RenderListEditorWithContext>`

--- designer/client/test/helpers/renderers-lists.tsx
+++ designer/client/test/helpers/renderers-lists.tsx
@@ -13,25 +13,29 @@ export interface RenderListEditorWithContextProps {
   children: ReactElement
   selectedListName?: string
   data: DataContextType['data']
+  meta?: DataContextType['meta']
   save?: DataContextType['save']
   state?: Parameters<typeof initComponentState>[0]
 }
 
-export function RenderListEditorWithContext({
-  children,
-  selectedListName,
-  state: componentState,
-  data,
-  save = jest.fn()
-}: RenderListEditorWithContextProps) {
+export function RenderListEditorWithContext(
+  props: RenderListEditorWithContextProps
+) {
   const [state, dispatch] = useReducer(
     componentReducer,
-    initComponentState(componentState),
-    initComponentState
+    initComponentState(props.state)
   )
 
+  const {
+    children,
+    selectedListName,
+    data = {} as DataContextType['data'],
+    meta,
+    save = jest.fn()
+  } = props
+
   return (
-    <DataContext.Provider value={{ data, save }}>
+    <DataContext.Provider value={{ data, meta, save }}>
       <ListsEditorContextProvider>
         <ComponentContext.Provider value={{ state, dispatch }}>
           <ListContextProvider selectedListName={selectedListName}>
