8ff9d95cc43e0fbad18b88e194404f7cb74c1a43
Colin Rotherham <work@colinr.com>
2024-08-28 08:38:37 +0100

    Remove unused model helpers

--- model/src/utils/helpers.ts
+++ model/src/utils/helpers.ts
@@ -1,39 +1,5 @@
 import slug from 'slug'
 
-export const serialiseAndDeserialise = <T>(obj: T): T => {
-  if (typeof obj === 'object' && obj !== null) {
-    return JSON.parse(JSON.stringify(obj))
-  }
-
-  return obj
-}
-
-export const clone = <T>(obj: T & { clone?: () => T }): T => {
-  if (obj) {
-    if (typeof obj.clone === 'function') {
-      return obj.clone()
-    }
-
-    return serialiseAndDeserialise<T>(obj)
-  }
-  return obj
-}
-
-export function filter<T extends Record<string, unknown>>(
-  obj: T,
-  predicate: (value: any) => boolean
-): Partial<T> {
-  const result = {}
-
-  for (const [key, value] of Object.entries(obj)) {
-    if (value && predicate(value)) {
-      result[key] = value
-    }
-  }
-
-  return result
-}
-
 /**
  * Replace whitespace, en-dashes and em-dashes with spaces
  * before running through the slug package
