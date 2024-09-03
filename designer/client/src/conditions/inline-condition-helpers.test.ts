3f08e39608fb63860562b0f85be74a854f9a84c6
Colin Rotherham <work@colinr.com>
2024-09-03 10:12:15 +0100

    Add test coverage for `tryParseInt()` with decimals, non-numbers

--- designer/client/src/conditions/inline-condition-helpers.test.ts
+++ designer/client/src/conditions/inline-condition-helpers.test.ts
@@ -9,7 +9,22 @@ describe('tryParseInt', () => {
     expect(result).toBe(2020)
   })
 
-  it("returns undefined if a valid integer can't be parsed", () => {
+  it('returns a valid integer for numbers', () => {
+    const result = tryParseInt(2020)
+    expect(result).toBe(2020)
+  })
+
+  it('returns a valid integer for decimals', () => {
+    const result = tryParseInt(2020.2)
+    expect(result).toBe(2020)
+  })
+
+  it('returns undefined for non-numbers', () => {
+    const result = tryParseInt('ABC')
+    expect(result).toBeUndefined()
+  })
+
+  it('returns undefined for empty strings', () => {
     const result = tryParseInt('')
     expect(result).toBeUndefined()
   })
