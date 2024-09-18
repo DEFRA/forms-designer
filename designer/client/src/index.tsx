b287f584aafda2c7727d0c0033e834aca4de0600
Colin Rotherham <work@colinr.com>
2024-09-18 11:54:28 +0100

    Initialise `<App>` when i18n is ready

--- designer/client/src/index.tsx
+++ designer/client/src/index.tsx
@@ -8,44 +8,62 @@ import { initI18n } from '~/src/i18n/i18n.jsx'
 
 import 'prismjs/components/prism-json.js'
 
-initI18n().catch((error: unknown) => {
-  logger.error(error, 'I18n')
-})
+interface Props {
+  container: HTMLElement
+}
 
-const $container = document.querySelector('.app-form-editor')
-const $definition = document.querySelector('.app-form-definition')
-const $metadata = document.querySelector('.app-form-metadata')
+export class App extends Component<Props> {
+  metadata: FormMetadata
+  definition: FormDefinition
+  previewUrl: string
 
-export class App extends Component {
-  render() {
+  constructor(props: Props) {
+    super(props)
+
+    const $definition = document.querySelector('.app-form-definition')
+    const $metadata = document.querySelector('.app-form-metadata')
+
+    // Extract from HTML data-* attributes
+    const { previewUrl } = props.container.dataset
+
     if (
-      !($container instanceof HTMLElement) ||
       !($metadata instanceof HTMLScriptElement) ||
       !($definition instanceof HTMLScriptElement) ||
-      !$container.dataset.previewUrl ||
       !$definition.textContent ||
-      !$metadata.textContent
+      !$metadata.textContent ||
+      !previewUrl
     ) {
       throw new Error('Missing form data')
     }
 
-    let definition: FormDefinition | undefined
-    let metadata: FormMetadata | undefined
+    this.definition = JSON.parse($definition.textContent) as FormDefinition
+    this.metadata = JSON.parse($metadata.textContent) as FormMetadata
+    this.previewUrl = previewUrl
+  }
 
-    try {
-      definition = JSON.parse($definition.textContent) as FormDefinition
-      metadata = JSON.parse($metadata.textContent) as FormMetadata
-    } catch (error) {
-      logger.error(error, 'App')
-      throw error
-    }
-
-    // Extract from HTML data-* attributes
-    const { previewUrl } = $container.dataset
+  render() {
     return (
-      <Designer meta={metadata} data={definition} previewUrl={previewUrl} />
+      <Designer
+        meta={this.metadata}
+        data={this.definition}
+        previewUrl={this.previewUrl}
+      />
     )
   }
 }
 
-ReactDOM.render(<App />, $container)
+function initApp() {
+  const $root = document.querySelector('.app-form-editor')
+
+  if (!($root instanceof HTMLElement)) {
+    throw new Error('Missing form data')
+  }
+
+  ReactDOM.render(<App container={$root} />, $root)
+}
+
+initI18n()
+  .then(initApp)
+  .catch((error: unknown) => {
+    logger.error(error, 'App')
+  })
