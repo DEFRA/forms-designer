4f1b4e36404410a3944649be0f17a617e6b54c6c
David Stone <davidjamesstone@gmail.com>
2024-09-18 10:13:24 +0100

    Rename markdowntoHtml filter to markdown

--- designer/server/src/common/nunjucks/filters/index.js
+++ designer/server/src/common/nunjucks/filters/index.js
@@ -1,4 +1,4 @@
-export { markdownToHtml } from '@defra/forms-model'
+export { markdownToHtml as markdown } from '@defra/forms-model'
 export { formatCurrency } from '~/src/common/nunjucks/filters/format-currency.js'
 export { formatDate } from '~/src/common/nunjucks/filters/format-date.js'
 export { formatJSON } from '~/src/common/nunjucks/filters/format-json.js'
