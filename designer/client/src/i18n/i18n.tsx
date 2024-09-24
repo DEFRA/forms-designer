e03a0c5d590dc09eaed41d326a0fd300e3bad5af
Colin Rotherham <work@colinr.com>
2024-09-24 16:10:49 +0100

    Remove i18n JSON from bundle

--- designer/client/src/i18n/i18n.tsx
+++ designer/client/src/i18n/i18n.tsx
@@ -1,22 +1,15 @@
 import I18next, { type InitOptions, type TOptions } from 'i18next'
-import Backend from 'i18next-http-backend'
+import Backend, { type HttpBackendOptions } from 'i18next-http-backend'
 import lowerFirst from 'lodash/lowerFirst.js'
 import upperFirst from 'lodash/upperFirst.js'
 
-import enCommonTranslations from '~/src/i18n/translations/en.translation.json'
-
-const DEFAULT_SETTINGS: InitOptions = {
+const DEFAULT_SETTINGS: InitOptions<HttpBackendOptions> = {
   lng: 'en',
   fallbackLng: 'en',
   debug: false,
   interpolation: {
     escapeValue: false,
     skipOnVariables: false
-  },
-  resources: {
-    en: {
-      translation: enCommonTranslations
-    }
   },
   backend: {
     loadPath: '/assets/translations/{{lng}}.{{ns}}.json'
