8e8e58fbafaf61e23a974d0baba33626dbc9c058
Kiran Varma <varma.ap@gmail.com>
2024-10-01 15:02:49 +0100

    change key string

--- designer/server/src/createServer.ts
+++ designer/server/src/createServer.ts
@@ -86,10 +86,10 @@ export async function createServer() {
   )
   server.method('session.drop', (id) => cache.drop(id))
 
-  server.method('state.get', (userId, key) => cache.get(userId + key))
+  server.method('state.get', (userId, key) => cache.get(`${userId}.${key}`))
   server.method('state.set', (userId, key, value, ttl = config.sessionTtl) => {
     if (!userId) return Promise.reject(new Error('userId is required'))
-    return cache.set(userId + key, value, ttl)
+    return cache.set(`${userId}.${key}`, value, ttl)
   })
   server.method('state.drop', (id) => cache.drop(id))
 
