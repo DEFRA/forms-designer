79d1fbe3be2c2de9e704f4f8706cd834d9bdcd45
Colin Rotherham <work@colinr.com>
2024-10-02 12:52:15 +0100

    Move ESLint overrides into related workspaces

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -222,32 +222,6 @@ module.exports = {
     },
     {
       env: {
-        browser: true
-      },
-      extends: [
-        'plugin:jsx-a11y/recommended',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'plugin:react-hooks/recommended'
-      ],
-      files: ['**/*.{jsx,tsx}'],
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module'
-      },
-      plugins: ['jsx-a11y', 'react', 'react-hooks'],
-      rules: {
-        'react/prop-types': 0,
-        'react-hooks/rules-of-hooks': 'warn'
-      },
-      settings: {
-        react: {
-          version: 'detect'
-        }
-      }
-    },
-    {
-      env: {
         'jest/globals': true
       },
       extends: ['plugin:jest/recommended', 'plugin:jest/style'],
