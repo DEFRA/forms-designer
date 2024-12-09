4afc3711fb1f2704df7ed27aa3301ab9911b026a
Colin Rotherham <work@colinr.com>
2024-12-09 11:55:42 +0000

    Update fixup tests to modify `data` instead

--- designer/client/src/data/page/fixupPages.test.ts
+++ designer/client/src/data/page/fixupPages.test.ts
@@ -49,74 +49,58 @@ describe('fixupPages', () => {
   })
 
   it('should fix empty start page using pages array', () => {
-    const updated = {
-      ...data,
+    // Empty start page
+    data.startPage = ''
 
-      // Empty start page
-      startPage: ''
-    }
+    const returned = fixupPages(data)
 
-    const returned = fixupPages(updated)
-
-    expect(returned.pages).toEqual(updated.pages)
+    expect(returned.pages).toEqual(data.pages)
     expect(returned.startPage).toBe('/first-page')
   })
 
   it('should fix incorrect start page using pages array', () => {
-    const updated = {
-      ...data,
+    // Incorrect start page
+    data.startPage = '/404'
 
-      // Incorrect start page
-      startPage: '/404'
-    }
+    const returned = fixupPages(data)
 
-    const returned = fixupPages(updated)
-
-    expect(returned.pages).toEqual(updated.pages)
+    expect(returned.pages).toEqual(data.pages)
     expect(returned.startPage).toBe('/first-page')
   })
 
   it('should fix incorrect start page when pages are deleted', () => {
-    const updated = {
-      ...data,
+    // Remove first page
+    data.pages = data.pages.slice(1)
 
-      // Remove first page
-      pages: data.pages.slice(1)
-    }
+    const returned = fixupPages(data)
 
-    const returned = fixupPages(updated)
-
-    expect(returned.pages).toEqual(updated.pages)
+    expect(returned.pages).toEqual(data.pages)
     expect(returned.startPage).toBe('/second-page')
   })
 
   it('should remove start page with empty pages', () => {
-    const updated = {
-      ...data,
-      pages: []
-    }
+    // Remove all pages
+    data.pages = []
 
-    const returned = fixupPages(updated)
+    const returned = fixupPages(data)
 
-    expect(returned.pages).toEqual(updated.pages)
+    expect(returned.pages).toEqual(data.pages)
     expect(returned.startPage).toBeUndefined()
   })
 
-  it('should remove start page update when multiple journeys are possible', () => {
+  it('should remove start page when multiple journeys are possible', () => {
     const pages = structuredClone(data.pages) as PageQuestion[]
 
     pages[0].next = [{ path: pages[2].path }]
     pages[1].next = [{ path: pages[2].path }]
     pages[2].next = [{ path: pages[2].path }]
 
-    const updated = {
-      ...data,
-      pages
-    }
+    // Update all page links
+    data.pages = pages
 
-    const returned = fixupPages(updated)
+    const returned = fixupPages(data)
 
-    expect(returned.pages).toEqual(updated.pages)
+    expect(returned.pages).toEqual(data.pages)
     expect(returned.startPage).toBeUndefined()
   })
 
@@ -127,14 +111,12 @@ describe('fixupPages', () => {
     pages[1].next = []
     pages[2].next = []
 
-    const updated = {
-      ...data,
-      pages
-    }
+    // Remove all page links
+    data.pages = pages
 
-    const returned = fixupPages(updated)
+    const returned = fixupPages(data)
 
-    expect(returned.pages).toEqual(updated.pages)
+    expect(returned.pages).toEqual(data.pages)
     expect(returned.startPage).toBeUndefined()
   })
 
