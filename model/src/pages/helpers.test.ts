b65558af15e70881ba2d26228eaf60dded7e0430
Colin Rotherham <work@colinr.com>
2024-12-10 11:20:06 +0000

    Add helper test coverage

--- model/src/pages/helpers.test.ts
+++ model/src/pages/helpers.test.ts
@@ -1,7 +1,120 @@
 import { ControllerTypes } from '~/src/pages/controller-types.js'
-import { controllerNameFromPath } from '~/src/pages/helpers.js'
+import { ControllerType } from '~/src/pages/enums.js'
+import {
+  controllerNameFromPath,
+  getPageDefaults,
+  hasComponents,
+  hasFormComponents,
+  hasNext,
+  hasRepeater
+} from '~/src/pages/helpers.js'
+import { PageTypes } from '~/src/pages/page-types.js'
 
 describe('helpers', () => {
+  describe('getPageDefaults', () => {
+    it.each([...PageTypes])(
+      "returns page defaults for page type '$controller'",
+      (page) => {
+        const { controller } = page
+        expect(getPageDefaults({ controller })).toEqual(page)
+      }
+    )
+  })
+
+  describe('hasComponents', () => {
+    const supported = [
+      ControllerType.Start,
+      ControllerType.Page,
+      ControllerType.FileUpload,
+      ControllerType.Repeat
+    ]
+
+    it.each(
+      PageTypes.filter(({ controller }) => {
+        return !!controller && supported.includes(controller)
+      })
+    )("returns true for supported page type '$controller'", (page) =>
+      expect(hasComponents(page)).toBe(true)
+    )
+
+    it.each(
+      PageTypes.filter(({ controller }) => {
+        return !!controller && !supported.includes(controller)
+      })
+    )("returns false for unsupported page type '$controller'", (page) =>
+      expect(hasComponents(page)).toBe(false)
+    )
+  })
+
+  describe('hasFormComponents', () => {
+    const supported = [
+      ControllerType.Page,
+      ControllerType.FileUpload,
+      ControllerType.Repeat
+    ]
+
+    it.each(
+      PageTypes.filter(({ controller }) => {
+        return !!controller && supported.includes(controller)
+      })
+    )("returns true for supported page type '$controller'", (page) =>
+      expect(hasFormComponents(page)).toBe(true)
+    )
+
+    it.each(
+      PageTypes.filter(({ controller }) => {
+        return !!controller && !supported.includes(controller)
+      })
+    )("returns false for unsupported page type '$controller'", (page) =>
+      expect(hasFormComponents(page)).toBe(false)
+    )
+  })
+
+  describe('hasNext', () => {
+    const supported = [
+      ControllerType.Start,
+      ControllerType.Page,
+      ControllerType.FileUpload,
+      ControllerType.Repeat
+    ]
+
+    it.each(
+      PageTypes.filter(({ controller }) => {
+        return !!controller && supported.includes(controller)
+      })
+    )("returns true for supported page type '$controller'", (page) =>
+      expect(hasNext(page)).toBe(true)
+    )
+
+    it.each(
+      PageTypes.filter(({ controller }) => {
+        return !!controller && !supported.includes(controller)
+      })
+    )("returns false for unsupported page type '$controller'", (page) =>
+      expect(hasNext(page)).toBe(false)
+    )
+  })
+
+  describe('hasRepeater', () => {
+    const supported = [ControllerType.Repeat]
+
+    it.each(
+      PageTypes.filter(({ controller }) => {
+        return !!controller && supported.includes(controller)
+      })
+    )("returns true for supported page type '$controller'", (page) =>
+      expect(hasRepeater(page)).toBe(true)
+    )
+
+    it.each(
+      PageTypes.filter(({ controller }) => {
+        return !!controller && !supported.includes(controller)
+      })
+    )("returns false for unsupported page type '$controller'", (page) =>
+      expect(hasRepeater(page)).toBe(false)
+    )
+  })
+
   describe('controllerNameFromPath', () => {
     it.each([...ControllerTypes])(
       "returns controller name for '$path' legacy path",
