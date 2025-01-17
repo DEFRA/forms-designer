5c2f3f94649673be065a21b31dea24193218c3ea
David Stone <davidjamesstone@gmail.com>
2025-01-17 14:50:28 +0000

    Change what position pages are added at for V2

--- designer/client/src/data/page/addPage.ts
+++ designer/client/src/data/page/addPage.ts
@@ -1,4 +1,9 @@
-import { type FormDefinition, type Page } from '@defra/forms-model'
+import {
+  ControllerType,
+  Engine,
+  type FormDefinition,
+  type Page
+} from '@defra/forms-model'
 
 import { findPage } from '~/src/data/page/findPage.js'
 
@@ -11,7 +16,23 @@ export function addPage(data: FormDefinition, page: Page) {
     const definition = structuredClone(data)
 
     // Add new page
-    definition.pages.push(page)
+    if (data.engine === Engine.V2) {
+      // If this new page is a "Start" page, add it as the first page
+      if (page.controller === ControllerType.Start) {
+        definition.pages.splice(0, 0, page)
+      } else {
+        const lastPage = data.pages.at(data.pages.length - 1)
+
+        // If the last page is a "Summary" page, add this new page before it
+        if (lastPage?.controller === ControllerType.Summary) {
+          definition.pages.splice(data.pages.length - 1, 0, page)
+        } else {
+          definition.pages.push(page)
+        }
+      }
+    } else {
+      definition.pages.push(page)
+    }
 
     return definition
   }
