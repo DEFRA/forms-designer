0ba25ecb48b09ed360a5ccb2fe9ec7122bd054d0
David Stone <davidjamesstone@gmail.com>
2025-01-20 14:38:01 +0000

    Add V2 tests for addPage helper

--- designer/client/src/data/page/addPage.test.ts
+++ designer/client/src/data/page/addPage.test.ts
@@ -1,4 +1,4 @@
-import { type FormDefinition } from '@defra/forms-model'
+import { ControllerType, Engine, type FormDefinition } from '@defra/forms-model'
 
 import { addPage } from '~/src/data/page/addPage.js'
 
@@ -50,6 +50,83 @@ test('addPage adds a page if one does not exist with the same path', () => {
   ).toContainEqual({
     title: 'soft boiled',
     path: '/soft-boiled',
+    next: [],
+    components: []
+  })
+})
+
+test('addPage adds a start page to position 0 in V2', () => {
+  const v2 = {
+    engine: Engine.V2,
+    ...data
+  }
+
+  const result = addPage(v2, {
+    title: 'Start',
+    path: '/start',
+    controller: ControllerType.Start,
+    next: [{ path: '/scrambled' }],
+    components: []
+  })
+
+  expect(result.pages).toHaveLength(4)
+  expect(result.pages.at(0)).toEqual({
+    title: 'Start',
+    path: '/start',
+    controller: ControllerType.Start,
+    next: [{ path: '/scrambled' }],
+    components: []
+  })
+})
+
+test('addPage adds a non-start pages to the last position V2', () => {
+  const v2 = {
+    engine: Engine.V2,
+    ...data
+  }
+
+  const result = addPage(v2, {
+    title: 'Last',
+    path: '/last',
+    next: [],
+    components: []
+  })
+
+  expect(result.pages).toHaveLength(4)
+  expect(result.pages.at(result.pages.length - 1)).toEqual({
+    title: 'Last',
+    path: '/last',
+    next: [],
+    components: []
+  })
+})
+
+test('addPage adds pages to the penultimate position if they is a summary page in V2', () => {
+  const result = addPage(
+    {
+      engine: Engine.V2,
+      ...data,
+      pages: [
+        ...data.pages,
+        {
+          title: 'Summary',
+          path: '/summary',
+          controller: ControllerType.Summary
+        }
+      ]
+    },
+    {
+      title: 'Penultimate',
+      path: '/penultimate',
+      next: [],
+      components: []
+    }
+  )
+
+  expect(result.pages).toHaveLength(5)
+  expect(result.pages.at(result.pages.length - 2)).toEqual({
+    title: 'Penultimate',
+    path: '/penultimate',
     next: [],
     components: []
   })
