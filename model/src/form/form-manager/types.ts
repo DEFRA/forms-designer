4bf35d09a9e67cd243c9161578053222130b6e92
Chris Cole <whitewaterdesign>
2025-02-21 23:06:59 +0000

    chore: prettier fix

--- model/src/form/form-manager/types.ts
+++ model/src/form/form-manager/types.ts
@@ -2,4 +2,6 @@ import { type Page } from '~/src/index.js'
 
 export type PatchPageFields = Partial<Pick<Page, 'title' | 'path'>>
 
-export interface AddComponentQueryOptions { prepend?: boolean }
+export interface AddComponentQueryOptions {
+  prepend?: boolean
+}
