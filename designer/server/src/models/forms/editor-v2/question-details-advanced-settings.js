73be16c639f1edd7800d087fbfed6943f5b2f933
Jez Barnsley <jbarnsley.github@gmail.com>
2025-03-17 15:25:34 +0000

    Reworked in line with review suggestions

--- designer/server/src/models/forms/editor-v2/question-details-advanced-settings.js
+++ designer/server/src/models/forms/editor-v2/question-details-advanced-settings.js
@@ -2,7 +2,7 @@ import { ComponentType } from '@defra/forms-model'
 
 import { QuestionAdvancedSettings } from '~/src/common/constants/editor.js'
 import { insertValidationErrors } from '~/src/lib/utils.js'
-import { GOVUK_LABEL__M } from '~/src/models/forms/editor-v2/common.js'
+import { allAdvancedSettingsFields } from '~/src/models/forms/editor-v2/advanced-settings-fields.js'
 
 /**
  * @param {GovukField} field
@@ -20,80 +20,6 @@ export function getFieldComponentType(field) {
         `Invalid or not implemented advanced setting field name (${field.name})`
       )
   }
-}
-
-/**
- * @param {string} fieldName
- * @param {FormEditor} formValues
- * @param { ErrorDetails | undefined } formErrors
- * @returns {GovukField}
- */
-export function buildField(fieldName, formValues, formErrors) {
-  switch (fieldName) {
-    case QuestionAdvancedSettings.MinLength:
-      return {
-        name: 'minLength',
-        id: 'minLength',
-        label: {
-          text: 'Minimum character length (optional)',
-          classes: GOVUK_LABEL__M
-        },
-        hint: {
-          text: 'The minimum number of characters users can enter'
-        },
-        value: formValues.minLength,
-        classes: 'govuk-input--width-3',
-        ...insertValidationErrors(formErrors?.minLength)
-      }
-    case QuestionAdvancedSettings.MaxLength:
-      return {
-        name: 'maxLength',
-        id: 'maxLength',
-        label: {
-          text: 'Maximum character length (optional)',
-          classes: GOVUK_LABEL__M
-        },
-        hint: {
-          text: 'The maximum number of characters users can enter'
-        },
-        value: formValues.maxLength,
-        classes: 'govuk-input--width-3',
-        ...insertValidationErrors(formErrors?.maxLength)
-      }
-    case QuestionAdvancedSettings.Regex:
-      return {
-        name: 'regex',
-        id: 'regex',
-        label: {
-          text: 'Regex (optional)',
-          classes: GOVUK_LABEL__M
-        },
-        hint: {
-          text: 'Specifies a regular expression to validate users’ inputs. Use JavaScript syntax'
-        },
-        rows: 3,
-        value: formValues.regex,
-        ...insertValidationErrors(formErrors?.regex)
-      }
-    case QuestionAdvancedSettings.Classes:
-      return {
-        name: 'classes',
-        id: 'classes',
-        label: {
-          text: 'Classes (optional)',
-          classes: GOVUK_LABEL__M
-        },
-        hint: {
-          text: 'Apply CSS classes to this field. For example, ‘govuk-input govuk-!-width-full’'
-        },
-        rows: 1,
-        value: formValues.classes,
-        ...insertValidationErrors(formErrors?.classes)
-      }
-    default:
-  }
-
-  throw new Error(`Invalid adveanced settings field type ${fieldName}`)
 }
 
 /**
@@ -115,15 +41,23 @@ function mapToQuestionOptions(question) {
  * @returns {GovukField[]}
  */
 export function advancedSettingsFields(options, question, validation) {
-  const formValues = /** @type {FormEditor} */ (
-    validation?.formValues ?? mapToQuestionOptions(question)
-  )
+  const formValues =
+    /** @type { Record<string, string | boolean | number | undefined> } */ (
+      validation?.formValues ?? mapToQuestionOptions(question)
+    )
   const formErrors = validation?.formErrors
 
-  return options.map((opt) => buildField(opt, formValues, formErrors))
+  return options.map((fieldName) => {
+    const fieldSettings = allAdvancedSettingsFields[fieldName]
+    return {
+      ...fieldSettings,
+      ...insertValidationErrors(formErrors ? formErrors[fieldName] : undefined),
+      value: formValues[fieldName]
+    }
+  })
 }
 
 /**
  * @import { FormEditor, GovukField, TextFieldComponent } from '@defra/forms-model'
- * @import { ErrorDetails, ValidationFailure } from '~/src/common/helpers/types.js'
+ * @import { ValidationFailure } from '~/src/common/helpers/types.js'
  */
