ccb2eba45da9e1f864211104509dfa31c6206715
Jez Barnsley <jbarnsley.github@gmail.com>
2025-03-27 14:37:46 +0000

    Stash

--- model/src/form/form-manager/types.ts
+++ model/src/form/form-manager/types.ts
@@ -1,6 +1,8 @@
 import { type Page } from '~/src/index.js'
 
-export type PatchPageFields = Partial<Pick<Page, 'title' | 'path'>>
+export type PatchPageFields = Partial<
+  Pick<Page, 'title' | 'path' | 'controller'>
+>
 
 export interface AddComponentQueryOptions {
   prepend?: boolean
