2270761eb0cb1d2a95ba89b1c87ed6ce8aa67385
Alex Luckett <alexgluckett@gmail.com>
2025-04-14 13:53:56 +0100

    Log headers for testing

--- designer/server/src/createServer.ts
+++ designer/server/src/createServer.ts
@@ -109,6 +109,15 @@ export async function createServer() {
 
     // if the user is accessing the old URL
     if (requestDomain !== baseDomain) {
+      Object.entries(request.headers).forEach(([key, value]) => {
+        if (
+          key.toLowerCase() !== 'authorization' &&
+          !key.toLowerCase().includes('session')
+        ) {
+          logger.debug(`Header: ${key} = ${value}`)
+        }
+      })
+
       logger.debug(
         `Request domain ${requestDomain} did not match base domain ${baseDomain}`
       )
