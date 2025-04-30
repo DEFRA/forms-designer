efc047faf3ca4cb52b856db5d848cc949d0f786a
Chris Cole <whitewaterdesign>
2025-04-30 15:54:11 +0100

    stash

--- designer/client/src/javascripts/preview/radio.test.js
+++ designer/client/src/javascripts/preview/radio.test.js
@@ -42,24 +42,70 @@ describe('radio', () => {
     ['45d67f82-9e77-49c0-a6f5-cdd32ef7b4a0', radio3]
   ])
 
-  document.body.innerHTML = questionDetailsStubPanels
+  beforeEach(() => {
+    document.body.innerHTML = questionDetailsStubPanels
+  })
+
   const questionElements = new RadioQuestionElements()
 
   describe('integration', () => {})
+
+  describe('RadioQuestionElements', () => {
+    it('should get all correct defaults', () => {
+      const radioQuestionElements = new RadioQuestionElements()
+      expect(radioQuestionElements.values).toEqual({
+        question: 'Which quest would you like to pick?',
+        hintText: 'Choose one adventure that best suits you.',
+        optional: false,
+        shortDesc: 'your quest',
+        items: [
+          {
+            id: '414d82a3-4cab-416a-bd54-6b86fbd51120',
+            text: 'Treasure Hunting',
+            value: 'Treasure Hunting'
+          },
+          {
+            id: 'e3c7dc34-a401-40e2-8836-8ec43bdee88a',
+            text: 'Rescuing the princess',
+            value: 'Rescuing the princess'
+          },
+          {
+            id: '6550cf75-2523-45c3-b119-8ca7bc27a159',
+            text: 'Saving a city',
+            value: 'Saving a city'
+          },
+          {
+            id: '6edb06e0-0d2d-4879-bac1-b58e70b8c067',
+            text: 'Defeating the baron',
+            value: 'Defeating the baron'
+          }
+        ]
+      })
+    })
+  })
+
+  describe('RadioEventListeners', () => {})
 
   describe('Radio class', () => {
     describe('addElement', () => {
       it('should add an element', () => {
         const radioClass = new Radio(questionElements)
-        radioClass.push(radio1)
+        radioClass.push(structuredClone(radio1))
         expect(radioClass.list).toEqual([radio1])
       })
+
       it('should delete an element', () => {
         const radioClass = new Radio(questionElements)
-        radioClass.push(radio1)
+        radioClass.push(structuredClone(radio1))
         expect(radioClass.list).toEqual([radio1])
         radioClass.delete(radio1Id)
         expect(radioClass.list).toEqual([])
+      })
+
+      it('should edit an element', () => {
+        const radioClass = new Radio(questionElements)
+        radioClass.push(structuredClone(radio1))
+        radioClass.update(radio1Id)
       })
     })
   })
