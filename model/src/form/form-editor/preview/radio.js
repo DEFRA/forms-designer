aee2793dbcb53738cabd2cba531e8891a43c8c60
Chris Cole <whitewaterdesign>
2025-05-13 00:24:10 +0100

    feat: 529786 - Fix build errors

--- model/src/form/form-editor/preview/radio.js
+++ model/src/form/form-editor/preview/radio.js
@@ -3,8 +3,3 @@ import { ListQuestion } from '~/src/form/form-editor/preview/list.js'
 export class RadioQuestion extends ListQuestion {
   _questionTemplate = 'radios.njk'
 }
-
-/**
- * @import { ListenerRow } from '~/src/form/form-editor/preview/types.js'
- * @import { ListElement, ListItemReadonly } from '@defra/forms-model'
- */
--- model/src/form/form-editor/preview/uk-address.js
+++ model/src/form/form-editor/preview/uk-address.js
@@ -4,7 +4,3 @@ export class UkAddressQuestion extends Question {
   _questionTemplate = 'ukaddressfield.njk'
   _fieldName = 'addressField'
 }
-
-/**
- * @import { QuestionBaseModel } from '~/src/form/form-editor/preview/types.js'
- */
