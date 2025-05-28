bcab924d056cef80b530db5bb1a61d1e7531942e
David Stone <davidjamesstone@gmail.com>
2025-05-28 18:08:32 +0100

    Add manage conditions page tests

--- designer/server/src/__stubs__/form-definition.js
+++ designer/server/src/__stubs__/form-definition.js
@@ -1,7 +1,11 @@
 import {
   ComponentType,
+  ConditionType,
   ControllerPath,
-  ControllerType
+  ControllerType,
+  Coordinator,
+  Engine,
+  OperatorName
 } from '@defra/forms-model'
 
 /**
@@ -836,6 +840,270 @@ export const testFormDefinitionWithRepeater = {
   conditions: [],
   sections: [],
   lists: []
+}
+
+/**
+ * @satisfies {FormDefinition}
+ */
+export const testFormDefinitionWithMultipleV2Conditions = {
+  name: 'Form name',
+  startPage: '/summary',
+  pages: [
+    {
+      title: 'What is your full name',
+      path: '/what-is-your-full-name',
+      components: [
+        {
+          type: ComponentType.TextField,
+          title: 'What is your full name',
+          name: 'RRwYht',
+          shortDescription: 'What is your full name',
+          hint: '',
+          options: {
+            required: true
+          },
+          schema: {},
+          id: '154271c2-79a2-4b59-b535-d210a13dbfe9'
+        }
+      ],
+      next: [],
+      id: '449c053b-9201-4312-9a75-187ac1b720eb'
+    },
+    {
+      title: 'Fave color',
+      path: '/fave-color',
+      components: [
+        {
+          type: ComponentType.RadiosField,
+          title: 'What is your favourite color',
+          name: 'nUaCCW',
+          shortDescription: 'Fave color',
+          hint: '',
+          options: {
+            required: true
+          },
+          list: 'ukcXCd',
+          id: '7bfc19cf-8d1d-47dd-926e-8363bcc761f2'
+        }
+      ],
+      next: [],
+      id: '2c3122b6-bf57-42c9-ab1b-6f67e8575703'
+    },
+    {
+      title: 'Fave animal',
+      path: '/fave-animal',
+      components: [
+        {
+          type: ComponentType.CheckboxesField,
+          title: 'What is your favourite animal',
+          name: 'nUaCCW',
+          shortDescription: 'Fave animal',
+          hint: '',
+          options: {
+            required: true
+          },
+          list: 'sdewRT',
+          id: 'f0f67bf7-cdbb-4247-9f3c-8cd919183968'
+        }
+      ],
+      next: [],
+      id: 'a86ea4ba-ae3b-4324-9acd-3a3f347cb0ec'
+    },
+    {
+      title: 'Summary',
+      path: '/summary',
+      controller: ControllerType.Summary,
+      components: [],
+      id: '0ee40b39-fcae-4012-8725-7e3f5f75f88c'
+    }
+  ],
+  conditions: [
+    {
+      name: 'd5e9f931-e151-4dd6-a2b9-68a03f3537e2',
+      displayName: 'isBobV2',
+      conditions: [
+        {
+          id: 'bd071563-1261-4e5c-ab30-05dde59b86f6',
+          componentId: '154271c2-79a2-4b59-b535-d210a13dbfe9',
+          operator: OperatorName.Is,
+          value: {
+            type: ConditionType.StringValue,
+            value: 'bob'
+          }
+        }
+      ]
+    },
+    {
+      displayName: 'isBob',
+      name: 'LoaWPy',
+      value: {
+        name: 'isBob',
+        conditions: [
+          {
+            field: {
+              name: 'RRwYht',
+              type: ComponentType.TextField,
+              display: 'What is your full name'
+            },
+            operator: OperatorName.Is,
+            value: {
+              type: ConditionType.Value,
+              value: 'Bob',
+              display: 'Bob'
+            }
+          }
+        ]
+      }
+    },
+    {
+      name: '4a82930a-b8f5-498c-adae-6158bb2aeeb5',
+      displayName: 'isFaveColourRedV2',
+      conditions: [
+        {
+          id: '7ccd81c7-6c44-4de2-9c2b-fc917b7e9f35',
+          componentId: '7bfc19cf-8d1d-47dd-926e-8363bcc761f2',
+          operator: OperatorName.Is,
+          value: {
+            type: ConditionType.StringValue,
+            value: 'red'
+          }
+        }
+      ]
+    },
+    {
+      displayName: 'isFaveColourRed',
+      name: 'SxzrgR',
+      value: {
+        name: 'isFaveColourRed',
+        conditions: [
+          {
+            field: {
+              name: 'nUaCCW',
+              type: ComponentType.RadiosField,
+              display: 'What is your favourite color'
+            },
+            operator: OperatorName.Is,
+            value: {
+              type: ConditionType.Value,
+              value: 'red',
+              display: 'Red'
+            }
+          }
+        ]
+      }
+    },
+    {
+      displayName: 'isBobAndFaveColourRed',
+      name: 'drFGth',
+      value: {
+        name: 'isBobAndFaveColourRed',
+        conditions: [
+          {
+            conditionName: 'isBob',
+            conditionDisplayName: 'isBob'
+          },
+          {
+            conditionName: 'isFaveColourRed',
+            conditionDisplayName: 'isFaveColourRed',
+            coordinator: Coordinator.AND
+          }
+        ]
+      }
+    },
+    {
+      displayName: 'isBobAndFaveColourRedV2',
+      name: '7ae768b6-1bc5-4a9b-911a-d813e5614e8e',
+      coordinator: Coordinator.AND,
+      conditions: [
+        {
+          id: 'f54fcebc-f103-451f-8356-1a08f1f32f56',
+          conditionId: 'd5e9f931-e151-4dd6-a2b9-68a03f3537e2'
+        },
+        {
+          id: 'f77a9375-c03f-4365-94b5-edbdbe9e29bd',
+          conditionId: '4a82930a-b8f5-498c-adae-6158bb2aeeb5'
+        }
+      ]
+    },
+    {
+      displayName: 'isFaveAnimalMonkey',
+      name: 'b4bcd680-b9b5-4d1e-a42f-5b5f8c91f551',
+      value: {
+        name: 'isFaveAnimalMonkey',
+        conditions: [
+          {
+            field: {
+              name: 'nUaCCW',
+              type: ComponentType.RadiosField,
+              display: 'What is your favourite animal'
+            },
+            operator: OperatorName.Contains,
+            value: {
+              type: ConditionType.Value,
+              value: 'monkey',
+              display: 'Monkey'
+            }
+          }
+        ]
+      }
+    }
+  ],
+  sections: [
+    {
+      name: 'section',
+      title: 'Section title',
+      hideTitle: false
+    }
+  ],
+  lists: [
+    {
+      name: 'ukcXCd',
+      title: 'List for question nUaCCW',
+      type: 'string',
+      items: [
+        {
+          id: 'e1d4f56e-ad92-49ea-89a8-cf0edb0480f7',
+          text: 'Red',
+          value: 'red'
+        },
+        {
+          id: '689d3f66-88f7-4dc0-b199-841b72393c19',
+          text: 'Blue',
+          value: 'blue'
+        },
+        {
+          id: '93d8b63b-4eef-4c3e-84a7-5b7edb7f9171',
+          text: 'Green',
+          value: 'green'
+        }
+      ],
+      id: '3e470333-c2aa-4bd4-bd1a-738819226a3a'
+    },
+    {
+      name: 'sdewRT',
+      title: 'Animals',
+      type: 'string',
+      items: [
+        {
+          id: 'fb3519b2-c6c7-40b6-8e03-2fb0db6d4f32',
+          text: 'Horse',
+          value: 'horse'
+        },
+        {
+          id: '0c546ae1-897e-48d0-9388-b0902fe23baf',
+          text: 'Monkey',
+          value: 'monkey'
+        },
+        {
+          id: '39f6fa65-1781-4569-9ba3-d8d13931f036',
+          text: 'Giraffe',
+          value: 'giraffe'
+        }
+      ],
+      id: '0e047f83-dbb6-4c82-b709-f9dbaddf8644'
+    }
+  ],
+  engine: Engine.V2
 }
 
 /**
--- designer/server/src/routes/forms/editor-v2/conditions.test.js
+++ designer/server/src/routes/forms/editor-v2/conditions.test.js
@@ -1,13 +1,12 @@
-import { ComponentType, ControllerType, Engine } from '@defra/forms-model'
+import { Engine } from '@defra/forms-model'
 
 import {
   buildDefinition,
-  testFormDefinitionWithSinglePage,
+  testFormDefinitionWithMultipleV2Conditions,
   testFormDefinitionWithSummaryOnly
 } from '~/src/__stubs__/form-definition.js'
 import { testFormMetadata } from '~/src/__stubs__/form-metadata.js'
 import { createServer } from '~/src/createServer.js'
-import * as editor from '~/src/lib/editor.js'
 import * as forms from '~/src/lib/forms.js'
 import { auth } from '~/test/fixtures/auth.js'
 import { renderResponse } from '~/test/helpers/component-helpers.js'
@@ -56,86 +55,28 @@ describe('Editor v2 conditions routes', () => {
     expect($noConditionsFallbackText).toHaveLength(1)
   })
 
-  test('GET - should check correct formData is rendered in the view with multiple pages', async () => {
-    const formDefinitionMultiplePages = buildDefinition({
-      ...testFormDefinitionWithSinglePage,
-      pages: [
-        {
-          id: 'p1',
-          path: '/page-one',
-          title: 'Page one',
-          section: 'section',
-          components: [
-            {
-              id: 'c1',
-              type: ComponentType.TextField,
-              name: 'textField',
-              title: 'This is your first field',
-              hint: 'Help text',
-              options: {},
-              schema: {}
-            }
-          ],
-          next: [{ path: '/page-two' }]
-        },
-        {
-          id: 'p1',
-          path: '/page-two',
-          title: 'Page two',
-          section: 'section',
-          components: [
-            {
-              id: 'c1',
-              type: ComponentType.TextField,
-              name: 'textField',
-              title: 'This is your second field',
-              hint: 'Help text',
-              options: {},
-              schema: {}
-            }
-          ],
-          next: [{ path: '/summary' }]
-        },
-        {
-          id: 'c2',
-          title: 'Summary',
-          path: '/summary',
-          controller: ControllerType.Summary
-        }
-      ],
-      engine: Engine.V2
-    })
-
+  test('GET - should check correct data is rendered in the view with multiple V2 conditions', async () => {
     jest.mocked(forms.get).mockResolvedValueOnce(testFormMetadata)
     jest
       .mocked(forms.getDraftFormDefinition)
-      .mockResolvedValueOnce(formDefinitionMultiplePages)
-    const migrateToV2 = jest.mocked(editor.migrateDefinitionToV2)
+      .mockResolvedValueOnce(testFormDefinitionWithMultipleV2Conditions)
 
     const options = {
       method: 'get',
-      url: '/library/my-form-slug/editor-v2/pages',
+      url: '/library/my-form-slug/editor-v2/conditions',
       auth
     }
 
     const { container } = await renderResponse(server, options)
 
-    expect(migrateToV2).not.toHaveBeenCalled()
-
     const $mainHeading = container.getByRole('heading', { level: 1 })
+    const $cardHeadings = container.getAllByText('All conditions')
 
-    const $pageTitles = container.getAllByRole('heading', { level: 2 })
+    expect($mainHeading).toHaveTextContent('Manage conditions')
+    expect($cardHeadings[0]).toHaveTextContent('All conditions')
 
-    const $actions = container.getAllByRole('button')
-
-    expect($mainHeading).toHaveTextContent('Add and edit pages')
-    expect($pageTitles[0]).toHaveTextContent('Page 1: Page one')
-    expect($pageTitles[1]).toHaveTextContent('Page 2: Page two')
-    expect($pageTitles[2]).toHaveTextContent('End pages')
-    expect($pageTitles[3]).toHaveTextContent('Check your answers')
-    expect($actions).toHaveLength(4)
-    expect($actions[2]).toHaveTextContent('Add new page')
-    expect($actions[3]).toHaveTextContent('Re-order pages')
+    const $rows = container.getAllByRole('row')
+    expect($rows).toHaveLength(4)
   })
 })
 
