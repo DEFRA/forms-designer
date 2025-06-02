17cc977bff852a4ccf07346ca64fc7e8ff162467
Chris Cole <whitewaterdesign>
2025-06-02 14:55:00 +0100

    chore: remove console log

--- designer/precompile-govuk-components.js
+++ designer/precompile-govuk-components.js
@@ -2,8 +2,6 @@ import { join } from 'node:path'
 import { precompileFn } from './bin/precompile.js'
 
 const inputPath = join(import.meta.dirname, 'node_modules/govuk-frontend/dist')
-console.log('~~~~~~ Chris Debug ~~~~~~ ', 'InputPath', inputPath)
-
 const outputPath = join(
   import.meta.dirname,
   'client/src/assets/nunjucks/govuk-components.js'
