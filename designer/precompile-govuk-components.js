cd54b88e833a281733491f4a49bf5366c40ccc79
Chris Cole <whitewaterdesign>
2025-06-02 14:50:50 +0100

    fix conflict

--- designer/precompile-govuk-components.js
+++ designer/precompile-govuk-components.js
@@ -1,11 +1,8 @@
 import { join } from 'node:path'
 import { precompileFn } from './bin/precompile.js'
 
-const inputPath = join(
-  import.meta.dirname,
-  '../node_modules/govuk-frontend/dist'
-)
-
+const inputPath = join(import.meta.dirname, 'node_modules/govuk-frontend/dist')
+console.log('~~~~~~ Chris Debug ~~~~~~ ', 'InputPath', inputPath)
 const outputPath = join(
   import.meta.dirname,
   'client/src/assets/nunjucks/govuk-components.js'
