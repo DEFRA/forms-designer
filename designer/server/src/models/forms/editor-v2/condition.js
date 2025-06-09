6ddb7de33b2da757f5d36c60b13876cde2d5dbd5
David Stone <davidjamesstone@gmail.com>
2025-06-09 11:41:02 +0100

    Filter condition errors

--- designer/server/src/models/forms/editor-v2/condition.js
+++ designer/server/src/models/forms/editor-v2/condition.js
@@ -64,7 +64,7 @@ export function buildValueField(
 
     case ConditionType.StringValue: {
       return {
-        id: 'value',
+        id: `items[${idx}].value`,
         name: `items[${idx}][value][value]`,
         label: {
           text: 'Enter a value'
