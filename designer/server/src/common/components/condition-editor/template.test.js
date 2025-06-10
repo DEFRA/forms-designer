20606e57df985bf5becdc519f7831f3d31042fa6
Jez Barnsley <jbarnsley.github@gmail.com>
2025-06-10 15:28:35 +0100

    Enhanced test

--- designer/server/src/common/components/condition-editor/template.test.js
+++ designer/server/src/common/components/condition-editor/template.test.js
@@ -40,7 +40,35 @@ describe('Condition editor component', () => {
                 component: {
                   id: 'items[0].componentId',
                   name: 'items[0][componentId]',
-                  value: 'f453f07e-d636-404b-8792-e2e923c8032b'
+                  value: 'f453f07e-d636-404b-8792-e2e923c8032b',
+                  items: [
+                    {
+                      components: [
+                        {
+                          type: 'RadiosField',
+                          title: 'Hair colour',
+                          name: 'CMhiZv',
+                          shortDescription: 'Hair colour',
+                          hint: '',
+                          list: '1a822117-d981-4409-b264-c45534d03ee4',
+                          options: { required: true },
+                          schema: {},
+                          id: '4d2f0222-dec1-481c-87cc-71aaa7e73379'
+                        },
+                        {
+                          type: 'RadiosField',
+                          title: 'Fave channel',
+                          name: 'nhfAXA',
+                          shortDescription: 'Fave channel',
+                          hint: '',
+                          list: 'f20442ba-587d-4467-8876-128c07dee207',
+                          options: { required: true },
+                          schema: {},
+                          id: '715e2681-ef28-4501-a7bd-dcd6986ec13b'
+                        }
+                      ]
+                    }
+                  ]
                 },
                 operator: {
                   id: 'items[0].operator',
@@ -99,7 +127,25 @@ describe('Condition editor component', () => {
         'govuk-button govuk-!-margin-bottom-0 govuk-!-margin-left-3'
       )
       expect($buttons[0]).toHaveTextContent('Select')
+
       expect($conditionlists).toHaveLength(2)
+
+      expect($conditionlists[0].innerHTML).toContain(
+        '<option value="">Select a question</option>'
+      )
+      expect($conditionlists[0].innerHTML).toContain(
+        '<option value="4d2f0222-dec1-481c-87cc-71aaa7e73379">Page : Hair colour</option>'
+      )
+      expect($conditionlists[0].innerHTML).toContain(
+        '<option value="715e2681-ef28-4501-a7bd-dcd6986ec13b">Page : Fave channel</option>'
+      )
+
+      expect($conditionlists[1].innerHTML).toContain(
+        '<option value="is" selected="">Is</option>'
+      )
+      expect($conditionlists[1].innerHTML).toContain(
+        '<option value="is not">Is not</option>'
+      )
     })
   })
 })
