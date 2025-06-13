790ec8a530e8fcf62e0608f50fa2f676eb324a68
Chris Cole <whitewaterdesign>
2025-06-13 10:41:45 +0100

    feat: 552374 - Add select field class

--- model/src/form/form-editor/preview/select.js
+++ model/src/form/form-editor/preview/select.js
@@ -2,10 +2,10 @@ import { ComponentType } from '~/src/components/enums.js'
 import { ListSortableQuestion } from '~/src/form/form-editor/preview/list-sortable.js'
 import { Question } from '~/src/index.js'
 
-export class RadioSortableQuestion extends ListSortableQuestion {
-  _questionTemplate = Question.PATH + 'radios.njk'
+export class SelectSortableQuestion extends ListSortableQuestion {
+  _questionTemplate = Question.PATH + 'selectfield.njk'
   /**
    * @type {ComponentType}
    */
-  componentType = ComponentType.RadiosField
+  componentType = ComponentType.SelectField
 }
