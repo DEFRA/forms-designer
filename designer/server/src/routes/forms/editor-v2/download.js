b8f0687b52c263ab6ca0b033e9cdc110c36932c3
Mohammed Khalid <emailmokhld@gmail.com>
2025-06-17 10:14:27 +0100

    Feature 532794 & 532743: Upload and download json (#912)
    * feat: upload and download json
    
    * test: fixed broken pages.test.js suite
    
    * refactor: sonar gripes
    
    * test: tests for upload.js route and model
    
    * test: added tests for download.js
    
    * test: increase coverage
    
    * refactor: remove unneeded split between regular and end pages
    
    * test: fixed broken test and use node Readble for reading streams
    
    * feat: added manage conditions button
    
    * refactor: adjusted margin of x pages text
    
    * test: coverage for multipart payload
    
    * feat: adjust page layout to full-width header with constrained content cards
    
    * refactor: PR review actions

---
+++ designer/server/src/routes/forms/editor-v2/download.js
@@ -0,0 +1,45 @@
+import * as scopes from '~/src/common/constants/scopes.js'
+import * as forms from '~/src/lib/forms.js'
+
+export const ROUTE_PATH_DOWNLOAD = 'download'
+export const ROUTE_FULL_PATH_DOWNLOAD = '/library/{slug}/editor-v2/download'
+
+export default [
+  /**
+   * @satisfies {ServerRoute<{ Params: { slug: string } }>}
+   */
+  ({
+    method: 'GET',
+    path: ROUTE_FULL_PATH_DOWNLOAD,
+    async handler(request, h) {
+      const { params, auth } = request
+      const { token } = auth.credentials
+      const { slug } = params
+
+      const metadata = await forms.get(slug, token)
+      const formId = metadata.id
+      const definition = await forms.getDraftFormDefinition(formId, token)
+
+      const filename = `${slug}.json`
+      const definitionJson = JSON.stringify(definition, null, 2)
+
+      return h
+        .response(definitionJson)
+        .header('Content-Type', 'application/json')
+        .header('Content-Disposition', `attachment; filename="${filename}"`)
+    },
+    options: {
+      auth: {
+        mode: 'required',
+        access: {
+          entity: 'user',
+          scope: [`+${scopes.SCOPE_READ}`]
+        }
+      }
+    }
+  })
+]
+
+/**
+ * @import { ServerRoute } from '@hapi/hapi'
+ */
