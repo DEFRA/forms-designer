f55668e240cc398bfa0522402aac945542affacd
Alex Luckett <alexluckett@users.noreply.github.com>
2025-06-24 12:53:12 +0100

    Resolve schema failures (#948)
    * Revert "A series of schema/model fixes to prevent braking of runner (#946)"
    
    This reverts commit b1a109f28e6c98a1f45a872bbfaf71e924b9a88b.
    
    * Add relative date type

--- model/src/form/form-definition/index.ts
+++ model/src/form/form-definition/index.ts
@@ -759,7 +759,7 @@ export const pagePayloadSchemaV2 = pageSchemaV2
 const baseListItemSchema = Joi.object<Item>()
   .description('Base schema for list items with common properties')
   .keys({
-    id: idSchemaOptional.description('Unique identifier for the list item'),
+    id: idSchema.description('Unique identifier for the list item'),
     text: Joi.string()
       .trim()
       .allow('')
@@ -810,18 +810,6 @@ const numberListItemSchema = baseListItemSchema
   })
   .description('List item with numeric value')
 
-const stringListItemSchemaV2 = stringListItemSchema
-  .append({
-    id: idSchema.description('Unique identifier for the list item')
-  })
-  .description('List item with string value')
-
-const numberListItemSchemaV2 = numberListItemSchema
-  .append({
-    id: idSchema.description('Unique identifier for the list item')
-  })
-  .description('List item with string value')
-
 export const listSchema = Joi.object<List>()
   .description('Reusable list of options for select components')
   .keys({
@@ -843,11 +831,13 @@ export const listSchema = Joi.object<List>()
       is: 'string',
       then: Joi.array()
         .items(stringListItemSchema)
+        .unique('id')
         .unique('text')
         .unique('value')
         .description('Array of items with string values'),
       otherwise: Joi.array()
         .items(numberListItemSchema)
+        .unique('id')
         .unique('text')
         .unique('value')
         .description('Array of items with numeric values')
@@ -859,22 +849,7 @@ export const listSchema = Joi.object<List>()
  */
 export const listSchemaV2 = listSchema
   .keys({
-    id: idSchema.description('Unique identifier for the list'),
-    items: Joi.when('type', {
-      is: 'string',
-      then: Joi.array()
-        .items(stringListItemSchemaV2)
-        .unique('id')
-        .unique('text')
-        .unique('value')
-        .description('Array of items with string values'),
-      otherwise: Joi.array()
-        .items(numberListItemSchemaV2)
-        .unique('id')
-        .unique('text')
-        .unique('value')
-        .description('Array of items with numeric values')
-    })
+    id: idSchema.description('Unique identifier for the list')
   })
   .description('List schema for V2 forms')
 
