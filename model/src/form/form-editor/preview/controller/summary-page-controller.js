0079cca255398955f43cea9ffd8f6c94660d8443
whitewater design <whitewaterdesign>
2025-07-11 11:08:32 +0100

    feat: DF-69 - Setup SummaryPageController

---
+++ model/src/form/form-editor/preview/controller/summary-page-controller.js
@@ -0,0 +1,61 @@
+import { hasFormField } from '~/src/components/helpers.js'
+import { PreviewPageControllerBase } from '~/src/form/form-editor/preview/controller/page-controller-base.js'
+import { hasComponents } from '~/src/pages/helpers.js'
+
+export class SummaryPageController extends PreviewPageControllerBase {
+  /**
+   * @type {string}
+   * @protected
+   */
+  _pageTemplate = PreviewPageControllerBase.PATH + 'summary-controller.njk'
+  /**
+   * @type {FormComponentsDef[]}
+   * @private
+   */
+  _componentDefs = []
+  /**
+   * @param {PagePreviewBaseElements} elements
+   * @param {FormDefinition} formDefinition
+   * @param {PageRenderer} renderer
+   */
+  constructor(elements, formDefinition, renderer) {
+    super(elements, renderer)
+    this._componentDefs = formDefinition.pages.flatMap((page) => {
+      if (hasComponents(page)) {
+        return page.components.filter(hasFormField)
+      }
+      return []
+    })
+  }
+
+  get componentRows() {
+    return this._componentDefs.map((component) => {
+      const summaryRowHeading = component.shortDescription
+      return {
+        key: { text: summaryRowHeading },
+        value: { text: 'lorem ipsum dolar sit amet' },
+        actions: {
+          items: [
+            { href: '#', text: 'Change', visuallyHiddenText: summaryRowHeading }
+          ]
+        }
+      }
+    })
+  }
+
+  /**
+   * @returns {{ text: string; classes: string }}
+   */
+  get pageTitle() {
+    return {
+      text: 'Check your answers before sending your form',
+      classes: ''
+    }
+  }
+}
+
+/**
+ * @import { ComponentDef, ContentComponentsDef, ListComponent, FormComponentsDef } from '~/src/components/types.js'
+ * @import { FormDefinition } from '~/src/form/form-definition/types.js'
+ * @import { PageRenderer, PagePreviewBaseElements } from '~/src/form/form-editor/preview/types.js'
+ */
---
+++ model/src/form/form-editor/preview/controller/summary-page-controller.test.js
@@ -0,0 +1,121 @@
+import {
+  buildAutoCompleteComponent,
+  buildCheckboxComponent,
+  buildDateComponent,
+  buildEmailAddressFieldComponent,
+  buildMonthYearFieldComponent,
+  buildNumberFieldComponent,
+  buildRadioComponent,
+  buildSelectFieldComponent,
+  buildTelephoneNumberFieldComponent,
+  buildTextFieldComponent,
+  buildUkAddressFieldComponent,
+  buildYesNoFieldComponent
+} from '~/src/__stubs__/components.js'
+import { buildDefinition } from '~/src/__stubs__/form-definition.js'
+import { buildQuestionPage } from '~/src/__stubs__/pages.js'
+import { PageRendererStub } from '~/src/form/form-editor/__stubs__/preview.js'
+import { PagePreviewElements } from '~/src/form/form-editor/preview/controller/page-controller-base.js'
+import { SummaryPageController } from '~/src/form/form-editor/preview/controller/summary-page-controller.js'
+
+describe('summary page controller', () => {
+  const pageRenderMock = jest.fn()
+  const renderer = new PageRendererStub(pageRenderMock)
+
+  it('should work without a declaration', () => {
+    const elements = new PagePreviewElements(undefined)
+    const definition = buildDefinition({
+      pages: [
+        buildQuestionPage({
+          components: [
+            buildTextFieldComponent({
+              title: 'What is your full name?',
+              shortDescription: 'Your full name'
+            }),
+            buildDateComponent({
+              title: 'What is your date of birth',
+              shortDescription: 'Your date of birth'
+            }),
+            buildUkAddressFieldComponent({
+              title: 'What is your address?',
+              shortDescription: 'Your address'
+            }),
+            buildEmailAddressFieldComponent({
+              title: 'What is your email address?',
+              shortDescription: 'Your email address'
+            }),
+            buildAutoCompleteComponent({
+              title: 'What is your native language?',
+              shortDescription: 'Your native language'
+            }),
+            buildSelectFieldComponent({
+              title: 'Which country do you live in?',
+              shortDescription: 'Your country'
+            }),
+            buildTelephoneNumberFieldComponent({
+              title: 'What is your phone number?',
+              shortDescription: 'Your phone number'
+            })
+          ]
+        }),
+        buildQuestionPage({
+          components: [
+            buildRadioComponent({
+              title: 'What is your favourite pizza?',
+              shortDescription: 'Your favourite pizza'
+            }),
+            buildNumberFieldComponent({
+              title: "How many pizza's would you like to order?",
+              shortDescription: 'Number of pizzas'
+            }),
+            buildCheckboxComponent({
+              title: 'Which toppings would you like?',
+              shortDescription: 'Your choice of toppings'
+            })
+          ]
+        }),
+        buildQuestionPage({
+          components: [
+            buildMonthYearFieldComponent({
+              title: 'Which month would you like your free pizza?',
+              shortDescription: 'Free pizza month'
+            }),
+            buildYesNoFieldComponent({
+              title: 'Are you ready for delivery?',
+              shortDescription: 'Your delivery confirmation'
+            })
+          ]
+        })
+      ]
+    })
+    const controller = new SummaryPageController(elements, definition, renderer)
+    const expectedActions = /** @type {string} */ (
+      (hiddenText) => ({
+        actions: {
+          items: [{ href: '#', text: 'Change', visuallyHiddenText: hiddenText }]
+        }
+      })
+    )
+    const DEFAULT_TEXT = 'lorem ipsum dolar sit amet'
+    // prettier-ignore
+    expect(controller.componentRows).toEqual([
+      { key: { text: 'Your full name' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Your full name')},
+      { key: { text: 'Your date of birth' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Your date of birth')},
+      { key: { text: 'Your address' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Your address')},
+      { key: { text: 'Your email address' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Your email address')},
+      { key: { text: 'Your native language' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Your native language')},
+      { key: { text: 'Your country' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Your country')},
+      { key: { text: 'Your phone number' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Your phone number')},
+      { key: { text: 'Your favourite pizza' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Your favourite pizza')},
+      { key: { text: 'Number of pizzas' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Number of pizzas')},
+      { key: { text: 'Your choice of toppings' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Your choice of toppings')},
+      { key: { text: 'Free pizza month' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Free pizza month')},
+      { key: { text: 'Your delivery confirmation' }, value: { text: DEFAULT_TEXT }, ...expectedActions('Your delivery confirmation') }
+    ])
+
+    expect(controller.components).toEqual([])
+    expect(controller.pageTitle.text).toBe(
+      'Check your answers before sending your form'
+    )
+  })
+})
