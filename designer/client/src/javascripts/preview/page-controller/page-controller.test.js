ed4b937a1436cb490c6a0d79b9ab75fc3da149b3
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2025-07-16 16:03:29 +0100

    fix/DF-163 preview button (#996)
    * Stash for Chris
    
    * Stash again
    
    * feat: DF-69 - Setup SummaryPageController
    
    * feat: DF-69 - Check answers settings setupPreview
    
    * stash
    
    * feat: DF-69 - SummaryPageController declaration text
    
    * feat: DF-69 - Check answers listener
    
    * test: DF-69 - Improve coverage
    
    * test: DF-69 - Fix TS error
    
    * test: DF-69 - Fix sonarlint error
    
    * fix: DF-160 - Wrap text fix for radios hint
    
    * fix: DF-157 - Use black for highlight
    
    * fix: DF-157 - Use black for highlight
    
    * fix: DF-156 - Use govuk tag green for preview panel title text
    
    * fix: DF-158 - Fix Yes No preview inline
    
    * fix: DF-163 - Hide preview button on question page
    
    * fix: DF-163 - Check preview btn is hidden
    
    ---------
    
    Co-authored-by: Jez Barnsley <jbarnsley.github@gmail.com>
    Co-authored-by: whitewater design <whitewaterdesign>

--- designer/client/src/javascripts/preview/page-controller/page-controller.test.js
+++ designer/client/src/javascripts/preview/page-controller/page-controller.test.js
@@ -85,11 +85,11 @@ describe('page-controller', () => {
     const renderer = new PageRendererStub(pageRendererCb)
     const inputEvent = new InputEvent('input', { bubbles: true })
     const changeEvent = new InputEvent('change', { bubbles: true })
-
+    const previewBtn = `<a href="http://localhost" role="button" id="preview-page" class="govuk-button  govuk-button--inverse" data-module="govuk-button" data-govuk-button-init="">Preview page</a>`
     beforeEach(() => {
       jest.clearAllMocks()
       document.body.innerHTML =
-        pageHeadingAndGuidanceHTML + questionDetailsPreviewHTML
+        previewBtn + pageHeadingAndGuidanceHTML + questionDetailsPreviewHTML
       pagePreviewElements = new PagePreviewDomElements()
       pageController = new PreviewPageController(
         components,
@@ -109,7 +109,11 @@ describe('page-controller', () => {
     })
 
     it('should instantiate', () => {
+      if (!pagePreviewElements.previewPageButton) {
+        throw new Error('Failed')
+      }
       expect(pageListeners).toBeInstanceOf(PagePreviewListeners)
+      expect(pagePreviewElements.previewPageButton.style.display).toBe('none')
     })
 
     it('should highlight guidance', () => {
