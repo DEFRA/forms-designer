bedc6180dcc4e342df6e77308da8e979ba35bd57
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2025-07-25 17:31:43 +0100

    Feat/df 266 sns audit model (#1017)
    * feat: DF-266 - Create enum and intial types for AuditEventMessageType
    
    * feat: DF-266 - Export forms audit on base export
    
    * feat: DF-266 - Remove updatedBy from FormCreatedMessageData
    
    * feat: DF-266 - Remove duplicated createdAt, createdBy
    
    * Add AuditEventMessageSchemaVersion
    
    * Forms audit schema
    
    * feat: DF-266 - Add schema to form-audit
    
    * feat: DF-266 - Add mongo audit record schema to model
    
    * feat: DF-266 - Add mongo audit record schema to model
    
    * Import bug
    
    * feat: DF-266 - Add MessageBody
    
    * feat: DF-266 - Update message types to use AuditMessage
    
    * feat: DF-266 - Extend auditRecord schema with additional fields
    
    * Consistent naming and change messageCreatedAt
    
    * feat: DF-266 - Set messageCreatedAt and recordCreatedAt defaults
    
    * feat: DF-266 - Remove defaults for messageCreatedAt, recordCreatedAt
    
    * feat: DF-266 - Add entity id to messageSchema
    
    * Add message data types
    
    * feat: DF-266 - Setup schema for form audit
    
    * chore: fix linting errors
    
    * feat: DF-266 - Setup schema for form audit
    
    * feat: DF-266 - Fix type errors
    
    * test: DF-266 - Update snap
    
    ---------
    
    Co-authored-by: whitewater design <whitewaterdesign>
    Co-authored-by: David Stone <davidjamesstone@gmail.com>

--- model/src/form/form-audit/types.ts
+++ model/src/form/form-audit/types.ts
@@ -21,13 +21,122 @@ export interface FormCreatedMessageData extends FormMessageDataBase {
   teamEmail: string
 }
 
-export interface SupportEmailChanges {
-  supportEmail: string
+export interface FormTitleChanges {
+  title: string
+}
+
+export interface FormOrganisationChanges {
+  organisation: string
+}
+
+export interface FormTeamNameChanges {
+  teamName: string
+}
+
+export interface FormTeamEmailChanges {
+  teamEmail: string
+}
+
+export interface FormSupportPhoneChanges {
+  phone: string
+}
+
+export interface FormSupportEmailChanges {
+  address: string
   responseTime: string
 }
 
-export interface SupportEmailUpdatedMessageData extends FormMessageDataBase {
-  changes: ChangesMessageData<SupportEmailChanges>
+export interface FormSupportOnlineChanges {
+  url: string
+  text: string
+}
+
+export interface FormPrivacyNoticeChanges {
+  privacyNoticeUrl: string
+}
+
+export interface FormNotificationEmailChanges {
+  notificationEmail: string
+}
+
+export interface FormSubmissionGuidanceChanges {
+  submissionGuidance: string
+}
+
+export interface FormUploadedChanges {
+  value: string
+}
+
+export interface FormTitleUpdatedMessageData extends FormMessageDataBase {
+  changes: ChangesMessageData<FormTitleChanges>
+}
+
+export interface FormOrganisationUpdatedMessageData
+  extends FormMessageDataBase {
+  changes: ChangesMessageData<FormOrganisationChanges>
+}
+
+export interface FormTeamNameUpdatedMessageData extends FormMessageDataBase {
+  changes: ChangesMessageData<FormTeamNameChanges>
+}
+
+export interface FormTeamEmailUpdatedMessageData extends FormMessageDataBase {
+  changes: ChangesMessageData<FormTeamEmailChanges>
+}
+
+export interface FormSupportPhoneUpdatedMessageData
+  extends FormMessageDataBase {
+  changes: ChangesMessageData<FormSupportPhoneChanges>
+}
+
+export interface FormSupportEmailUpdatedMessageData
+  extends FormMessageDataBase {
+  changes: ChangesMessageData<FormSupportEmailChanges>
+}
+
+export interface FormSupportOnlineUpdatedMessageData
+  extends FormMessageDataBase {
+  changes: ChangesMessageData<FormSupportOnlineChanges>
+}
+
+export interface FormPrivacyNoticeUpdatedMessageData
+  extends FormMessageDataBase {
+  changes: ChangesMessageData<FormPrivacyNoticeChanges>
+}
+
+export interface FormNotificationEmailUpdatedMessageData
+  extends FormMessageDataBase {
+  changes: ChangesMessageData<FormNotificationEmailChanges>
+}
+
+export interface FormSubmissionGuidanceUpdatedMessageData
+  extends FormMessageDataBase {
+  changes: ChangesMessageData<FormSubmissionGuidanceChanges>
+}
+
+export interface FormUploadedMessageData extends FormMessageDataBase {
+  changes: ChangesMessageData<FormUploadedChanges>
+}
+
+export type FormMessageChangesData =
+  | FormTitleUpdatedMessageData
+  | FormOrganisationUpdatedMessageData
+  | FormTeamNameUpdatedMessageData
+  | FormTeamEmailUpdatedMessageData
+  | FormSupportPhoneUpdatedMessageData
+  | FormSupportEmailUpdatedMessageData
+  | FormSupportOnlineUpdatedMessageData
+  | FormPrivacyNoticeUpdatedMessageData
+  | FormNotificationEmailUpdatedMessageData
+  | FormSubmissionGuidanceUpdatedMessageData
+  | FormUploadedMessageData
+
+export type FormMessageActivitiesData =
+  | FormCreatedMessageData
+  | FormUpdatedMessageData
+
+export interface FormUpdatedMessageData extends FormMessageDataBase {
+  description: string
 }
 
 export interface AuditUser {
@@ -35,17 +144,16 @@ export interface AuditUser {
   displayName: string
 }
 
-export type MessageData =
-  | FormCreatedMessageData
-  | SupportEmailUpdatedMessageData
+export type MessageData = FormMessageChangesData | FormMessageActivitiesData
 
 export interface MessageBase {
   schemaVersion: AuditEventMessageSchemaVersion
   category: AuditEventMessageCategory
   type: AuditEventMessageType
+  entityId: string
   createdAt: Date
   createdBy: AuditUser
-  data: MessageData
+  data?: MessageData
   messageCreatedAt: Date
 }
 
@@ -55,13 +163,129 @@ export interface FormCreatedMessage extends MessageBase {
   data: FormCreatedMessageData
 }
 
+export interface FormTitleUpdatedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_TITLE_UPDATED
+  data: FormTitleUpdatedMessageData
+}
+
+export interface FormOrganisationUpdatedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_ORGANISATION_UPDATED
+  data: FormOrganisationUpdatedMessageData
+}
+
+export interface FormTeamNameUpdatedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_TEAM_NAME_UPDATED
+  data: FormTeamNameUpdatedMessageData
+}
+
+export interface FormTeamEmailUpdatedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_TEAM_EMAIL_UPDATED
+  data: FormTeamEmailUpdatedMessageData
+}
+
+export interface FormSupportPhoneUpdatedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_SUPPORT_PHONE_UPDATED
+  data: FormSupportPhoneUpdatedMessageData
+}
+
 export interface FormSupportEmailUpdatedMessage extends MessageBase {
   category: AuditEventMessageCategory.FORM
   type: AuditEventMessageType.FORM_SUPPORT_EMAIL_UPDATED
-  data: SupportEmailUpdatedMessageData
+  data: FormSupportEmailUpdatedMessageData
 }
 
-export type AuditMessage = FormCreatedMessage | FormSupportEmailUpdatedMessage
+export interface FormSupportOnlineUpdatedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_SUPPORT_ONLINE_UPDATED
+  data: FormSupportOnlineUpdatedMessageData
+}
+
+export interface FormSupportPrivacyNoticeUpdatedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_PRIVACY_NOTICE_UPDATED
+  data: FormPrivacyNoticeUpdatedMessageData
+}
+
+export interface FormSupportNotificationEmailUpdatedMessage
+  extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_NOTIFICATION_EMAIL_UPDATED
+  data: FormNotificationEmailUpdatedMessageData
+}
+
+export interface FormSupportSubmissionGuidanceUpdatedMessage
+  extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_SUBMISSION_GUIDANCE_UPDATED
+  data: FormSubmissionGuidanceUpdatedMessageData
+}
+
+export interface FormUploadedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_JSON_UPLOADED
+  data: FormUploadedMessageData
+}
+
+export interface FormDownloadedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_JSON_DOWNLOADED
+}
+
+export interface FormPublishedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_PUBLISHED
+}
+
+export interface FormDraftCreatedFromLiveMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_DRAFT_CREATED_FROM_LIVE
+}
+
+export interface FormLiveCreatedFromDraftMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_LIVE_CREATED_FROM_DRAFT
+}
+
+export interface FormDraftDeletedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_DRAFT_DELETED
+}
+
+export interface FormMigratedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_MIGRATED
+}
+
+export interface FormUpdatedMessage extends MessageBase {
+  category: AuditEventMessageCategory.FORM
+  type: AuditEventMessageType.FORM_UPDATED
+}
+
+export type AuditMessage =
+  | FormCreatedMessage
+  | FormTitleUpdatedMessage
+  | FormOrganisationUpdatedMessage
+  | FormTeamNameUpdatedMessage
+  | FormTeamEmailUpdatedMessage
+  | FormSupportPhoneUpdatedMessage
+  | FormSupportEmailUpdatedMessage
+  | FormSupportOnlineUpdatedMessage
+  | FormSupportPrivacyNoticeUpdatedMessage
+  | FormSupportNotificationEmailUpdatedMessage
+  | FormSupportSubmissionGuidanceUpdatedMessage
+  | FormUploadedMessage
+  | FormDownloadedMessage
+  | FormPublishedMessage
+  | FormDraftCreatedFromLiveMessage
+  | FormLiveCreatedFromDraftMessage
+  | FormDraftDeletedMessage
+  | FormMigratedMessage
+  | FormUpdatedMessage
 
 export interface AuditEvent {
   message: AuditMessage
@@ -69,7 +293,6 @@ export interface AuditEvent {
 
 export type AuditRecord = AuditMessage & {
   messageId: string
-  entityId: string
   recordCreatedAt: Date
 }
 
