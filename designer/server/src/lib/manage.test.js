5743ec486f4adb28dcd6496e1bbaf66707cc9f4d
Jez Barnsley <jbarnsley.github@gmail.com>
2025-07-28 12:12:38 +0100

    Extra test

--- designer/server/src/lib/manage.test.js
+++ designer/server/src/lib/manage.test.js
@@ -1,14 +1,36 @@
 import config from '~/src/config.js'
 import { createMockResponse } from '~/src/lib/__stubs__/editor.js'
-import { postJson } from '~/src/lib/fetch.js'
-import { addUser } from '~/src/lib/manage.js'
+import { getJson, postJson } from '~/src/lib/fetch.js'
+import { addUser, getRoles } from '~/src/lib/manage.js'
 
 jest.mock('~/src/lib/fetch.js')
 
 const usersEndpoint = new URL('/users/', config.entitlementUrl)
 const token = 'someToken'
 
+const rolesList = [
+  { name: 'Admin', code: 'admin', description: 'admin desc' },
+  {
+    name: 'Form creator',
+    code: 'form-creator',
+    description: 'form creator desc'
+  }
+]
+
 describe('manage.js', () => {
+  describe('getRoles', () => {
+    it('should get the list of roles', async () => {
+      jest.mocked(getJson).mockResolvedValueOnce({
+        response: createMockResponse(),
+        body: {
+          roles: rolesList
+        }
+      })
+      const result = await getRoles(token)
+      expect(result).toEqual(rolesList)
+    })
+  })
+
   describe('addUser', () => {
     it('should add the user', async () => {
       jest.mocked(postJson).mockResolvedValueOnce({
