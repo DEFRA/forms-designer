447be1b5335b7484f075b476577d448b97ab6524
whitewater design <whitewaterdesign>
2025-08-14 11:25:15 +0100

    test: DF-366 - More Sonar fixes

--- model/src/form/form-audit/__snapshots__/index.snap.js
+++ model/src/form/form-audit/__snapshots__/index.snap.js
@@ -1,4 +1,8 @@
-// Jest Snapshot v1, https://goo.gl/fbAQLP
+import {
+  AuditEventMessageCategory,
+  AuditEventMessageSchemaVersion,
+  AuditEventMessageSource
+} from '~/src/form/form-audit/enums.js'
 
 const CREATED_BY = {
   displayName: 'Enrique Chase',
@@ -7,9 +11,10 @@ const CREATED_BY = {
 
 const CREATED_AT = new Date('2025-07-26T00:00:00.000Z')
 const FORM_ID = '689b7ab1d0eeac9711a7fb33'
+const SLUG = 'audit-form'
 
 export const FORM_ORGANISATION_UPDATED = {
-  category: 'FORM',
+  category: AuditEventMessageCategory.FORM,
   createdAt: CREATED_AT,
   createdBy: CREATED_BY,
   data: {
@@ -22,62 +27,62 @@ export const FORM_ORGANISATION_UPDATED = {
       }
     },
     formId: FORM_ID,
-    slug: 'audit-form'
+    slug: SLUG
   },
   entityId: FORM_ID,
   messageCreatedAt: new Date('2025-07-26T00:00:00.000Z'),
-  schemaVersion: 1,
-  source: 'FORMS_MANAGER',
+  schemaVersion: AuditEventMessageSchemaVersion.V1,
+  source: AuditEventMessageSource.FORMS_MANAGER,
   type: 'FORM_ORGANISATION_UPDATED'
 }
 
 export const FORM_DRAFT_CREATED_FROM_LIVE = {
-  category: 'FORM',
+  category: AuditEventMessageCategory.FORM,
   createdAt: new Date('2025-07-24T00:00:00.000Z'),
   createdBy: CREATED_BY,
   entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
-  schemaVersion: 1,
-  source: 'FORMS_MANAGER',
+  schemaVersion: AuditEventMessageSchemaVersion.V1,
+  source: AuditEventMessageSource.FORMS_MANAGER,
   type: 'FORM_DRAFT_CREATED_FROM_LIVE'
 }
 
 export const FORM_CREATED = {
-  category: 'FORM',
+  category: AuditEventMessageCategory.FORM,
   createdAt: new Date('2025-07-23T00:00:00.000Z'),
   createdBy: CREATED_BY,
   data: {
     formId: FORM_ID,
     organisation: 'Defra',
-    slug: 'audit-form',
+    slug: SLUG,
     teamEmail: 'forms@example.uk',
     teamName: 'Forms',
     title: 'My Audit Event Form'
   },
   entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
-  schemaVersion: 1,
-  source: 'FORMS_MANAGER',
+  schemaVersion: AuditEventMessageSchemaVersion.V1,
+  source: AuditEventMessageSource.FORMS_MANAGER,
   type: 'FORM_CREATED'
 }
 
 export const FORM_DRAFT_DELETED = {
-  category: 'FORM',
+  category: AuditEventMessageCategory.FORM,
   createdAt: new Date('2025-08-13T15:26:02.054Z'),
   createdBy: CREATED_BY,
   data: {
     formId: FORM_ID,
-    slug: 'audit-form'
+    slug: SLUG
   },
   entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
-  schemaVersion: 1,
-  source: 'FORMS_MANAGER',
+  schemaVersion: AuditEventMessageSchemaVersion.V1,
+  source: AuditEventMessageSource.FORMS_MANAGER,
   type: 'FORM_DRAFT_DELETED'
 }
 
 export const REPLACE_DRAFT = {
-  category: 'FORM',
+  category: AuditEventMessageCategory.FORM,
   createdAt: new Date('2020-01-01T00:00:00.000Z'),
   createdBy: CREATED_BY,
   data: {
@@ -89,28 +94,28 @@ export const REPLACE_DRAFT = {
       filename: '6883d8667a2a64da10af4312.json',
       s3Key: 'audit-definitions/6883d8667a2a64da10af4312.json'
     },
-    slug: 'test-form'
+    slug: SLUG
   },
   entityId: '661e4ca5039739ef2902b214',
   messageCreatedAt: new Date('2025-08-14'),
-  schemaVersion: 1,
-  source: 'FORMS_MANAGER',
+  schemaVersion: AuditEventMessageSchemaVersion.V1,
+  source: AuditEventMessageSource.FORMS_MANAGER,
   type: 'FORM_UPDATED'
 }
 
 export const FORM_MIGRATED = {
-  category: 'FORM',
+  category: AuditEventMessageCategory.FORM,
   createdAt: new Date('2025-07-24T00:00:00.000Z'),
   createdBy: CREATED_BY,
   entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
-  schemaVersion: 1,
-  source: 'FORMS_MANAGER',
+  schemaVersion: AuditEventMessageSchemaVersion.V1,
+  source: AuditEventMessageSource.FORMS_MANAGER,
   type: 'FORM_MIGRATED'
 }
 
 export const FORM_TITLE_UPDATED = {
-  category: 'FORM',
+  category: AuditEventMessageCategory.FORM,
   createdAt: new Date('2025-07-24T00:00:00.000Z'),
   createdBy: CREATED_BY,
   data: {
@@ -123,17 +128,17 @@ export const FORM_TITLE_UPDATED = {
       }
     },
     formId: FORM_ID,
-    slug: 'audit-form'
+    slug: SLUG
   },
   entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
-  schemaVersion: 1,
-  source: 'FORMS_MANAGER',
+  schemaVersion: AuditEventMessageSchemaVersion.V1,
+  source: AuditEventMessageSource.FORMS_MANAGER,
   type: 'FORM_TITLE_UPDATED'
 }
 
 export const FORM_UPDATED = {
-  category: 'FORM',
+  category: AuditEventMessageCategory.FORM,
   createdAt: CREATED_AT,
   createdBy: CREATED_BY,
   data: {
@@ -147,22 +152,22 @@ export const FORM_UPDATED = {
     },
     requestType: 'CREATE_PAGE',
     s3Meta: undefined,
-    slug: 'test-form'
+    slug: SLUG
   },
   entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
-  schemaVersion: 1,
-  source: 'FORMS_MANAGER',
+  schemaVersion: AuditEventMessageSchemaVersion.V1,
+  source: AuditEventMessageSource.FORMS_MANAGER,
   type: 'FORM_UPDATED'
 }
 
 export const FORM_LIVE_CREATED_FROM_DRAFT = {
-  category: 'FORM',
+  category: AuditEventMessageCategory.FORM,
   createdAt: CREATED_AT,
   createdBy: CREATED_BY,
   entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
-  schemaVersion: 1,
-  source: 'FORMS_MANAGER',
+  schemaVersion: AuditEventMessageSchemaVersion.V1,
+  source: AuditEventMessageSource.FORMS_MANAGER,
   type: 'FORM_LIVE_CREATED_FROM_DRAFT'
 }
