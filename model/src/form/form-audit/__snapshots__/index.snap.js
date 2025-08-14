348bedbc633250203a15377e9f307cd3f22c3a2e
whitewater design <whitewaterdesign>
2025-08-14 11:12:47 +0100

    test: DF-366 - Sonar fix

--- model/src/form/form-audit/__snapshots__/index.snap.js
+++ model/src/form/form-audit/__snapshots__/index.snap.js
@@ -1,12 +1,17 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
+const CREATED_BY = {
+  displayName: 'Enrique Chase',
+  id: 'f50ceeed-b7a4-47cf-a498-094efc99f8bc'
+}
+
+const CREATED_AT = new Date('2025-07-26T00:00:00.000Z')
+const FORM_ID = '689b7ab1d0eeac9711a7fb33'
+
 export const FORM_ORGANISATION_UPDATED = {
   category: 'FORM',
-  createdAt: new Date('2025-07-26T00:00:00.000Z'),
-  createdBy: {
-    displayName: 'Enrique Chase',
-    id: 'f50ceeed-b7a4-47cf-a498-094efc99f8bc'
-  },
+  createdAt: CREATED_AT,
+  createdBy: CREATED_BY,
   data: {
     changes: {
       new: {
@@ -16,10 +21,10 @@ export const FORM_ORGANISATION_UPDATED = {
         organisation: 'Natural England'
       }
     },
-    formId: '689b7ab1d0eeac9711a7fb33',
+    formId: FORM_ID,
     slug: 'audit-form'
   },
-  entityId: '689b7ab1d0eeac9711a7fb33',
+  entityId: FORM_ID,
   messageCreatedAt: new Date('2025-07-26T00:00:00.000Z'),
   schemaVersion: 1,
   source: 'FORMS_MANAGER',
@@ -29,11 +34,8 @@ export const FORM_ORGANISATION_UPDATED = {
 export const FORM_DRAFT_CREATED_FROM_LIVE = {
   category: 'FORM',
   createdAt: new Date('2025-07-24T00:00:00.000Z'),
-  createdBy: {
-    displayName: 'Gandalf',
-    id: 'a53b4360-bdf6-4d13-8975-25032ce76312'
-  },
-  entityId: '689b7ab1d0eeac9711a7fb33',
+  createdBy: CREATED_BY,
+  entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
   schemaVersion: 1,
   source: 'FORMS_MANAGER',
@@ -43,19 +45,16 @@ export const FORM_DRAFT_CREATED_FROM_LIVE = {
 export const FORM_CREATED = {
   category: 'FORM',
   createdAt: new Date('2025-07-23T00:00:00.000Z'),
-  createdBy: {
-    displayName: 'Enrique Chase',
-    id: '83f09a7d-c80c-4e15-bcf3-641559c7b8a7'
-  },
+  createdBy: CREATED_BY,
   data: {
-    formId: '689b7ab1d0eeac9711a7fb33',
+    formId: FORM_ID,
     organisation: 'Defra',
     slug: 'audit-form',
     teamEmail: 'forms@example.uk',
     teamName: 'Forms',
     title: 'My Audit Event Form'
   },
-  entityId: '689b7ab1d0eeac9711a7fb33',
+  entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
   schemaVersion: 1,
   source: 'FORMS_MANAGER',
@@ -65,15 +64,12 @@ export const FORM_CREATED = {
 export const FORM_DRAFT_DELETED = {
   category: 'FORM',
   createdAt: new Date('2025-08-13T15:26:02.054Z'),
-  createdBy: {
-    displayName: 'Enrique Chase',
-    id: 'f50ceeed-b7a4-47cf-a498-094efc99f8bc'
-  },
+  createdBy: CREATED_BY,
   data: {
-    formId: '689b7ab1d0eeac9711a7fb33',
+    formId: FORM_ID,
     slug: 'audit-form'
   },
-  entityId: '689b7ab1d0eeac9711a7fb33',
+  entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
   schemaVersion: 1,
   source: 'FORMS_MANAGER',
@@ -83,10 +79,7 @@ export const FORM_DRAFT_DELETED = {
 export const REPLACE_DRAFT = {
   category: 'FORM',
   createdAt: new Date('2020-01-01T00:00:00.000Z'),
-  createdBy: {
-    displayName: 'Enrique Chase',
-    id: 'f50ceeed-b7a4-47cf-a498-094efc99f8bc'
-  },
+  createdBy: CREATED_BY,
   data: {
     formId: '661e4ca5039739ef2902b214',
     payload: undefined,
@@ -108,11 +101,8 @@ export const REPLACE_DRAFT = {
 export const FORM_MIGRATED = {
   category: 'FORM',
   createdAt: new Date('2025-07-24T00:00:00.000Z'),
-  createdBy: {
-    displayName: 'Gandalf',
-    id: 'a53b4360-bdf6-4d13-8975-25032ce76312'
-  },
-  entityId: '689b7ab1d0eeac9711a7fb33',
+  createdBy: CREATED_BY,
+  entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
   schemaVersion: 1,
   source: 'FORMS_MANAGER',
@@ -122,10 +112,7 @@ export const FORM_MIGRATED = {
 export const FORM_TITLE_UPDATED = {
   category: 'FORM',
   createdAt: new Date('2025-07-24T00:00:00.000Z'),
-  createdBy: {
-    displayName: 'Gandalf',
-    id: 'a53b4360-bdf6-4d13-8975-25032ce76312'
-  },
+  createdBy: CREATED_BY,
   data: {
     changes: {
       new: {
@@ -135,10 +122,10 @@ export const FORM_TITLE_UPDATED = {
         title: 'Old form title'
       }
     },
-    formId: '689b7ab1d0eeac9711a7fb33',
+    formId: FORM_ID,
     slug: 'audit-form'
   },
-  entityId: '689b7ab1d0eeac9711a7fb33',
+  entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
   schemaVersion: 1,
   source: 'FORMS_MANAGER',
@@ -147,13 +134,10 @@ export const FORM_TITLE_UPDATED = {
 
 export const FORM_UPDATED = {
   category: 'FORM',
-  createdAt: new Date('2020-01-01T00:00:00.000Z'),
-  createdBy: {
-    displayName: 'Enrique Chase',
-    id: 'f50ceeed-b7a4-47cf-a498-094efc99f8bc'
-  },
+  createdAt: CREATED_AT,
+  createdBy: CREATED_BY,
   data: {
-    formId: '661e4ca5039739ef2902b214',
+    formId: FORM_ID,
     payload: {
       components: [],
       id: 'ffefd409-f3f4-49fe-882e-6e89f44631b1',
@@ -165,7 +149,7 @@ export const FORM_UPDATED = {
     s3Meta: undefined,
     slug: 'test-form'
   },
-  entityId: '661e4ca5039739ef2902b214',
+  entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
   schemaVersion: 1,
   source: 'FORMS_MANAGER',
@@ -174,12 +158,9 @@ export const FORM_UPDATED = {
 
 export const FORM_LIVE_CREATED_FROM_DRAFT = {
   category: 'FORM',
-  createdAt: new Date('2025-07-24T00:00:00.000Z'),
-  createdBy: {
-    displayName: 'Gandalf',
-    id: 'a53b4360-bdf6-4d13-8975-25032ce76312'
-  },
-  entityId: '689b7ab1d0eeac9711a7fb33',
+  createdAt: CREATED_AT,
+  createdBy: CREATED_BY,
+  entityId: FORM_ID,
   messageCreatedAt: new Date('2025-08-14'),
   schemaVersion: 1,
   source: 'FORMS_MANAGER',
