ba9f09cecf78ed03a7bd11033d07f30cee505647
Jez Barnsley <jbarnsley.github@gmail.com>
2025-10-01 16:04:18 +0100

    Dummy commit

--- designer/server/src/models/forms/editor-v2/pages.test.js
+++ designer/server/src/models/forms/editor-v2/pages.test.js
@@ -71,18 +71,22 @@ describe('editor-v2 - pages model', () => {
         testFormDefinitionWithTwoPagesAndQuestions,
         testFormDefinitionWithTwoPagesAndQuestions.pages[0]
       )
+
       const resPageTwoQuestions = mapQuestionRows(
         testFormDefinitionWithTwoPagesAndQuestions,
         testFormDefinitionWithTwoPagesAndQuestions.pages[1]
       )
+
       const resPageSummaryQuestions = mapQuestionRows(
         testFormDefinitionWithTwoPagesAndQuestions,
         testFormDefinitionWithTwoPagesAndQuestions.pages[2]
       )
+
       const resPageSummaryExistingMarkdown = mapQuestionRows(
         testFormDefinitionWithExistingSummaryDeclaration,
         testFormDefinitionWithExistingSummaryDeclaration.pages[1]
       )
+
       const resPageSummaryRepeater = mapQuestionRows(
         testFormDefinitionWithRepeater,
         testFormDefinitionWithRepeater.pages[0]
