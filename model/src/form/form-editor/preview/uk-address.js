736ec4d799c706e3f0363db43d7525ac30c5e98d
Mohammed Khalid <emailmokhld@gmail.com>
2025-10-15 12:25:16 +0100

    chore: progress commit

--- model/src/form/form-editor/preview/uk-address.js
+++ model/src/form/form-editor/preview/uk-address.js
@@ -1,6 +1,31 @@
 import { ComponentType } from '~/src/components/enums.js'
 import { FieldsetQuestion } from '~/src/form/form-editor/preview/fieldset-question.js'
 import { PreviewComponent } from '~/src/form/form-editor/preview/preview.js'
+import { QuestionComponentElements } from '~/src/form/form-editor/preview/question.js'
+
+/**
+ * @implements {QuestionElements}
+ */
+export class UkAddressComponentPreviewElements extends QuestionComponentElements {
+  /**
+   * @param {FormComponentsDef & { options: { usePostcodeLookup: boolean } }} component
+   */
+  constructor(component) {
+    super(component)
+    this._usePostcodeLookup = component.options.usePostcodeLookup
+  }
+
+  /**
+   * @protected
+   * @returns {BaseSettings}
+   */
+  _getValues() {
+    return {
+      ...super._getValues(),
+      usePostcodeLookup: this._usePostcodeLookup
+    }
+  }
+}
 
 export class UkAddressQuestion extends FieldsetQuestion {
   /**
@@ -9,8 +34,14 @@ export class UkAddressQuestion extends FieldsetQuestion {
   componentType = ComponentType.UkAddressField
   _questionTemplate = PreviewComponent.PATH + 'ukaddressfield.njk'
   _fieldName = 'addressField'
+  _usePostcodeLookup = false
+
+  get usePostcodeLookup() {
+    return this._usePostcodeLookup
+  }
 }
 
 /**
- * @import { QuestionBaseModel } from '~/src/form/form-editor/preview/types.js'
+ * @import { BaseSettings, QuestionElements } from '~/src/form/form-editor/preview/types.js'
+ * @import { FormComponentsDef } from '~/src/components/types.js'
  */
