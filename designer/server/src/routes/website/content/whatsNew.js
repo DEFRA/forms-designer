fd117f554d4756bd0ea0f2e3200ca274ff677ca3
daniel-defra <daniel.dasilveira@defra.gov.uk>
2025-10-24 17:18:27 +0100

    Added a new section in What's new for October's release

--- designer/server/src/routes/website/content/whatsNew.js
+++ designer/server/src/routes/website/content/whatsNew.js
@@ -10,20 +10,41 @@ const Product = {
 
 export const whatsNewLatest = [
   {
+    when: 'October 2025',
+    updates: [
+      {
+        product: Product.FORM_DESIGNER,
+        content: `Launch of the Defra Forms microsite. https://forms.defra.gov.uk has been launched to:
+- describe our offering
+- showcase the features available in our Form Designer application and developer plugin
+- provide a step-by-step guide to getting started with Defra Forms and making a form live
+- offer a collection of resources to support form creation
+        
+Confirmation emails for form submissions:
+- members of the public can now request confirmation emails when submitting a form.
+- this feature is automatically enabled for all newly created forms and is available as an opt-in for existing forms.
+- we'll be rolling this out to all forms soon. If you'd like this enabled for an existing form as a priority, please reach out to us.
+- improved error messaging:
+- we've enhanced the error messages shown when deleting questions, pages, or list items that are referenced in conditions.
+        `
+      }
+    ]
+  },
+  {
     when: 'July 2025',
     updates: [
       {
         product: Product.FORM_DESIGNER,
         content: `Final updates for conditions:
-- Delete Conditions
-- Create and Manage Combined Conditions
-- Apply Conditions to a Guidance Page
-- View the Conditions Listing Page
+- delete conditions
+- create and manage combined conditions
+- apply conditions to a guidance page
+- view the conditions listing page
 
 Enhancements to Interactive Preview Panel:
-- Better keyboard navigation using skip links
-- Termination Pages
-- Fixes for defects like lost repeater settings and hint text`
+- better keyboard navigation using skip links
+- termination pages
+- fixes for defects like lost repeater settings and hint text`
       },
       {
         product: Product.DEVELOPER_PLUGIN,
@@ -40,15 +61,15 @@ Enhancements to Interactive Preview Panel:
       {
         product: Product.FORM_DESIGNER,
         content: `Final updates for conditions:
-- Delete Conditions
-- Create and Manage Combined Conditions
-- Apply Conditions to a Guidance Page
-- View the Conditions Listing Page
+- delete conditions
+- create and manage combined conditions
+- apply conditions to a guidance page
+- view the conditions listing page
 
 Enhancements to Interactive Preview Panel:
-- Better keyboard navigation using skip links
-- Termination Pages
-- Fixes for defects like lost repeater settings and hint text`
+- better keyboard navigation using skip links
+- termination pages
+- fixes for defects like lost repeater settings and hint text`
       }
     ]
   },
@@ -60,9 +81,9 @@ Enhancements to Interactive Preview Panel:
         content: `Service moved to new domain https://forms.defra.gov.uk/ to improve access for colleagues across different ALBs.
 
 No disruption expected:
-- Forms and files remain
-- System functions normally
-- Old links will be automatically redirected`
+- forms and files remain
+- system functions normally
+- old links will be automatically redirected`
       }
     ]
   }
