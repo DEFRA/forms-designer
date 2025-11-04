1b9c437f5f3a2e29b123008c5b546f6498b5fa6e
Alex Luckett <alexluckett@users.noreply.github.com>
2025-11-04 10:02:13 +0000

    Disable location question type (#1186)
    * Disable location question type
    
    * Fixed unit test
    
    ---------
    
    Co-authored-by: Jez Barnsley <jbarnsley.github@gmail.com>

--- designer/server/src/routes/forms/editor-v2/question-type.test.js
+++ designer/server/src/routes/forms/editor-v2/question-type.test.js
@@ -104,7 +104,7 @@ describe('Editor v2 question routes', () => {
     expect($actions).toHaveLength(3)
     expect($actions[2]).toHaveTextContent('Save and continue')
 
-    expect($radios).toHaveLength(18)
+    expect($radios).toHaveLength(17)
     expect($radios[0]).toHaveAccessibleName('Written answer')
     expect($radios[1]).toHaveAccessibleName('Short answer (a single line)')
     expect($radios[2]).toHaveAccessibleName(
@@ -114,19 +114,18 @@ describe('Editor v2 question routes', () => {
     expect($radios[4]).toHaveAccessibleName('Date')
     expect($radios[5]).toHaveAccessibleName('Day, month and year')
     expect($radios[6]).toHaveAccessibleName('Month and year')
-    expect($radios[7]).toHaveAccessibleName('Location')
-    expect($radios[8]).toHaveAccessibleName('UK address')
-    expect($radios[9]).toHaveAccessibleName('Phone number')
-    expect($radios[10]).toHaveAccessibleName('Supporting evidence')
-    expect($radios[11]).toHaveAccessibleName('Email address')
-    expect($radios[12]).toHaveAccessibleName(
+    expect($radios[7]).toHaveAccessibleName('UK address')
+    expect($radios[8]).toHaveAccessibleName('Phone number')
+    expect($radios[9]).toHaveAccessibleName('Supporting evidence')
+    expect($radios[10]).toHaveAccessibleName('Email address')
+    expect($radios[11]).toHaveAccessibleName(
       'A list of options that users can choose from'
     )
-    expect($radios[13]).toHaveAccessibleName('Yes or No')
-    expect($radios[14]).toHaveAccessibleName('Checkboxes')
-    expect($radios[15]).toHaveAccessibleName('Radios')
-    expect($radios[16]).toHaveAccessibleName('Autocomplete')
-    expect($radios[17]).toHaveAccessibleName('Select')
+    expect($radios[12]).toHaveAccessibleName('Yes or No')
+    expect($radios[13]).toHaveAccessibleName('Checkboxes')
+    expect($radios[14]).toHaveAccessibleName('Radios')
+    expect($radios[15]).toHaveAccessibleName('Autocomplete')
+    expect($radios[16]).toHaveAccessibleName('Select')
   })
 
   test('POST - should error if missing mandatory fields', async () => {
