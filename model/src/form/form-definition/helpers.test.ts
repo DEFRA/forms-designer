394fe0d85254cdb250c16c187fd068cb302cc6c5
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2025-11-06 09:48:53 +0000

    feat: DF-254 - Referential integrity on list items for conditions (#1153)
    * feat: DF-254 - Referential integrity on list items for conditions
    
    * feat: DF-559 - Improve error message
    
    * Return errorCode and errorType in the local context
    
    * Use isFormDefinition helper
    
    * Update list condition itemId custom validator and tests
    
    * Add test to listItemIdValidator
    
    * Update form definition type helpers
    
    * Use isConditionListItemRefValueDataV2 type guard
    
    * Update name to avoid collision
    
    * Update test var name
    
    * Add comment regarding custom error types
    
    ---------
    
    Co-authored-by: whitewater design <whitewaterdesign>
    Co-authored-by: David Stone <davidjamesstone@gmail.com>

--- model/src/form/form-definition/helpers.test.ts
+++ model/src/form/form-definition/helpers.test.ts
@@ -1,4 +1,8 @@
-import { isFormDefinition } from '~/src/form/form-definition/helpers.js'
+import { type ConditionListItemRefValueDataV2 } from '~/src/conditions/types.js'
+import {
+  isConditionListItemRefValueData,
+  isFormDefinition
+} from '~/src/form/form-definition/helpers.js'
 import { type FormDefinition } from '~/src/form/form-definition/types.js'
 
 describe('helpers', () => {
@@ -17,12 +21,54 @@ describe('helpers', () => {
       expect(isFormDefinition(def)).toBe(false)
     })
 
+    it('should return false if primitive string', () => {
+      const def = ''
+      expect(isFormDefinition(def)).toBe(false)
+    })
+
+    it('should return false if primitive number', () => {
+      const def = 1
+      expect(isFormDefinition(def)).toBe(false)
+    })
+
     it('should return false if not a form def', () => {
       const def = {
         conditions: [],
         pages: []
       } as unknown as FormDefinition
       expect(isFormDefinition(def)).toBe(false)
+    })
+  })
+
+  describe('isConditionListItemRefValueData', () => {
+    it('should return true if a ConditionListItemRefValueDataV2', () => {
+      const data = {
+        itemId: 'b3a5030c-57f1-4d2e-8db9-6adeeba43c07',
+        listId: '58dfa64d-7b14-4b48-b8b1-48128ecf1f60'
+      } as ConditionListItemRefValueDataV2
+      expect(isConditionListItemRefValueData(data)).toBe(true)
+    })
+
+    it('should return false if undefined', () => {
+      const data = undefined
+      expect(isConditionListItemRefValueData(data)).toBe(false)
+    })
+
+    it('should return false if primitive string', () => {
+      const data = ''
+      expect(isConditionListItemRefValueData(data)).toBe(false)
+    })
+
+    it('should return false if primitive number', () => {
+      const data = 1
+      expect(isConditionListItemRefValueData(data)).toBe(false)
+    })
+
+    it('should return false if not a ConditionListItemRefValueDataV2', () => {
+      const data = {
+        itemId: 'b3a5030c-57f1-4d2e-8db9-6adeeba43c07'
+      } as ConditionListItemRefValueDataV2
+      expect(isConditionListItemRefValueData(data)).toBe(false)
     })
   })
 })
