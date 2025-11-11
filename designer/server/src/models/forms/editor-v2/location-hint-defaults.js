d2d7f3f268218aaa3195409ec42bef8e619bc065
David Stone <davidjamesstone@gmail.com>
2025-11-11 20:36:35 +0000

    Revert "Fixed hint text"
    This reverts commit 3b22899cacbde023373d0934c055dd2434377876.

--- designer/server/src/models/forms/editor-v2/location-hint-defaults.js
+++ designer/server/src/models/forms/editor-v2/location-hint-defaults.js
@@ -6,10 +6,10 @@ export const locationHintDefaults = {
   EastingNorthingField: 'For example. Easting: 248741, Northing: 63688',
 
   OsGridRefField:
-    'An OS grid reference number is made up of 2 letters and 6 numbers, for example, SO041885',
+    'An OS grid reference number is made up of 2 letters followed by 10 numbers, for example, SO7394301364',
 
   NationalGridFieldNumberField:
-    'A National Grid field number is made up of 2 letters followed by 8 numbers or 10 numbers, for example, SO7394301364',
+    'A National Grid field number is made up of 2 letters and 8 numbers, for example, SO04188589',
 
   LatLongField:
     'For Great Britain, the latitude will be a number between 49.850 and 60.859. The longitude will be a number between -13.687 and 1.767'
