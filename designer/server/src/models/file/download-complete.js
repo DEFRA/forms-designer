f337f296fdacc3de64b90618ef34accfcf32ce63
Jez Barnsley <jbarnsley.github@gmail.com>
2025-11-11 12:59:35 +0000

    Correct local download url

--- designer/server/src/models/file/download-complete.js
+++ designer/server/src/models/file/download-complete.js
@@ -1,7 +1,19 @@
+import config from '~/src/config.js'
+
+const LOCALHOST = 'http://localhost'
+const INTERNAL_DOCKER = 'http://host.docker.internal'
+
 /**
  * @param { string } [downloadUrl]
  */
 export function downloadCompleteModel(downloadUrl) {
+  // Amend download url if running locally
+  if (
+    config.appBaseUrl.startsWith(LOCALHOST) &&
+    downloadUrl?.startsWith(INTERNAL_DOCKER)
+  ) {
+    downloadUrl = downloadUrl.replace(INTERNAL_DOCKER, LOCALHOST)
+  }
   const pageTitle = 'Your file is downloading'
   return {
     pageTitle,
