609e37d06b001593b1985e9eff1b6647d8fbbf86
Aaron Scully <aaron@scullyon.com>
2025-11-28 11:13:43 +0000

    Removing errant line.

--- designer/server/src/models/forms/editor-v2/location-hint-defaults.js
+++ designer/server/src/models/forms/editor-v2/location-hint-defaults.js
@@ -7,7 +7,6 @@ export const locationHintDefaults = {
 
   OsGridRefField:
     'An OS grid reference number is made up of 2 letters followed by either 6, 8 or 10 numbers, for example, TQ123456',
-  // An OS grid reference number is made up of 2 letters followed by either 6, 8 or 10 numbers, for example, TQ123456
 
   NationalGridFieldNumberField:
     'A National Grid field number is made up of 2 letters and 8 numbers, for example, SO04188589',
