1a3426c97643097e7009ac442193aa79e6c66f97
Jez Barnsley <jbarnsley.github@gmail.com>
2025-11-28 12:43:25 +0000

    UI changes but no API linkage yet

--- designer/server/src/routes/forms/index.js
+++ designer/server/src/routes/forms/index.js
@@ -25,6 +25,7 @@ import editorQuestionDetails from '~/src/routes/forms/editor-v2/question-details
 import editorQuestionDelete from '~/src/routes/forms/editor-v2/question-or-page-delete.js'
 import editorQuestion from '~/src/routes/forms/editor-v2/question-type.js'
 import editorQuestions from '~/src/routes/forms/editor-v2/questions.js'
+import editorResponses from '~/src/routes/forms/editor-v2/responses.js'
 import editorSections from '~/src/routes/forms/editor-v2/sections.js'
 import editorState from '~/src/routes/forms/editor-v2/state.js'
 import editorUpload from '~/src/routes/forms/editor-v2/upload.js'
@@ -47,6 +48,7 @@ export default [
   editorQuestionDelete,
   editorQuestionDetails,
   editorQuestions,
+  editorResponses,
   editorState,
   editorListItemDelete,
   editorCheckAnswersOverview,
