e32f50e63186e959d49f6a2eb3673821db538314
David Stone <davidjamesstone@gmail.com>
2025-12-01 14:45:05 +0000

    Add form submission service

---
+++ designer/server/src/services/formSubmissionService.test.js
@@ -0,0 +1,37 @@
+import config from '~/src/config.js'
+import { postJson } from '~/src/lib/fetch.js'
+import { sendSubmissionsFile } from '~/src/services/formSubmissionService.js'
+
+const submissionUrl = config.submissionUrl
+
+jest.mock('~/src/lib/fetch.js')
+
+describe('formSubmissionService', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('sendSubmissionsFile', () => {
+    it('should make a call to the submission service', async () => {
+      const formId = '296701a7-1076-40df-8378-4b6468993fad'
+      const fileId = '303f24b1-d486-4fa0-8bd3-ece8839c45b3'
+      const response = /** @type {IncomingMessage} */ ({})
+
+      jest.mocked(postJson).mockResolvedValue({
+        response,
+        body: { fileId }
+      })
+
+      const result = await sendSubmissionsFile(formId)
+
+      expect(jest.mocked(postJson)).toHaveBeenCalledWith(
+        new URL(`/submissions/${formId}`, submissionUrl)
+      )
+      expect(result).toEqual({ fileId })
+    })
+  })
+})
+
+/**
+ * @import { IncomingMessage } from 'http'
+ */
