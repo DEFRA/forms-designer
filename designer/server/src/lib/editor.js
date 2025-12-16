3e11dd539f5e20101da9abb309aba4623daa513d
David Stone <davidjamesstone@gmail.com>
2025-12-16 17:00:22 +0000

    Merge any unsupported keys from the source (e.g. customValidationMessage) when saving components

--- designer/server/src/lib/editor.js
+++ designer/server/src/lib/editor.js
@@ -11,6 +11,7 @@ import {
   isFormType,
   randomId
 } from '@defra/forms-model'
+import { merge } from '@hapi/hoek'
 
 import config from '~/src/config.js'
 import { delJson, patchJson, postJson, putJson } from '~/src/lib/fetch.js'
@@ -158,6 +159,10 @@ export async function updateQuestion(
   if (questionToChange) {
     // Side-effect to the component within the page
     questionToChange.type = questionDetails.type ?? ComponentType.TextField
+
+    // Merge any unsupported keys (e.g. customValidationMessage)
+    // from the source into the new question details
+    questionDetails = merge(questionToChange, questionDetails)
   }
 
   const payload = buildRepeaterPayload(page, origControllerType)
