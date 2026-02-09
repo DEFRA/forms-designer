bed40b6de31449fd1772c31471afe1e84245dfd2
Mohammed Khalid <emailmokhld@gmail.com>
2026-02-09 12:07:43 +0000

    refactor: improve currency formatting and add test

--- designer/server/src/common/nunjucks/filters/format-currency.js
+++ designer/server/src/common/nunjucks/filters/format-currency.js
@@ -1,7 +1,9 @@
 /**
+ * Formats a currency amount with thousand separators and two decimal places
  * @param {Parameters<Intl.NumberFormat['format']>[0]} value
- * @param {Intl.LocalesArgument} locale
- * @param {string} currency
+ * @param {string} [locale] - locale for formatting (e.g. 'en-GB', 'en-US')
+ * @param {string} [currency] - currency code (e.g. 'GBP', 'USD')
+ * @returns {string} Formatted amount (e.g., "£1,234.56")
  */
 export function formatCurrency(value, locale = 'en-GB', currency = 'GBP') {
   const formatter = new Intl.NumberFormat(locale, {
