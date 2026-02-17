d813565d631379045da439e41f2db5b5a921c529
David Stone <davidjamesstone@gmail.com>
2026-02-17 18:45:55 +0000

    Update role mapper

--- designer/server/src/models/account/role-mapper.js
+++ designer/server/src/models/account/role-mapper.js
@@ -1,15 +1,21 @@
 export const Roles = {
+  Superadmin: 'admin',
   Admin: 'admin',
+  FormPublisher: 'form-publisher',
   FormCreator: 'form-creator'
 }
 
 export const RoleNames = {
+  [Roles.Superadmin]: 'Superadmin',
   [Roles.Admin]: 'Admin',
+  [Roles.FormPublisher]: 'Form publisher',
   [Roles.FormCreator]: 'Form creator'
 }
 
 export const RoleDescriptions = {
+  [Roles.Superadmin]: 'Can publish and delete forms and manage users',
   [Roles.Admin]: 'Can publish and delete forms and manage users',
+  [Roles.FormPublisher]: 'Can create, edit and publish forms',
   [Roles.FormCreator]: 'Can create and edit existing forms only'
 }
 
