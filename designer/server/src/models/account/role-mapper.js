8cbda2f809d4b92accb0ae3d65a3c2ef46e12e3e
David Stone <davidjamesstone@gmail.com>
2026-02-20 10:37:31 +0000

    Update RoleDescription for Superadmin

--- designer/server/src/models/account/role-mapper.js
+++ designer/server/src/models/account/role-mapper.js
@@ -13,7 +13,8 @@ export const RoleNames = {
 }
 
 export const RoleDescriptions = {
-  [Roles.Superadmin]: 'Can publish and delete forms and manage users',
+  [Roles.Superadmin]:
+    'Can manage forms, users and perform system administration tasks',
   [Roles.Admin]: 'Can publish and delete forms and manage users',
   [Roles.FormPublisher]: 'Can create, edit and publish forms',
   [Roles.FormCreator]: 'Can create and edit existing forms only'
