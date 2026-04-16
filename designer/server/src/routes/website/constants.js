f40945f3d911d09692abb24e11d6de146f6dc478
Dean Vipond <deanvipond@gmail.com>
2026-04-16 11:21:28 +0100

    Added images and updated a couple of file names

--- designer/server/src/routes/website/constants.js
+++ designer/server/src/routes/website/constants.js
@@ -37,15 +37,21 @@ export const Level2MakingAFormMenu = {
   PLAN_TO_RECEIVE_DIGITAL_SUBMISSIONS: 'plan-to-receive-digital-submissions',
   ONLY_ASK_FOR_INFORMATION_YOU_NEED: 'only-ask-for-information-you-need',
   PLAN_YOUR_FORM: 'plan-your-form',
-  ADAPTING_A_PDF_OR_PAPER_FORM: 'adapting-a-pdf-or-paper-form',
+  ADAPTING_A_PDF_OR_PAPER_FORM_TO_A_DIGITAL_FORM:
+    'adapting-a-pdf-or-paper-form-to-a-digital-form',
   TAKE_PAYMENTS: 'take-payments',
   WRITE_A_PRIVACY_NOTICE: 'write-a-privacy-notice',
   AGREE_TO_THE_DATA_PROTECTION_TERMS_AND_CONDITIONS:
     'agree-to-the-data-protection-terms-and-conditions',
   CREATE_GUIDANCE_PAGES_ON_GOVUK: 'create-guidance-pages-on-govuk',
   HAVE_AN_ALTERNATIVE_CONTACT_METHOD: 'have-an-alternative-contact-method',
+  CHOOSE_THE_RIGHT_FORMAT_FOR_YOUR_QUESTIONS:
+    'choose-the-right-format-for-your-questions',
+  HOW_TO_MAKE_CONDITIONS: 'how-to-make-conditions',
   SHARE_A_DRAFT_WITH_OTHERS: 'share-a-draft-with-others',
-  GET_A_PEER_REVIEW: 'get-a-peer-review'
+  GET_A_PEER_REVIEW: 'get-a-peer-review',
+  SUBMIT_FOR_PUBLICATION: 'submit-for-publication',
+  RETIRING_A_PDF_OR_PAPER_FORM: 'retiring-a-pdf-or-paper-form'
 }
 
 /**
