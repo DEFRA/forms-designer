40e0e4dcbb83d403cd30b65d1a9c67a4c5b6900b
Parvesh M <parveshh@gmail.com>
2026-04-17 12:37:46 +0100

    refactor: downloadCompleteModel to improve handling of localstack url

--- designer/server/src/models/file/download-complete.js
+++ designer/server/src/models/file/download-complete.js
@@ -3,15 +3,32 @@ import config from '~/src/config.js'
 const URL_PROTOCOL = 'http://'
 const LOCALHOST = `${URL_PROTOCOL}localhost`
 const INTERNAL_DOCKER = `${URL_PROTOCOL}host.docker.internal`
+const LOCALSTACK_CONTAINER = `${URL_PROTOCOL}localstack`
 
 /**
  * @param { string } [downloadUrl]
  */
 export function downloadCompleteModel(downloadUrl) {
-  // Change host in download url if running locally
+  // Change host in download url if running locally.
+  // The presigned URL may embed an internal Docker hostname (host.docker.internal
+  // when using Docker Desktop, or the localstack service name when containers
+  // communicate over the Docker network in CI). Neither is resolvable by the
+  // browser, so we rewrite to localhost so the browser can fetch the file
+  // directly from LocalStack.
+  const isInternalDockerUrl =
+    downloadUrl !== undefined &&
+    (downloadUrl.startsWith(INTERNAL_DOCKER) ||
+      downloadUrl.startsWith(LOCALSTACK_CONTAINER))
+
   const isRunningLocally =
-    config.appBaseUrl.startsWith(LOCALHOST) &&
-    downloadUrl?.startsWith(INTERNAL_DOCKER)
+    config.appBaseUrl.startsWith(LOCALHOST) && isInternalDockerUrl
+
+  let effectiveDownloadUrl = downloadUrl
+  if (isRunningLocally && downloadUrl) {
+    effectiveDownloadUrl = downloadUrl
+      .replace(INTERNAL_DOCKER, LOCALHOST)
+      .replace(LOCALSTACK_CONTAINER, LOCALHOST)
+  }
 
   const pageTitle = 'Your file is downloading'
   return {
@@ -20,9 +37,7 @@ export function downloadCompleteModel(downloadUrl) {
       text: pageTitle,
       size: 'large'
     },
-    downloadUrl: isRunningLocally
-      ? downloadUrl?.replace(INTERNAL_DOCKER, LOCALHOST)
-      : downloadUrl,
+    downloadUrl: effectiveDownloadUrl,
     buttonText: 'Download file'
   }
 }
