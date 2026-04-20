45fe3e4225b630edd2ac58f0d7ae8c95637f6c20
Parvesh M <parveshh@gmail.com>
2026-04-20 13:26:14 +0100

    chore: adding test coverage for download-complete

--- designer/server/src/models/file/download-complete.test.js
+++ designer/server/src/models/file/download-complete.test.js
@@ -9,6 +9,7 @@ const hostDockerInternalDownloadUrl =
 const localstackDownloadUrl = 'http://localstack:4566/my-bucket/test-file.txt'
 const zeroAddressDownloadUrl = 'http://0.0.0.0:4566/my-bucket/test-file.txt'
 const localhostDownloadUrl = 'http://localhost:4566/my-bucket/test-file.txt'
+const externalDownloadUrl = 'http://example.com/my-bucket/test-file.txt'
 const pageTitle = 'Your file is downloading'
 const buttonText = 'Download file'
 
@@ -65,6 +66,55 @@ describe('download complete model', () => {
         size: 'large'
       },
       downloadUrl: localstackDownloadUrl,
+      buttonText
+    })
+  })
+
+  test('does not rewrite external download URLs when running locally', () => {
+    jest.mocked(config).appBaseUrl = localAppBaseUrl
+
+    const result = downloadCompleteModel(externalDownloadUrl)
+
+    expect(result).toEqual({
+      pageTitle,
+      pageHeading: {
+        text: pageTitle,
+        size: 'large'
+      },
+      downloadUrl: externalDownloadUrl,
+      buttonText
+    })
+  })
+
+  test('does not rewrite download URLs when APP_BASE_URL is invalid', () => {
+    jest.mocked(config).appBaseUrl = 'not-a-valid-url'
+
+    const result = downloadCompleteModel(localstackDownloadUrl)
+
+    expect(result).toEqual({
+      pageTitle,
+      pageHeading: {
+        text: pageTitle,
+        size: 'large'
+      },
+      downloadUrl: localstackDownloadUrl,
+      buttonText
+    })
+  })
+
+  test('returns the original download URL when it cannot be parsed', () => {
+    jest.mocked(config).appBaseUrl = localAppBaseUrl
+
+    const invalidDownloadUrl = 'http://%'
+    const result = downloadCompleteModel(invalidDownloadUrl)
+
+    expect(result).toEqual({
+      pageTitle,
+      pageHeading: {
+        text: pageTitle,
+        size: 'large'
+      },
+      downloadUrl: invalidDownloadUrl,
       buttonText
     })
   })
