4e1f1981bffada1d8d03195948406258dbffc740
Jez Barnsley <jbarnsley.github@gmail.com>
2026-04-21 09:22:29 +0100

    Fixed test

--- designer/server/src/lib/dead-letter-queue.js
+++ designer/server/src/lib/dead-letter-queue.js
@@ -58,8 +58,6 @@ export async function getDeadLetterQueueMessages(dlq, token) {
   const { body } = await getJsonByType(requestUrl, getHeaders(token))
 
   // Dedupe in case of duplicate messages
-  // Ensure the last occurrence of the same MessageId is used as this will contain the valid ReceiptHandle
-  // (older ReceiptHandles for the same message will not be valid)
   const uniqueMessages = new Map()
   for (const message of body.messages) {
     uniqueMessages.set(message.MessageId, message)
