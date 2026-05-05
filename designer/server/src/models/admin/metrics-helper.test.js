06cc5e9d1cb5271615834bbe771172f354a01931
Jez Barnsley <jbarnsley.github@gmail.com>
2026-05-05 09:04:22 +0100

    More Coverage

--- designer/server/src/models/admin/metrics-helper.test.js
+++ designer/server/src/models/admin/metrics-helper.test.js
@@ -1,7 +1,10 @@
+import { FormMetricType, FormStatus } from '@defra/forms-model'
+
 import {
   buildAriaLabel,
   buildChangePhrase,
-  calcChangePercentage
+  calcChangePercentage,
+  mapOverviewMetrics
 } from '~/src/models/admin/metrics-helper.js'
 
 describe('metrics model', () => {
@@ -54,4 +57,88 @@ describe('metrics model', () => {
       expect(calcChangePercentage(1, 2)).toBe('-50.0')
     })
   })
+
+  describe('mapOverviewMetrics', () => {
+    it('should map all metric types when maps empty and draft', () => {
+      const metrics = /** @type {FormOverviewMetric[]} */ ([
+        {
+          type: FormMetricType.OverviewMetric,
+          formStatus: FormStatus.Draft,
+          formId: 'form-id',
+          summaryMetrics: {
+            name: 'form-name',
+            features: []
+          },
+          featureMetrics: {},
+          submissionsCount: 0,
+          updatedAt: new Date('2026-01-01')
+        }
+      ])
+      const submissionCountsDraft = new Map()
+      const submissionCountsLive = new Map()
+      const formDaysToPublish = new Map()
+      const formRepublished = new Map()
+      expect(
+        mapOverviewMetrics(
+          metrics,
+          submissionCountsDraft,
+          submissionCountsLive,
+          formDaysToPublish,
+          formRepublished
+        )
+      ).toEqual([
+        {
+          daysToPublish: '-',
+          features: '',
+          name: 'form-name',
+          formName: 'form-name',
+          republished: '-',
+          submissions: 0
+        }
+      ])
+    })
+
+    it('should map all metric types when live', () => {
+      const metrics = /** @type {FormOverviewMetric[]} */ ([
+        {
+          type: FormMetricType.OverviewMetric,
+          formStatus: FormStatus.Live,
+          formId: 'form-id',
+          summaryMetrics: {
+            name: 'form-name',
+            features: ['Email', 'File upload']
+          },
+          featureMetrics: {},
+          submissionsCount: 0,
+          updatedAt: new Date('2026-01-01')
+        }
+      ])
+      const submissionCountsDraft = new Map([['form-id', 3]])
+      const submissionCountsLive = new Map([['form-id', 7]])
+      const formDaysToPublish = new Map([['form-id', 17]])
+      const formRepublished = new Map([['form-id', 2]])
+      expect(
+        mapOverviewMetrics(
+          metrics,
+          submissionCountsDraft,
+          submissionCountsLive,
+          formDaysToPublish,
+          formRepublished
+        )
+      ).toEqual([
+        {
+          features: 'Email, File upload',
+          name: 'form-name',
+          formName: 'form-name',
+          submissions: 7,
+          daysToPublish: 17,
+          republished: 2
+        }
+      ])
+    })
+  })
 })
+
+/**
+ * @import { FormOverviewMetric } from '@defra/forms-model'
+ */
