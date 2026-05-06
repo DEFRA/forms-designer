b6f3d17cbe37a0d38cfcf785a567c0ffe1123b6e
Jez Barnsley <jbarnsley.github@gmail.com>
2026-05-06 10:48:29 +0100

    Extra coverage

--- designer/server/src/routes/admin/form-metrics.test.js
+++ designer/server/src/routes/admin/form-metrics.test.js
@@ -4,6 +4,7 @@ import { StatusCodes } from 'http-status-codes'
 
 import { createServer } from '~/src/createServer.js'
 import { getMetrics } from '~/src/lib/metrics.js'
+import { publishPlatformMetricsDownloadRequestedEvent } from '~/src/messaging/publish.js'
 import { authSuperAdmin as auth } from '~/test/fixtures/auth.js'
 import { renderResponse } from '~/test/helpers/component-helpers.js'
 
@@ -28,7 +29,7 @@ describe('Form metrics routes', () => {
   })
 
   describe('form-metrics', () => {
-    test('should render report form', async () => {
+    test('should render report form with form activity', async () => {
       const mockMetrics = {
         overview: [],
         totals: /** @type {FormTotalsMetric} */ ({
@@ -49,6 +50,50 @@ describe('Form metrics routes', () => {
       const options = {
         method: 'get',
         url: '/admin/form-metrics',
+        auth
+      }
+
+      const { response, container } = await renderResponse(server, options)
+
+      const $mastheadHeading = container.getByRole('heading', { level: 1 })
+      const $links = container.getAllByRole('link')
+
+      expect($mastheadHeading).toHaveTextContent('Defra Form Designer metrics')
+      expect($mastheadHeading).toHaveClass('govuk-heading-xl')
+
+      // Check tab headings and active tab
+      expect($links[5]).toHaveTextContent('My account')
+      expect($links[6]).toHaveTextContent('Manage users')
+      expect($links[7]).toHaveTextContent('Admin tools')
+      expect($links[8]).toHaveTextContent('Support')
+      expect($links[9]).toHaveTextContent('Back to admin tools')
+
+      expect(response.statusCode).toEqual(StatusCodes.OK)
+      expect(response.headers['content-type']).toContain('text/html')
+      expect(response.result).toMatchSnapshot()
+    })
+
+    test('should render report form with component usage', async () => {
+      const mockMetrics = {
+        overview: [],
+        totals: /** @type {FormTotalsMetric} */ ({
+          last7Days: {},
+          prev7Days: {},
+          last30Days: {},
+          prev30Days: {},
+          lastYear: {},
+          prevYear: {},
+          allTime: {},
+          draftSubmissions: {},
+          liveSubmissions: {},
+          updatedAt: new Date('2026-01-01T00:00:00.000Z')
+        })
+      }
+      jest.mocked(getMetrics).mockResolvedValueOnce(mockMetrics)
+
+      const options = {
+        method: 'get',
+        url: '/admin/form-metrics/component-usage',
         auth
       }
 
@@ -146,6 +191,37 @@ describe('Form metrics routes', () => {
       // Verify only headers rows (since supplied data did not include any live rows)
       const csvContent = response.payload
       expect(csvContent).toBe('"Form name","Form URL","Live submissions"')
+    })
+
+    test('should throw if error during download', async () => {
+      const mockMetrics = {
+        overview: [
+          {
+            formStatus: FormStatus.Draft,
+            summaryMetrics: { name: 'Form 2', slug: 'form-2' },
+            submissionsCount: 2
+          }
+        ],
+        totals: {}
+      }
+      // @ts-expect-error - partial mock of data
+      jest.mocked(getMetrics).mockResolvedValueOnce(mockMetrics)
+
+      jest
+        .mocked(publishPlatformMetricsDownloadRequestedEvent)
+        .mockImplementationOnce(() => {
+          throw new Error('unable to send audit message')
+        })
+
+      const options = {
+        method: 'get',
+        url: '/admin/form-metrics-download',
+        auth
+      }
+
+      const { response } = await renderResponse(server, options)
+
+      expect(response.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR)
     })
   })
 })
