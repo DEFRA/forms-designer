808d9016802e99592e78f1f55036af580c61ae86
David Stone <davidjamesstone@gmail.com>
2026-05-21 15:00:26 +0100

    feat/DF-1093: Micrositeredesign (#1438)
    * Added first few form guidance pages
    
    * More guidance pages and Level2 nav additions
    
    * Couple of extra pages
    
    * Conditions page added.
    
    * Added images and updated a couple of file names
    
    * Added alt text to images and created a replacement 'resources' page (currently called dv-resources, for discussion with Dave before we replace the existing).
    
    * Stash
    
    * Stash
    
    * Stash
    
    * Fix typo in comment
    
    * Remove unused routes
    
    * Remove about website route
    
    * Update cookies route
    
    * Rename making a form index page
    
    * Fix indentation
    
    * Website making a form section
    
    * Update the homepage and removed tabbed content
    
    * Updated the website resources page
    
    * Home page nav link text
    
    * Remove unused resources route and its associated options
    
    * Formatting
    
    * Remove "resources" sub menus
    
    * Making a form website page
    
    * Making a form sub menu and formatting
    
    * Making a form sub menu and formatting
    
    * New resources page (currently dv-resources) and updated features page, plus some links added.
    
    * Remove unused css class
    
    * Website features page
    
    * Formatting
    
    * Style website images
    
    * File link example
    
    * Fix website tests
    
    * Sonar fixes (constant for duplicate text)
    
    * Added downloadable files and finished Resources and Features pages
    
    * Added link to Resources page
    
    * Update snapshot tests
    
    * Added a couple of missing links to pages in making-a-form section
    
    * Update href in making a form website section
    
    * Fix Making a Form > Get started links
    
    ---------
    
    Co-authored-by: Dean Vipond <deanvipond@gmail.com>

--- designer/server/src/common/nunjucks/context/build-navigation.js
+++ designer/server/src/common/nunjucks/context/build-navigation.js
@@ -1,12 +1,9 @@
 import {
-  formsAboutPath,
   formsFeaturesPath,
-  formsGetStartedPath,
   formsLibraryPath,
+  formsMakingAFormPath,
   formsResourcesPath,
-  formsServicesPath,
-  formsSupportPath,
-  formsWebsitePaths
+  formsSupportPath
 } from '~/src/models/links.js'
 
 /**
@@ -24,53 +21,28 @@ export function buildEntry(text, url, options) {
 }
 
 /**
- * @param { string | undefined } path
- * @param { string } pathChecked
- */
-export function isVisible(path, pathChecked) {
-  if (path !== undefined && formsWebsitePaths.includes(pathChecked)) {
-    return formsWebsitePaths.some((websitePath) => {
-      return path.includes(websitePath)
-    })
-  }
-
-  return true
-}
-
-/**
  * @param {Partial<Request> | null} request
  */
 export function buildNavigation(request) {
   return [
-    buildEntry('Forms library', formsLibraryPath, {
+    buildEntry('Home', formsLibraryPath, {
       isActive: !!request?.path?.startsWith(formsLibraryPath)
     }),
-    buildEntry('Services', formsServicesPath, {
-      isActive:
-        !!request?.path?.startsWith(formsServicesPath) || request?.path === '/',
-      isVisible: isVisible(request?.path, formsServicesPath)
-    }),
-    buildEntry('About', formsAboutPath, {
-      isActive: !!request?.path?.startsWith(formsAboutPath),
-      isVisible: isVisible(request?.path, formsAboutPath)
-    }),
-    buildEntry('Get started', formsGetStartedPath, {
-      isActive: !!request?.path?.startsWith(formsGetStartedPath),
-      isVisible: isVisible(request?.path, formsGetStartedPath)
-    }),
     buildEntry('Features', formsFeaturesPath, {
-      isActive: !!request?.path?.startsWith(formsFeaturesPath),
-      isVisible: isVisible(request?.path, formsFeaturesPath)
+      isActive: !!request?.path?.startsWith(formsFeaturesPath)
+    }),
+    buildEntry('Making a form', formsMakingAFormPath, {
+      isActive: !!request?.path?.startsWith(formsMakingAFormPath)
     }),
     buildEntry('Resources', formsResourcesPath, {
       isActive: !!request?.path?.startsWith(formsResourcesPath),
-      isVisible: isVisible(request?.path, formsResourcesPath)
+      isVisible: request?.auth?.isAuthenticated ?? false
     }),
     buildEntry('Support', formsSupportPath, {
       isActive: !!request?.path?.startsWith(formsSupportPath)
     })
   ]
-    .filter(({ isVisible: isVisibleFilter }) => isVisibleFilter)
+    .filter(({ isVisible }) => isVisible)
     .map(({ isVisible: _isVisible, ...entry }) => {
       return entry
     })
--- designer/server/src/routes/website/content.js
+++ designer/server/src/routes/website/content.js
@@ -1,102 +1,130 @@
-import {
-  Level2GetStartedMenu,
-  Level2ResourcesMenu,
-  WebsiteLevel1Routes
-} from '~/src/routes/website/constants.js'
+import { Level2MakingAFormMenu } from '~/src/routes/website/constants.js'
 import features from '~/src/routes/website/content/features.js'
-import { whatsNewLatest } from '~/src/routes/website/content/whatsNew.js'
+
+const heading = 'Create and publish Defra forms on GOV.UK'
 
 export default {
   home: {
     mastHead: {
-      heading: 'Create and publish Defra forms on GOV.UK',
-      description: 'Create and publish Defra forms on GOV.UK'
-    },
-    whatsNew: {
-      date: {
-        text: '30 January 2026',
-        iso: '2026-01-30T15:00:00.000Z'
-      },
-      text: 'Confirmation emails now include customer answers and unique reference numbers.'
+      heading,
+      description: heading
     }
   },
-  whatsNew: {
+  makingAForm: {
     mastHead: {
-      heading: 'Updates',
-      caption: "What's new",
-      description: 'Updates to the Defra Form Designer and Developer Plugin.'
+      heading,
+      description: heading
     },
-    latest: whatsNewLatest
-  },
-  getStarted: {
     menus: [
       {
         text: 'Get started',
-        param: WebsiteLevel1Routes.GET_STARTED,
         parent: true,
         children: [
           {
-            param: Level2GetStartedMenu.GET_ACCESS,
-            text: 'Get access to the Defra Form Designer'
+            param: Level2MakingAFormMenu.MAKE_SURE_YOU_NEED_A_FORM,
+            text: 'Make sure you need a form'
           },
           {
-            param: Level2GetStartedMenu.MAKE_FORM_LIVE,
-            text: 'Make a form live checklist'
+            param: Level2MakingAFormMenu.VIEW_THE_DEMO_FORM,
+            text: 'View the demo form'
           },
           {
-            param: Level2GetStartedMenu.FORM_SUITABILITY,
-            text: 'Form suitability criteria'
-          },
-          {
-            param: Level2GetStartedMenu.MEASURING_SUCCESS,
-            text: 'Measuring the success of your form'
+            param: Level2MakingAFormMenu.WATCH_THE_OVERVIEW_VIDEO,
+            text: 'Watch the overview video'
           }
         ]
-      }
-    ]
-  },
-  resources: {
-    menus: [
+      },
       {
-        text: 'Good form design guide',
-        param: WebsiteLevel1Routes.RESOURCES,
+        text: 'Planning and design',
         parent: true,
         children: [
           {
-            param: Level2ResourcesMenu.DOES_IT_NEED,
-            text: 'Does this need to be a form?'
+            param: Level2MakingAFormMenu.WHAT_MAKES_A_GOOD_FORM,
+            text: 'What makes a good form'
           },
           {
-            param: Level2ResourcesMenu.ACCESSIBILITY,
-            text: 'Accessibility and inclusion'
+            param: Level2MakingAFormMenu.GET_THE_RIGHT_PEOPLE_TOGETHER,
+            text: 'Get the right people together'
           },
           {
-            param: Level2ResourcesMenu.SMES,
-            text: 'Working with subject matter experts (SMEs)'
+            param: Level2MakingAFormMenu.PLAN_TO_RECEIVE_ONLINE_SUBMISSIONS,
+            text: 'Plan to receive digital submissions'
           },
           {
-            param: Level2ResourcesMenu.QUESTION_PROTOCOLS,
-            text: 'Question protocols'
+            param: Level2MakingAFormMenu.ONLY_ASK_FOR_INFORMATION_YOU_NEED,
+            text: 'Only ask for information you need'
           },
           {
-            param: Level2ResourcesMenu.PROTOTYPING,
-            text: 'Prototyping a form'
+            param: Level2MakingAFormMenu.PLAN_YOUR_FORM,
+            text: 'Plan your form'
           },
           {
-            param: Level2ResourcesMenu.FORM_PAGES_GOVUK,
-            text: 'Form pages on GOV.UK'
+            param:
+              Level2MakingAFormMenu.ADAPTING_A_PDF_OR_PAPER_FORM_TO_A_DIGITAL_FORM,
+            text: 'Adapting a PDF or paper form to a digital form'
           },
           {
-            param: Level2ResourcesMenu.PEER_REVIEWING,
-            text: 'Peer reviewing forms'
+            param: Level2MakingAFormMenu.TAKE_PAYMENTS,
+            text: 'Take payments'
+          }
+        ]
+      },
+      {
+        text: 'Mandatory tasks',
+        parent: true,
+        children: [
+          {
+            param: Level2MakingAFormMenu.WRITE_A_PRIVACY_NOTICE,
+            text: 'Write a privacy notice'
           },
           {
-            param: Level2ResourcesMenu.PRIVACY_NOTICES,
-            text: 'Privacy notices'
+            param:
+              Level2MakingAFormMenu.AGREE_TO_THE_DATA_PROTECTION_TERMS_AND_CONDITIONS,
+            text: 'Agree to the data protection terms and conditions'
+          },
+          {
+            param: Level2MakingAFormMenu.CREATE_GUIDANCE_PAGES_ON_GOVUK,
+            text: 'Create guidance pages on GOV.UK'
+          },
+          {
+            param: Level2MakingAFormMenu.HAVE_AN_ALTERNATIVE_CONTACT_METHOD,
+            text: 'Have an alternative contact method'
+          }
+        ]
+      },
+      {
+        text: 'Build, test and publish',
+        parent: true,
+        children: [
+          {
+            param:
+              Level2MakingAFormMenu.CHOOSE_THE_RIGHT_FORMAT_FOR_YOUR_QUESTIONS,
+            text: 'Choose the right format for your questions'
+          },
+          {
+            param: Level2MakingAFormMenu.HOW_TO_MAKE_CONDITIONS,
+            text: 'How to make conditions'
+          },
+          {
+            param: Level2MakingAFormMenu.SHARE_A_DRAFT_WITH_OTHERS,
+            text: 'Share a draft with others'
+          },
+          {
+            param: Level2MakingAFormMenu.GET_A_PEER_REVIEW,
+            text: 'Get a peer review'
+          },
+          {
+            param: Level2MakingAFormMenu.SUBMIT_FOR_PUBLICATION,
+            text: 'Submit for publication'
+          },
+          {
+            param: Level2MakingAFormMenu.RETIRING_A_PDF_OR_PAPER_FORM,
+            text: 'Retiring a PDF or paper form'
           }
         ]
       }
     ]
   },
+  resources: {},
   features
 }
