095cae3b04a52002c7ff1c8fe5d7fa87bfd1171f
David Stone <davidjamesstone@gmail.com>
2026-05-21 13:29:00 +0100

    Download all submission files

--- designer/server/src/models/file/file.js
+++ designer/server/src/models/file/file.js
@@ -1,3 +1,6 @@
+import { hasComponentsEvenIfNoNext } from '@defra/forms-model'
+import Boom from '@hapi/boom'
+
 import { buildErrorList } from '~/src/common/helpers/build-error-details.js'
 import { formsLibraryBackLink } from '~/src/models/links.js'
 
@@ -30,5 +33,101 @@ export function fileViewModel(email, validation) {
 }
 
 /**
+ * @param {string} email
+ * @param {string} referenceNumber
+ * @param {Record<string, FormAdapterFile[]>} files
+ * @param {FormDefinition} definition
+ */
+export function downloadAllViewModel(
+  email,
+  referenceNumber,
+  files,
+  definition
+) {
+  const pageTitle = 'Download attached files'
+  const fileKeys = Object.keys(files)
+  const allFiles = fileKeys.flatMap((key) => files[key])
+
+  const list = Object.entries(files).flatMap(([name, fileList]) => {
+    const component = getComponent(definition, name)
+
+    return fileList.map((file) => ({
+      key: {
+        text: file.fileName
+      },
+      value: {
+        html:
+          component.title +
+          ` <strong id="downloaded-tag-${file.fileId}" class="govuk-tag govuk-tag--green pull-right app-hidden">Downloaded</strong>
+        <strong id="downloading-tag-${file.fileId}" class="govuk-tag govuk-tag--yellow pull-right app-hidden">Downloading</strong>`
+      },
+      actions: {
+        classes: 'app-summary-list__actions--narrow',
+        items: [
+          {
+            href: `/file-download/${file.fileId}`,
+            text: 'Download',
+            visuallyHiddenText: `Download file ${file.fileName} for component ${component.title}`,
+            attributes: {
+              'data-fileid': file.fileId
+            }
+          }
+        ]
+      }
+    }))
+  })
+
+  return {
+    backLink: formsLibraryBackLink,
+    pageTitle,
+    pageHeading: {
+      text: pageTitle,
+      size: 'large'
+    },
+    email,
+    list,
+    files,
+    allFiles,
+    // field: {
+    //   id: 'email',
+    //   name: 'email',
+    //   label: {
+    //     text: 'Email address'
+    //   },
+    //   value: validation?.formValues.email ?? email
+    // },
+    // errorList: buildErrorList(validation?.formErrors, ['email']),
+    // formErrors: validation?.formErrors,
+    // formValues: validation?.formValues,
+    referenceNumber,
+    buttonText: 'Download all files'
+  }
+}
+
+/**
+ * Find component by name
+ * @param {FormDefinition} definition
+ * @param {string} componentName
+ * @returns {ComponentDef}
+ * @throws {Error} If component is not found
+ */
+export function getComponent(definition, componentName) {
+  for (const page of definition.pages) {
+    if (hasComponentsEvenIfNoNext(page)) {
+      const component = page.components.find(
+        ({ name }) => name === componentName
+      )
+      if (component) {
+        return component
+      }
+    }
+  }
+
+  throw Boom.notFound(`Component not found with name '${componentName}'`)
+}
+
+/**
+ * @import { FormDefinition, ComponentDef } from '@defra/forms-model'
+ * @import { FormAdapterFile } from '@defra/forms-engine-plugin/engine/types.js'
  * @import { ValidationFailure } from '~/src/common/helpers/types.js'
  */
