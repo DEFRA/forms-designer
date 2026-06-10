dd82b32931af4183d1b72f31419a8bec8c1e06be
David Stone <davidjamesstone@gmail.com>
2026-06-10 13:03:46 +0100

    Refactor to reduce code duplication

--- designer/server/src/common/helpers/date-field-helper.js
+++ designer/server/src/common/helpers/date-field-helper.js
@@ -1,21 +1,26 @@
-import { isValid, parseISO } from 'date-fns'
-
 import {
-  insertValidationErrors,
-  leftPadDateIfSupplied
-} from '~/src/lib/utils.js'
+  buildDatePartsValuesAndErrors,
+  coerceDatePartsValue,
+  prepareDatePartsValue,
+  setupDatePartsSchema
+} from '~/src/common/helpers/date-parts-field-helper.js'
+import { leftPadDateIfSupplied } from '~/src/lib/utils.js'
 
 const BASE_ERROR = '{{#label}} must be a real date'
-const ERROR_CLASS = ' govuk-input--error'
 const NUMBER_OF_DATE_PARTS = 3
 
 const elementLookup = ['day', 'month', 'year']
+const partFields = [
+  { idSuffix: 'day', label: 'Day', classes: 'govuk-input--width-2' },
+  { idSuffix: 'month', label: 'Month', classes: 'govuk-input--width-2' },
+  { idSuffix: 'year', label: 'Year', classes: 'govuk-input--width-4' }
+]
 
 /**
- * @param {any} value
+ * @param {any[]} coerced
  */
-const isValidArrayPayload = (value) => {
-  return Array.isArray(value) && value.length === NUMBER_OF_DATE_PARTS
+function buildDateString(coerced) {
+  return `${coerced[2]}-${leftPadDateIfSupplied(coerced[1].toString())}-${leftPadDateIfSupplied(coerced[0].toString())}`
 }
 
 const errorMessages = {
@@ -35,45 +40,20 @@ function setupSchema(joi) {
   const daySchema = joi.number().min(1).max(31)
   const monthSchema = joi.number().min(1).max(12)
   const yearSchema = joi.number().min(1000).max(3000)
-  const emptyString = joi.string().trim().valid('').required()
 
-  return {
-    partsSchema: /** @type {import('joi').ArraySchema<number[]>} */ (
-      joi
-        .array()
-        .ordered(
-          daySchema.required(),
-          monthSchema.required(),
-          yearSchema.required()
-        )
-        .length(NUMBER_OF_DATE_PARTS)
-    ),
-    emptyPayload: joi
-      .array()
-      .items(emptyString, emptyString, emptyString)
-      .length(NUMBER_OF_DATE_PARTS)
-  }
+  return setupDatePartsSchema(
+    joi,
+    [daySchema.required(), monthSchema.required(), yearSchema.required()],
+    NUMBER_OF_DATE_PARTS
+  )
 }
 
 /**
  * @param {any} value
- * @param {ArraySchema<any[]>} emptyPayload
+ * @param {import('joi').ArraySchema<any[]>} emptyPayload
  */
 function prepareImpl(value, emptyPayload) {
-  if (!isValidArrayPayload(value)) {
-    return { value }
-  }
-
-  // Treat three empty strings as
-  // empty and let the base type handle it
-  const { error } = emptyPayload.validate(value)
-  const isEmpty = !error
-
-  if (isEmpty) {
-    return { value: undefined }
-  }
-
-  return { value }
+  return prepareDatePartsValue(value, emptyPayload, NUMBER_OF_DATE_PARTS)
 }
 
 /**
@@ -100,56 +80,12 @@ export const gdsDateExtension = (joi) => {
        * @param {any} helpers
        */
       method: function (value, helpers) {
-        if (!isValidArrayPayload(value)) {
-          return { value }
-        }
-
-        const key = helpers.state.path[0]
-        const { error, value: coerced } = partsSchema.validate(value, {
-          abortEarly: false
+        return coerceDatePartsValue(value, helpers, {
+          partsSchema,
+          partCount: NUMBER_OF_DATE_PARTS,
+          elementLookup,
+          buildDateString
         })
-
-        if (error) {
-          const details = error.details
-          const errors = helpers.errorsArray()
-
-          details.forEach((err) => {
-            const customContext = { ...err.context, key }
-            if (err.type === 'number.base') {
-              const elemName =
-                typeof err.context?.key === 'number' &&
-                err.context.key < NUMBER_OF_DATE_PARTS
-                  ? elementLookup[err.context.key]
-                  : ''
-
-              if (elemName) {
-                errors.push(
-                  helpers.error(`dateParts.${elemName}.required`, customContext)
-                )
-              }
-            } else {
-              errors.push(helpers.error(err.type, customContext))
-            }
-          })
-
-          return {
-            value: coerced,
-            errors
-          }
-        }
-
-        const date = parseISO(
-          `${coerced[2]}-${leftPadDateIfSupplied(coerced[1].toString())}-${leftPadDateIfSupplied(coerced[0].toString())}`
-        )
-
-        if (!isValid(date)) {
-          return {
-            value,
-            errors: helpers.error('date.base', { key })
-          }
-        }
-
-        return { value: date }
       }
     }
   }
@@ -161,42 +97,9 @@ export const gdsDateExtension = (joi) => {
  * @param {ErrorDetails | undefined} errors
  */
 export function buildDateValuesAndErrors(fieldName, values, errors) {
-  const parts = /** @type {string[]} */ (values[fieldName]) ?? ['', '', '']
-
-  const dateErrors = errors ? errors[fieldName] : undefined
-
-  return {
-    items: [
-      {
-        id: `${fieldName}-day`,
-        name: fieldName,
-        label: 'Day',
-        autocomplete: 'off',
-        value: /** @type { string | undefined } */ (parts[0]),
-        classes: 'govuk-input--width-2' + (dateErrors ? ERROR_CLASS : '')
-      },
-      {
-        id: `${fieldName}-month`,
-        name: fieldName,
-        label: 'Month',
-        autocomplete: 'off',
-        value: /** @type { string | undefined } */ (parts[1]),
-        classes: 'govuk-input--width-2' + (dateErrors ? ERROR_CLASS : '')
-      },
-      {
-        id: `${fieldName}-year`,
-        name: fieldName,
-        label: 'Year',
-        autocomplete: 'off',
-        value: /** @type { string | undefined } */ (parts[2]),
-        classes: 'govuk-input--width-4' + (dateErrors ? ERROR_CLASS : '')
-      }
-    ],
-    ...insertValidationErrors(dateErrors)
-  }
+  return buildDatePartsValuesAndErrors(fieldName, values, errors, partFields)
 }
 
 /**
- * @import { ArraySchema } from 'joi'
  * @import { ErrorDetails } from '~/src/common/helpers/types.js'
  */
--- designer/server/src/common/helpers/month-year-field-helper.js
+++ designer/server/src/common/helpers/month-year-field-helper.js
@@ -1,21 +1,25 @@
-import { isValid, parseISO } from 'date-fns'
-
 import {
-  insertValidationErrors,
-  leftPadDateIfSupplied
-} from '~/src/lib/utils.js'
+  buildDatePartsValuesAndErrors,
+  coerceDatePartsValue,
+  prepareDatePartsValue,
+  setupDatePartsSchema
+} from '~/src/common/helpers/date-parts-field-helper.js'
+import { leftPadDateIfSupplied } from '~/src/lib/utils.js'
 
 const BASE_ERROR = '{{#label}} must be a real date'
-const ERROR_CLASS = ' govuk-input--error'
 const NUMBER_OF_DATE_PARTS = 2
 
 const elementLookup = ['month', 'year']
+const partFields = [
+  { idSuffix: 'month', label: 'Month', classes: 'govuk-input--width-2' },
+  { idSuffix: 'year', label: 'Year', classes: 'govuk-input--width-4' }
+]
 
 /**
- * @param {any} value
+ * @param {any[]} coerced
  */
-const isValidArrayPayload = (value) => {
-  return Array.isArray(value) && value.length === NUMBER_OF_DATE_PARTS
+function buildDateString(coerced) {
+  return `${coerced[1]}-${leftPadDateIfSupplied(coerced[0].toString())}-01`
 }
 
 const errorMessages = {
@@ -33,41 +37,20 @@ const errorMessages = {
 function setupSchema(joi) {
   const monthSchema = joi.number().min(1).max(12)
   const yearSchema = joi.number().min(1000).max(3000)
-  const emptyString = joi.string().trim().valid('').required()
 
-  return {
-    partsSchema: /** @type {import('joi').ArraySchema<number[]>} */ (
-      joi
-        .array()
-        .ordered(monthSchema.required(), yearSchema.required())
-        .length(NUMBER_OF_DATE_PARTS)
-    ),
-    emptyPayload: joi
-      .array()
-      .items(emptyString, emptyString)
-      .length(NUMBER_OF_DATE_PARTS)
-  }
+  return setupDatePartsSchema(
+    joi,
+    [monthSchema.required(), yearSchema.required()],
+    NUMBER_OF_DATE_PARTS
+  )
 }
 
 /**
  * @param {any} value
- * @param {ArraySchema<any[]>} emptyPayload
+ * @param {import('joi').ArraySchema<any[]>} emptyPayload
  */
 function prepareImpl(value, emptyPayload) {
-  if (!isValidArrayPayload(value)) {
-    return { value }
-  }
-
-  // Treat two empty strings as
-  // empty and let the base type handle it
-  const { error } = emptyPayload.validate(value)
-  const isEmpty = !error
-
-  if (isEmpty) {
-    return { value: undefined }
-  }
-
-  return { value }
+  return prepareDatePartsValue(value, emptyPayload, NUMBER_OF_DATE_PARTS)
 }
 
 /**
@@ -94,56 +77,12 @@ export const gdsMonthYearExtension = (joi) => {
        * @param {any} helpers
        */
       method: function (value, helpers) {
-        if (!isValidArrayPayload(value)) {
-          return { value }
-        }
-
-        const key = helpers.state.path[0]
-        const { error, value: coerced } = partsSchema.validate(value, {
-          abortEarly: false
+        return coerceDatePartsValue(value, helpers, {
+          partsSchema,
+          partCount: NUMBER_OF_DATE_PARTS,
+          elementLookup,
+          buildDateString
         })
-
-        if (error) {
-          const details = error.details
-          const errors = helpers.errorsArray()
-
-          details.forEach((err) => {
-            const customContext = { ...err.context, key }
-            if (err.type === 'number.base') {
-              const elemName =
-                typeof err.context?.key === 'number' &&
-                err.context.key < NUMBER_OF_DATE_PARTS
-                  ? elementLookup[err.context.key]
-                  : ''
-
-              if (elemName) {
-                errors.push(
-                  helpers.error(`dateParts.${elemName}.required`, customContext)
-                )
-              }
-            } else {
-              errors.push(helpers.error(err.type, customContext))
-            }
-          })
-
-          return {
-            value: coerced,
-            errors
-          }
-        }
-
-        const date = parseISO(
-          `${coerced[1]}-${leftPadDateIfSupplied(coerced[0].toString())}-01`
-        )
-
-        if (!isValid(date)) {
-          return {
-            value,
-            errors: helpers.error('date.base', { key })
-          }
-        }
-
-        return { value: date }
       }
     }
   }
@@ -155,34 +94,9 @@ export const gdsMonthYearExtension = (joi) => {
  * @param {ErrorDetails | undefined} errors
  */
 export function buildMonthYearValuesAndErrors(fieldName, values, errors) {
-  const parts = /** @type {string[]} */ (values[fieldName]) ?? ['', '']
-
-  const dateErrors = errors ? errors[fieldName] : undefined
-
-  return {
-    items: [
-      {
-        id: `${fieldName}-month`,
-        name: fieldName,
-        label: 'Month',
-        autocomplete: 'off',
-        value: /** @type { string | undefined } */ (parts[0]),
-        classes: 'govuk-input--width-2' + (dateErrors ? ERROR_CLASS : '')
-      },
-      {
-        id: `${fieldName}-year`,
-        name: fieldName,
-        label: 'Year',
-        autocomplete: 'off',
-        value: /** @type { string | undefined } */ (parts[1]),
-        classes: 'govuk-input--width-4' + (dateErrors ? ERROR_CLASS : '')
-      }
-    ],
-    ...insertValidationErrors(dateErrors)
-  }
+  return buildDatePartsValuesAndErrors(fieldName, values, errors, partFields)
 }
 
 /**
- * @import { ArraySchema } from 'joi'
  * @import { ErrorDetails } from '~/src/common/helpers/types.js'
  */
