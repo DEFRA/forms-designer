347f419d2538ed03908b9cf0ccfeed44c7eeed36
Jez Barnsley <jbarnsley.github@gmail.com>
2026-07-13 12:32:20 +0100

    Temporarily hide welsh button as a feature flag

--- designer/server/src/models/forms/editor-v2/pages.js
+++ designer/server/src/models/forms/editor-v2/pages.js
@@ -346,13 +346,13 @@ function buildRightSideActions(slug) {
       href: `/library/${slug}/editor-v2/download`,
       classes: BUTTON_SECONDARY_CLASS,
       attributes: null
-    },
-    {
-      text: 'Welsh translation',
-      href: editorv2Path(slug, 'welsh'),
-      classes: BUTTON_SECONDARY_CLASS,
-      attributes: null
     }
+    // {
+    //   text: 'Welsh translation',
+    //   href: editorv2Path(slug, 'welsh'),
+    //   classes: BUTTON_SECONDARY_CLASS,
+    //   attributes: null
+    // }
   ]
 }
 
