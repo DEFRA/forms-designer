e643eb843a346797c8ca5c8ea73988b74b76414d
David Stone <davidjamesstone@gmail.com>
2026-07-27 12:29:44 +0100

    Sonar fixes (Split it into smaller functions)

--- designer/server/src/models/submission/index.js
+++ designer/server/src/models/submission/index.js
@@ -10,6 +10,137 @@ import {
 import { format } from '~/src/models/forms/history-date-utils.js'
 
 /**
+ * Process a page component
+ * @param {ComponentDef} component
+ * @param {Page} page
+ * @param {{ item: Record<string, any>, index: number}} [repeat]
+ * @param {Context} context
+ */
+function processSectionComponent(component, page, context, repeat) {
+  let value = ''
+  let actions
+  const { formModel, data, translator, referenceNumber } = context
+  const field = formModel.componentMap.get(component.name)
+
+  if (field instanceof FormComponent) {
+    if (component.type === ComponentType.FileUploadField) {
+      if (component.name in data.files) {
+        const files = /** @type {any} */ (data.files[component.name])
+        if (Array.isArray(files) && files.length) {
+          value = field.getDisplayStringFromFormValue(
+            /** @type {any} */ (files),
+            translator
+          )
+          actions = [
+            {
+              href: `/files-download/${referenceNumber}`,
+              text: 'Download files',
+              visuallyHiddenText: 'Download files'
+            }
+          ]
+
+          return {
+            key: { text: component.title },
+            value: { text: value },
+            actions: { items: actions }
+          }
+        }
+      }
+    } else {
+      const source = repeat?.item ?? data.main
+
+      if (component.name in source) {
+        value = field.getDisplayStringFromFormValue(
+          /** @type {any} */ (source[component.name]),
+          translator
+        )
+
+        if (component.type === ComponentType.GeospatialField) {
+          actions = [
+            {
+              text: 'Review map',
+              href: `/submission/${referenceNumber}/map-review/${page.id}/${component.id}${repeat ? '#answer_' + repeat.index.toString() : ''}`,
+              visuallyHiddenText: 'Review map'
+            }
+          ]
+        }
+
+        return {
+          key: { text: component.title },
+          value: { text: value },
+          actions: actions ? { items: actions } : undefined
+        }
+      }
+    }
+  }
+
+  return undefined
+}
+
+/**
+ * Process a section page
+ * @param {Page} page
+ * @param {Context} context
+ * @returns {SummaryRows}
+ */
+function processSectionPage(page, context) {
+  if (!hasComponentsEvenIfNoNext(page)) {
+    return []
+  }
+
+  if (hasRepeater(page)) {
+    const { name, title } = page.repeat.options
+    const items = context.data.repeaters[name] ?? []
+
+    /** @type {SummaryRowKey} */
+    const key = { text: title }
+
+    /** @type {SummaryRowRepeaterValue} */
+    const value = {
+      repeatTitle: title,
+      rows: items.map((item, index) =>
+        page.components
+          .map((c) =>
+            processSectionComponent(c, page, context, { item, index })
+          )
+          .filter((v) => v !== undefined)
+      )
+    }
+
+    return [{ key, value }]
+  }
+
+  const components = page.components
+    .map((c) => processSectionComponent(c, page, context))
+    .filter((v) => v !== undefined)
+
+  return components
+}
+
+/**
+ * Process form section
+ * @param {Section | undefined} section
+ * @param {Context} context
+ */
+function processSection(section, context) {
+  const { summaries, pages } = context
+
+  const sectionPages = pages.filter(
+    (page) => page.section === (section ? section.id : undefined)
+  )
+
+  /** @type {SummaryRows} */
+  const rows = sectionPages.flatMap((page) => processSectionPage(page, context))
+
+  if (rows.length) {
+    summaries.push({
+      section: section?.title,
+      rows
+    })
+  }
+}
+
+/**
  * @param {FormAdapterSubmissionMessagePayload} submission - the form submission record
  * @param {FormDefinition} definition - the form definition
  */
@@ -25,137 +156,23 @@ export function submissionViewModel(submission, definition) {
   const { pages, sections } = definition
 
   /**
-   * @type { { section: string | undefined, rows: SummaryRows }[] }
+   * @type {{ section: string | undefined, rows: SummaryRows }[]}
    */
   const summaries = []
+  const context = {
+    summaries,
+    pages,
+    translator,
+    data,
+    formModel,
+    referenceNumber
+  }
 
-  ;[...sections, undefined].forEach(processSection)
+  ;[...sections, undefined].forEach((section) =>
+    processSection(section, context)
+  )
 
   const lede = `Submitted on ${format(meta.timestamp, "dd MMMM yyyy 'at' h:mm")}`
-
-  /**
-   * Process a page component
-   * @param {ComponentDef} component
-   * @param {Page} page
-   * @param {{ item: Record<string, any>, index: number}} [repeat]
-   */
-  function processSectionComponent(component, page, repeat) {
-    let value = ''
-    let actions
-
-    const field = formModel.componentMap.get(component.name)
-
-    if (field instanceof FormComponent) {
-      if (component.type === ComponentType.FileUploadField) {
-        if (component.name in data.files) {
-          const files = /** @type {any} */ (data.files[component.name])
-          if (Array.isArray(files) && files.length) {
-            value = field.getDisplayStringFromFormValue(
-              /** @type {any} */ (files),
-              translator
-            )
-            actions = [
-              {
-                href: `/files-download/${referenceNumber}`,
-                text: 'Download files',
-                visuallyHiddenText: 'Download files'
-              }
-            ]
-
-            return {
-              key: { text: component.title },
-              value: { text: value },
-              actions: { items: actions }
-            }
-          }
-        }
-      } else {
-        const source = repeat?.item ?? data.main
-
-        if (component.name in source) {
-          value = field.getDisplayStringFromFormValue(
-            /** @type {any} */ (source[component.name]),
-            translator
-          )
-
-          if (component.type === ComponentType.GeospatialField) {
-            actions = [
-              {
-                text: 'Review map',
-                href: `/submission/${referenceNumber}/map-review/${page.id}/${component.id}${repeat ? '#answer_' + repeat.index.toString() : ''}`,
-                visuallyHiddenText: 'Review map'
-              }
-            ]
-          }
-
-          return {
-            key: { text: component.title },
-            value: { text: value },
-            actions: actions ? { items: actions } : undefined
-          }
-        }
-      }
-    }
-
-    return undefined
-  }
-
-  /**
-   * Process a section page
-   * @param {Page} page
-   * @returns {SummaryRows}
-   */
-  function processSectionPage(page) {
-    if (!hasComponentsEvenIfNoNext(page)) {
-      return []
-    }
-
-    if (hasRepeater(page)) {
-      const { name, title } = page.repeat.options
-      const items = data.repeaters[name] ?? []
-
-      /** @type {SummaryRowKey} */
-      const key = { text: title }
-
-      /** @type {SummaryRowRepeaterValue} */
-      const value = {
-        repeatTitle: title,
-        rows: items.map((item, index) =>
-          page.components
-            .map((c) => processSectionComponent(c, page, { item, index }))
-            .filter((v) => v !== undefined)
-        )
-      }
-
-      return [{ key, value }]
-    }
-
-    const components = page.components
-      .map((c) => processSectionComponent(c, page))
-      .filter((v) => v !== undefined)
-
-    return components
-  }
-
-  /**
-   * Process form section
-   * @param {Section | undefined} section
-   */
-  function processSection(section) {
-    const sectionPages = pages.filter(
-      (page) => page.section === (section ? section.id : undefined)
-    )
-
-    /** @type {SummaryRows} */
-    const rows = sectionPages.flatMap(processSectionPage)
-
-    if (rows.length) {
-      summaries.push({
-        section: section?.title,
-        rows
-      })
-    }
-  }
 
   return {
     pageTitle,
@@ -173,6 +190,7 @@ export function submissionViewModel(submission, definition) {
  * @typedef {{ items: { href: string, text: string, visuallyHiddenText: string }[]}} SummaryRowActions
  * @typedef {{ key: SummaryRowKey, value: SummaryRowTextValue | SummaryRowRepeaterValue, actions?: SummaryRowActions | undefined}} SummaryRow
  * @typedef {SummaryRow[]} SummaryRows
+ * @typedef {{ summaries: { section: string | undefined, rows: SummaryRows }[], pages: Page[], translator: any, data: FormAdapterSubmissionMessagePayload, formModel: FormModel, referenceNumber: string }} Context
  */
 
 /**
