90eee9fab2f371d545da23d9a17e2764e3fadf8f
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2026-08-25 11:11:29 +0100

    feat/df-1149: Adds 'Form submissions' to metrics download (#1556)
    * Adds 'Form submissions' to metrics download
    
    * Sonar fix
    
    * Rework after review
    
    * Reworked month formatting

--- designer/server/src/models/admin/metrics-excel.test.js
+++ designer/server/src/models/admin/metrics-excel.test.js
@@ -1,7 +1,11 @@
 import { FormMetricType } from '@defra/forms-model'
 
 import { excelSnapshot } from '~/src/models/admin/metrics-excel-snapshot.js'
-import { getMetricsAsExcel } from '~/src/models/admin/metrics-excel.js'
+import {
+  formatYearMonthInWords,
+  getMetricsAsExcel,
+  getSubmissionSheetData
+} from '~/src/models/admin/metrics-excel.js'
 
 describe('metrics-excel', () => {
   describe('getMetricsAsExcel', () => {
@@ -436,11 +440,98 @@ describe('metrics-excel', () => {
         }
       }
 
+      const submissionsPerMonth = {
+        '2026-02': {
+          '6a0c7073789bf5cdcc66eaf9': 15
+        }
+      }
+
       // @ts-expect-error - partial mock of data
-      const res = getMetricsAsExcel(metrics, metricsWelsh)
+      const res = getMetricsAsExcel(metrics, metricsWelsh, submissionsPerMonth)
       // Check the generated XLSX file against an example we know is correct
       // The snapshot is stored in hex
       expect(res.toString('hex')).toEqual(excelSnapshot)
     })
   })
+
+  describe('getSubmittedSheetData', () => {
+    it('should build up columns and data rows', () => {
+      const submissionsPerMonth =
+        /** @type {Record<string, Record<string, number>>} */
+        ({
+          '2026-04': {
+            'form-id-2': 5,
+            'form-id-1': 3
+          },
+          '2026-05': {
+            'form-id-3': 2,
+            'form-id-2': 1
+          }
+        })
+      const metrics = {
+        overview: [
+          {
+            formId: 'form-id-1',
+            summaryMetrics: {
+              name: 'Form 1'
+            }
+          },
+          {
+            formId: 'form-id-2',
+            summaryMetrics: {
+              name: 'Form 2'
+            }
+          },
+          {
+            formId: 'form-id-4',
+            summaryMetrics: {
+              name: 'Form 4'
+            }
+          },
+          {
+            formId: 'form-id-3',
+            summaryMetrics: {
+              name: 'Form 3'
+            }
+          }
+        ]
+      }
+      // @ts-expect-error - partial mock of data
+      const res = getSubmissionSheetData(submissionsPerMonth, metrics)
+      expect(res.columns).toEqual([
+        { title: 'Form name', dataKey: 'formName', attributes: { wch: 50 } },
+        { title: 'Apr-26', dataKey: '2026-04' },
+        { title: 'May-26', dataKey: '2026-05' }
+      ])
+      expect(res.data).toEqual([
+        { '2026-04': 3, '2026-05': 0, formName: 'Form 1' },
+        { '2026-04': 5, '2026-05': 1, formName: 'Form 2' },
+        { '2026-04': 0, '2026-05': 2, formName: 'Form 3' },
+        { '2026-04': 0, '2026-05': 0, formName: 'Form 4' }
+      ])
+    })
+  })
+
+  describe('formatYearMonthInWords', () => {
+    it('should convert year-month to correct format', () => {
+      expect(formatYearMonthInWords('2025-01')).toBe('Jan-25')
+      expect(formatYearMonthInWords('2025-02')).toBe('Feb-25')
+      expect(formatYearMonthInWords('2025-03')).toBe('Mar-25')
+      expect(formatYearMonthInWords('2025-04')).toBe('Apr-25')
+      expect(formatYearMonthInWords('2025-05')).toBe('May-25')
+      expect(formatYearMonthInWords('2025-06')).toBe('Jun-25')
+      expect(formatYearMonthInWords('2025-07')).toBe('Jul-25')
+      expect(formatYearMonthInWords('2025-08')).toBe('Aug-25')
+      expect(formatYearMonthInWords('2025-09')).toBe('Sep-25')
+      expect(formatYearMonthInWords('2025-10')).toBe('Oct-25')
+      expect(formatYearMonthInWords('2025-11')).toBe('Nov-25')
+      expect(formatYearMonthInWords('2025-12')).toBe('Dec-25')
+      expect(formatYearMonthInWords('2026-01')).toBe('Jan-26')
+      expect(formatYearMonthInWords('2026-02')).toBe('Feb-26')
+    })
+  })
 })
+
+/**
+ * @import { FormOverviewMetric, FormTotalsMetric } from '@defra/forms-model'
+ */
